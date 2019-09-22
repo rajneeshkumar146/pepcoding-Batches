111dea90ba12b00032a38ef304e276edd114f440
Rajneesh <rajneeshkumar146@gmail.com>
2019-09-22 19:58:24 +0530

    update

--- 2019/Aug(81)/lecture_004_recursion_basic/l004_voidType.cpp
+++ 2019/Aug(81)/lecture_004_recursion_basic/l004_voidType.cpp
@@ -50,15 +50,129 @@ int permuation(string str, string ans)
     return count;
 }
 
+string codes[]={"+","abc","def","ghi","jkl","mno","pqrs","tuv","wx","yz","*","#"};
+
+int keyPad(string ques,string ans){
+if(ques.length()==0){
+    cout<<ans<<" ";
+    return 1;
+}
+
+char ch=ques[0];
+string roq=ques.substr(1);
+string code=codes[ch-'0'];
+
+int count=0;
+for(int i=0;i<code.length();i++){
+     count+=keyPad(roq,ans+code[i]);
+}
+
+return count;
+}
+
+
 void basic()
 {
-    // cout << subsequence("abc", "") << endl;
-    cout << permuation("aaa", "") << endl;
+    // << subsequence("abc", "") << endl;
+    // cout << permuation("aaa", "") << endl;
+    cout<<keyPad("235","");
+}
+
+
+//pathProblem.===========================
+int functionCall=0;
+string res="";
+
+int mazePath(int sr,int sc,int er,int ec,string ans){
+     functionCall++;
+   if(sr==er && sc==ec){
+        // cout<<ans<<" ";
+        return 0;
+    }
+
+    int count=0;
+    int lefts=0;
+    int rights=0;
+    int diagonals=0;
+    if(sc+1<=ec){
+     lefts=mazePath(sr,sc+1,er,ec,ans+"H");
+    //  lefts+="H";
+    }
+
+
+    if(sr+1<=er){
+     rights=mazePath(sr+1,sc,er,ec,ans+"V");
+    //  rights+="V";
+    }
+
+
+    if(sr+1<=er&& sc+1<=ec){
+     diagonals=mazePath(sr+1,sc+1,er,ec,ans+"D");
+    //    diagonals+="D";  
+    }
+
+    // string ans_="";
+    // if(ans_.length()<lefts.length()) ans_=lefts;
+    // if(ans_.length()<rights.length()) ans_=rights;
+    // if(ans_.length()<diagonals.length()) ans_=diagonals;
+    
+    return max(max(lefts,rights),diagonals)+1;
+}
+
+
+int mazePath_multi(int sr,int sc,int er,int ec,string ans){
+    if(sr==er && sc==ec){
+        cout<<ans<<" ";
+        return 1;
+    }
+
+    int count=0;
+    for(int i=1;sc+i<=ec;i++){
+     count+=mazePath_multi(sr,sc+i,er,ec,ans+"H"+to_string(i));
+    }
+
+
+    for(int i=1;sr+i<=er;i++){
+     count+=mazePath_multi(sr+i,sc,er,ec,ans+"V"+ to_string(i));
+    }
+
+
+    for(int i=1;sr+i<=er && sc+i<=ec;i++){
+     count+=mazePath_multi(sr+i,sc+i,er,ec,ans+"D"+to_string(i));
+    }
+    
+    return count;
+}
+
+int boardPath(int si,int ei,string ans){
+    if(si==ei){
+        cout<<ans<<endl;
+        return 1;
+    }
+
+    int count=0;
+    for(int dice=1;dice<=6 && si+dice<=ei;dice++){
+        count+=boardPath(si+dice,ei,ans+to_string(dice));
+    }
+
+    return count;
+
+}
+
+void pathProblem(){
+    cout<<mazePath(0,0,3,5,"")<<endl;
+    cout<<functionCall<<endl;
+    cout<<res<<endl;
+    
+    // cout<<mazePath_multi(0,0,2,2,"")<<endl;
+    // cout<<boardPath(0,10,"")<<endl;
+
 }
 
 void solve()
 {
     basic();
+    // pathProblem();
 }
 
 int main(int args, char **argv)
