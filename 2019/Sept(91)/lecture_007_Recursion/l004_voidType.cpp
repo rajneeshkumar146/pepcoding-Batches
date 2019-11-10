da8d722edc6d907667d2440732247121b7ad670b
Rajneesh <rajneeshkumar146@gmail.com>
2019-11-10 23:12:09 +0530

    update

--- 2019/Sept(91)/lecture_007_Recursion/l004_voidType.cpp
+++ 2019/Sept(91)/lecture_007_Recursion/l004_voidType.cpp
@@ -285,6 +285,29 @@ int coinChange_permu02(vector<int> &arr, vector<bool> &isDone, int target, strin
     return count;
 }
 
+int coinChange_perm03(vector<int> &arr, int vidx, int target, string ans)
+{
+
+    if (vidx == arr.size() || (target == 0 && ans.length() != 0))
+    {
+        if (target == 0 && ans.length() != 0)
+        {
+            cout << ans << endl;
+            return 1;
+        }
+        return 0;
+    }
+
+    int count = 0;
+    if (target - arr[vidx] >= 0)
+    {
+        count += coinChange_perm03(arr, 0, target - arr[vidx], ans + to_string(arr[vidx]) + " ");
+    }
+    count += coinChange_perm03(arr, vidx + 1, target, ans);
+
+    return count;
+}
+
 int coinChange_combi01(vector<int> &arr, int vidx, int target, string ans)
 {
 
@@ -363,6 +386,145 @@ void combiAndPermu()
     cout << coinChange_combi03(arr, 0, 10, "") << endl;
 }
 
+//=========================================
+
+int nqueenCombi(int boxes, int tnq, int qloc, int qpsf, string ans)
+{
+    if (qpsf == tnq)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int i = qloc + 1; i <= boxes; i++)
+    {
+        count += nqueenCombi(boxes, tnq, i, qpsf + 1, ans + "b" + to_string(i) + "q" + to_string(qpsf) + " ");
+    }
+    return count;
+}
+
+int nqueenPermu(int boxes, int tnq, vector<bool> loc, int qpsf, string ans)
+{
+    if (qpsf == tnq)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int i = 0; i <= boxes; i++)
+    {
+        if (!loc[i])
+        {
+            loc[i] = true;
+            count += nqueenPermu(boxes, tnq, loc, qpsf + 1, ans + "b" + to_string(i) + "q" + to_string(qpsf) + " ");
+            loc[i] = false;
+        }
+    }
+    return count;
+}
+
+int nqueenCombi_sub(int boxes, int tnq, int qloc, int qpsf, string ans)
+{
+    if (qpsf == tnq || qloc == boxes)
+    {
+        if (qpsf == tnq)
+        {
+            cout << ans << endl;
+            return 1;
+        }
+        return 0;
+    }
+
+    int count = 0;
+    return nqueenCombi_sub(boxes, tnq, qloc + 1, qpsf + 1, ans + "b" + to_string(qloc + 1) + "q" + to_string(qpsf) + " ") +
+           nqueenCombi_sub(boxes, tnq, qloc + 1, qpsf, ans);
+}
+
+int nqueenPermu_sub(int boxes, int tnq, int qloc, vector<bool> &loc, int qpsf, string ans)
+{
+    if (qpsf == tnq || qloc > boxes)
+    {
+        if (qpsf == tnq)
+        {
+            cout << ans << endl;
+            return 1;
+        }
+        return 0;
+    }
+    int count_ = 0;
+    if (!loc[qloc])
+    {
+        loc[qloc] = true;
+        count_ += nqueenPermu_sub(boxes, tnq, 0, loc, qpsf + 1, ans + "b" + to_string(qloc) + "q" + to_string(qpsf) + " ");
+        loc[qloc] = false;
+    }
+    count_ += nqueenPermu_sub(boxes, tnq, qloc + 1, loc, qpsf, ans);
+    return count_;
+}
+
+void nqueenWays()
+{
+    vector<bool> loc(8, false);
+    // cout << nqueenCombi(7, 3, -1, 0, "") << endl;
+    // cout << nqueenPermu(7, 3, loc, 0, "") << endl;
+    // cout<<nqueenCombi_sub(7,3,-1,0,"")<<endl;
+    cout << nqueenPermu_sub(7, 3, 0, loc, 0, "") << endl;
+}
+
+//=================================================
+
+bool isSafeToPlace(vector<vector<bool>> &boxes, int x, int y)
+{
+
+    int dir[4][2] = {{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+    for (int d = 0; d < 4; d++)
+    {
+
+        for (int rad = 1; rad < boxes[0].size(); rad++)
+        {
+            int r = x + rad * dir[d][0];
+            int c = y + rad * dir[d][1];
+            if (r >= 0 && c >= 0 && r < boxes.size() && c < boxes[0].size() && boxes[r][c])
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int nQueen(vector<vector<bool>> &boxes, int tnq, int qloc, int qpsf, string ans)
+{
+    if (qpsf == tnq)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int i = qloc + 1; i < boxes.size() * boxes[0].size(); i++)
+    {
+        int x = i / boxes.size();
+        int y = i % boxes.size();
+        if (isSafeToPlace(boxes, x, y))
+        {
+            boxes[x][y] = true;
+            count += nQueen(boxes, tnq, i, qpsf + 1, ans + "(" + to_string(x) + ", " + to_string(y) + ")" + "@" + to_string(qpsf) + " ");
+            boxes[x][y] = false;
+        }
+    }
+    return count;
+}
+
+void Nqueen()
+{
+    vector<vector<bool>> boxes(4, vector<bool>(4, false));
+    cout << nQueen(boxes, 4, -1, 0, "") << endl;
+}
+
 void pathType()
 {
     // cout << mazePathSimple(0, 0, 2, 2, "") << endl;
@@ -376,7 +538,9 @@ void solve()
 {
     // basicQues();
     // pathType();
-    combiAndPermu();
+    // combiAndPermu();
+    // nqueenWays();
+    Nqueen();
 }
 
 int main(int args, char **argv)
