944c8019c6cbecb70971105211841395f014432d
Rajneesh <rajneeshkumar146@gmail.com>
2019-11-12 20:20:38 +0530

    update

--- 2019/Aug(81)/lecture_008_AVL/avl.java
+++ 2019/Aug(81)/lecture_008_AVL/avl.java
@@ -1,13 +1,13 @@
 public class avl{
     public static void main(String[] args){
-    int[] arr={10,20,30,40,50,60,70,80,90,100};
+    int[] arr={10,20,30,40,50,60,70,80,90,100,110,120};
     Node root=construct(arr,0,arr.length-1);
     // display(root);
 
-    root=addData(root,62);
-    root=addData(root,63);
-    root=addData(root,64);
-    root=addData(root,65);
+    root=addData(root,51);
+    root=addData(root,52);
+    // root=addData(root,64);
+    // root=addData(root,65);
     display(root);
 
 
