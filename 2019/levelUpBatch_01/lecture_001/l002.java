d81a66bcff0baea4a2570f25e18e3fc4a4eccb3f
Rajneesh <rajneeshkumar146@gmail.com>
2019-12-27 20:47:39 +0530

    update

--- 2019/levelUpBatch_01/lecture_001/l002.java
+++ 2019/levelUpBatch_01/lecture_001/l002.java
@@ -16,9 +16,12 @@ public class l002{
         // System.out.println(coinChgCombi_03(arr,0,tar,""));
 
 
-        boolean[] boxes=new boolean[6];
-        System.out.println(QueenBoxCombi_01(boxes,0,3,0,""));
+        // boolean[] boxes=new boolean[16];
+        // System.out.println(QueenBoxCombi_01(boxes,0,4,0,""));
         
+        boolean[][] boxes=new boolean[4][4];
+        // System.out.println(QueenBoxCombi_2D(boxes,0,4,0,""));
+        System.out.println(QueenBoxCombi_2D_02(boxes,0,4,0,""));
 
 
     }
@@ -92,4 +95,95 @@ public class l002{
 
         return count;
     }
+public static boolean isQueenSafe(boolean[][] boxes,int x,int y){
+
+    int[][] dir={{0,-1},{-1,-1},{-1,0},{-1,1}, {0,1},{1,1},{1,0},{1,-1}};
+    
+    for(int i=0;i<dir.length;i++){
+        for(int rad=1;rad<Math.max(boxes.length,boxes[0].length);rad++){
+            int r=x+rad*dir[i][0];
+            int c=y+rad*dir[i][1];
+                
+            if(r<0 || c< 0 || r>=boxes.length || c>= boxes[0].length) break;
+            if(boxes[r][c]) return false;
+        }
+    }
+    return true;
+}
+
+
+
+    //lqpl=last queen place location
+    public static int QueenBoxCombi_2D(boolean[][] boxes,int lqpl,int tnq,int qpsf,String ans){
+        if(qpsf==tnq){
+            System.out.println(ans);
+            return 1;
+        }
+   
+        int count=0;
+        for(int i=0;i<boxes.length*boxes[0].length;i++){
+            int x=i/boxes[0].length;
+            int y=i%boxes[0].length;
+            if(!boxes[x][y] && isQueenSafe(boxes,x,y)){
+                 boxes[x][y]=true;
+                count+=QueenBoxCombi_2D(boxes,i+1,tnq,qpsf+1,ans+"("+x+", "+ y+") ");
+                boxes[x][y]=false; 
+            }
+    }
+   
+           return count;
+       }
+
+        //lqpl=last queen place location
+    public static int QueenBoxCombi_2D_02(boolean[][] boxes,int rowno,int tnq,int qpsf,String ans){
+        if(qpsf==tnq ){
+            if(qpsf==tnq){
+            System.out.println(ans);
+            return 1;
+            }
+            return 0;
+        }
+   
+        int count=0;
+        for(int i=0;i<boxes[0].length;i++){
+            int x=rowno;
+            int y=i;
+            if(isQueenSafe(boxes,x,y)){
+                //  boxes[x][y]=true;
+                count+=QueenBoxCombi_2D_02(boxes,rowno+1,tnq,qpsf+1,ans+"("+x+", "+ y+") ");
+                // boxes[x][y]=false; 
+            }
+    }
+   
+           return count;
+       }
+
+
+       public static int QueenBoxCombiSub_2D(boolean[][] boxes,int lqpl,int tnq,int qpsf,String ans){
+        if(qpsf==tnq || lqpl==boxes.length*boxes[0].length){
+            if(qpsf==tnq){
+            System.out.println(ans);
+            return 1;
+            }
+            return 0;
+        }
+   
+            int count=0;
+            int x=lqpl/boxes[0].length;
+            int y=lqpl%boxes[0].length;
+            if(isQueenSafe(boxes,x,y)){
+                 boxes[x][y]=true;
+
+                count+=QueenBoxCombiSub_2D(boxes,(x+1)*boxes[0].length,tnq,qpsf+1,ans+"("+x+", "+ y+") ");
+                boxes[x][y]=false; 
+            }
+
+            count+=QueenBoxCombiSub_2D(boxes,lqpl+1,tnq,qpsf,ans);
+   
+           return count;
+       }
+
+
+
+
 }
