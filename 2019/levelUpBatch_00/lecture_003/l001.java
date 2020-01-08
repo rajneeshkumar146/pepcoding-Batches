11b23352c11baa52d420010a2f46fa8af713d815
rajneeshkumar146 <rajneeshkumar146@gmail.com>
2020-01-08 22:04:35 +0530

    update

--- 2019/levelUpBatch_00/lecture_003/l001.java
+++ 2019/levelUpBatch_00/lecture_003/l001.java
@@ -13,8 +13,27 @@ public class l001 {
         // System.out.println(queen2DCombi_02(box, 0, 0, 0, 0, box.length, ""));
         // System.out.println(calls);
 
-        basicQuest();
+        // basicQuest();
+        // allSubstring();
+        // ArrayList<String> words = new ArrayList<>(
+        // Arrays.asList("i", "like", "ilike", "man", "go", "mango", "ice", "cream",
+        // "icecream", "and"));
 
+        // System.out.println(wordBreak("ilikeicecreamandmango", "", words));
+
+        char[][] board = { { '+', '-', '+', '+', '+', '+', '+', '+', '+', '+' },
+                { '+', '-', '+', '+', '+', '+', '+', '+', '+', '+' },
+                { '+', '-', '-', '-', '-', '-', '-', '-', '+', '+' },
+                { '+', '-', '+', '+', '+', '+', '+', '+', '+', '+' },
+                { '+', '-', '+', '+', '+', '+', '+', '+', '+', '+' },
+                { '+', '-', '-', '-', '-', '-', '-', '+', '+', '+' },
+                { '+', '-', '+', '+', '+', '-', '+', '+', '+', '+' },
+                { '+', '+', '+', '+', '+', '-', '+', '+', '+', '+' },
+                { '+', '+', '+', '+', '+', '-', '+', '+', '+', '+' },
+                { '+', '+', '+', '+', '+', '+', '+', '+', '+', '+' } };
+
+        String[] words = { "agra", "norway", "england", "gwalior" };
+        System.out.println(crossWord(board, words, 0));
     }
 
     public static int coinCombiInfi(int[] arr, int idx, int tar, String ans) {
@@ -146,8 +165,135 @@ public class l001 {
         }
 
         int count = 0;
-        count+=eqiSet(arr,idx+1,sum1+arr[idx],sum2,set1+arr[idx]+" ",set2);
-        count+=eqiSet(arr,idx+1,sum1,sum2+arr[idx],set1,set2+arr[idx]+" ");
+        count += eqiSet(arr, idx + 1, sum1 + arr[idx], sum2, set1 + arr[idx] + " ", set2);
+        count += eqiSet(arr, idx + 1, sum1, sum2 + arr[idx], set1, set2 + arr[idx] + " ");
+        return count;
+    }
+
+    public static int wordBreak(String que, String ans, ArrayList<String> words) {
+        if (que.length() == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = 1; i <= que.length(); i++) {
+            String word = que.substring(0, i);
+            if (words.contains(word)) {
+                count += wordBreak(que.substring(i), ans + word + " ", words);
+            }
+        }
+
+        return count;
+    }
+
+    public static void allSubstring() {
+        String s = "abcdefghijkl";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                System.out.println(s.substring(i, j));
+            }
+        }
+    }
+
+    // ==============================
+
+    public static boolean canHPword(char[][] board, int x, int y, String word) {
+
+        for (int j = 0; j < word.length(); j++) {
+            if (board[x][y + j] != '-' && board[x][y + j] != word.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean[] HPWord(char[][] board, int x, int y, String word) {
+        boolean[] loc = new boolean[word.length()];
+        for (int j = 0; j < word.length(); j++) {
+            if (board[x][y + j] == '-') {
+                loc[j] = true;
+                board[x][y + j] = word.charAt(j);
+            }
+        }
+
+        return loc;
+    }
+
+    public static void UnPHWord(char[][] board, int x, int y, boolean[] loc) {
+        for (int j = 0; j < loc.length; j++) {
+            if (loc[j]) {
+                board[x][y + j] = '-';
+            }
+        }
+    }
+
+    public static boolean canVPword(char[][] board, int x, int y, String word) {
+
+        for (int j = 0; j < word.length(); j++) {
+            if (board[x + j][y] != '-' && board[x + j][y] != word.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean[] HVWord(char[][] board, int x, int y, String word) {
+        boolean[] loc = new boolean[word.length()];
+        for (int j = 0; j < word.length(); j++) {
+            if (board[x + j][y] == '-') {
+                loc[j] = true;
+                board[x + j][y] = word.charAt(j);
+            }
+        }
+
+        return loc;
+    }
+
+    public static void UnPVWord(char[][] board, int x, int y, boolean[] loc) {
+        for (int j = 0; j < loc.length; j++) {
+            if (loc[j]) {
+                board[x + j][y] = '-';
+            }
+        }
+    }
+
+    public static int crossWord(char[][] board, String[] words, int idx) {
+
+        if (idx == words.length) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            return 1;
+        }
+
+        String word = words[idx];
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '-' || board[i][j] == word.charAt(0)) {
+                    if (canHPword(board, i, j, word)) {
+                        boolean[] loc = HPWord(board, i, j, word);
+                        count += crossWord(board, words, idx + 1);
+                        UnPHWord(board, i, j, loc);
+                    }
+
+                    if (canVPword(board, i, j, word)) {
+                        boolean[] loc = HVWord(board, i, j, word);
+                        count += crossWord(board, words, idx + 1);
+                        UnPVWord(board, i, j, loc);
+                    }
+                }
+            }
+
+        }
+
         return count;
     }
 
