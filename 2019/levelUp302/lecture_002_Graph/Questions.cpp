b91c521c34feceb2b8ebf3ae74a071cd306544e7
rajneeshkumar146 <rajneeshkumar146@gmail.com>
2020-04-23 22:56:45 +0530

    update

--- 2019/levelUp302/lecture_002_Graph/Questions.cpp
+++ 2019/levelUp302/lecture_002_Graph/Questions.cpp
@@ -170,3 +170,154 @@ int islandPerimeter(vector<vector<int>> &grid)
     }
     return (4 * totalOnes - 2 * commonRegion);
 }
+
+//Leetcode : 1091.=========================================================
+
+int shortestPathBinaryMatrix(vector<vector<int>> &grid)
+{
+    int n = grid.size();
+    if (n == 0)
+        return -1;
+    int m = grid[0].size();
+    if (m == 0)
+        return -1;
+
+    if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1)
+        return -1;
+
+    int dirA[8][2] = {{-1, -1}, {-1, 0}, {0, -1}, {-1, 1}, {1, -1}, {0, 1}, {1, 0}, {1, 1}};
+    queue<int> que;
+    que.push(0);
+    grid[0][0] = 1;
+
+    int level = 1;
+    while (que.size() != 0)
+    {
+        int size = que.size();
+        while (size-- > 0)
+        {
+            int rvtx = que.front();
+            que.pop();
+
+            int r = rvtx / m;
+            int c = rvtx % m;
+
+            if (r == n - 1 && c == m - 1)
+                return level;
+
+            for (int d = 0; d < 8; d++)
+            {
+                int x = r + dirA[d][0];
+                int y = c + dirA[d][1];
+
+                if (x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == 0)
+                {
+                    que.push((x * m + y));
+                    grid[x][y] = 1;
+                }
+            }
+        }
+        level++;
+    }
+
+    return -1;
+}
+
+//leetcode 286.=============================================================
+
+void wallsAndGates(vector<vector<int>> &grid)
+{
+    if (grid.size() == 0 || grid[0].size() == 0)
+        return;
+
+    int n = grid.size();
+    int m = grid[0].size();
+
+    int dirA[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    queue<int> que;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (grid[i][j] == 0)
+                que.push((i * m + j));
+
+    int level = 1;
+    while (que.size() != 0)
+    {
+        int size = que.size();
+        while (size-- > 0)
+        {
+            int rvtx = que.front();
+            que.pop();
+
+            int r = rvtx / m;
+            int c = rvtx % m;
+
+            for (int d = 0; d < 4; d++)
+            {
+                int x = r + dirA[d][0];
+                int y = c + dirA[d][1];
+
+                if (x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == 2147483647)
+                {
+                    que.push((x * m + y));
+                    grid[x][y] = level;
+                }
+            }
+        }
+        level++;
+    }
+}
+
+//leetcode 994.===========================================================
+
+int orangesRotting(vector<vector<int>> &grid)
+{
+    if (grid.size() == 0 || grid[0].size() == 0)
+        return;
+
+    int n = grid.size();
+    int m = grid[0].size();
+
+    int dirA[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    queue<int> que;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (grid[i][j] == 2)
+                que.push((i * m + j));
+
+    int level = 0;
+    while (que.size() != 0)
+    {
+        int size = que.size();
+        while (size-- > 0)
+        {
+            int rvtx = que.front();
+            que.pop();
+
+            int r = rvtx / m;
+            int c = rvtx % m;
+
+            for (int d = 0; d < 4; d++)
+            {
+                int x = r + dirA[d][0];
+                int y = c + dirA[d][1];
+
+                if (x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == 1)
+                {
+                    que.push((x * m + y));
+                    grid[x][y] = 2;
+                }
+            }
+        }
+        level++;
+    }
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (grid[i][j] == 1)
+                return -1;
+
+    return level;
+}
