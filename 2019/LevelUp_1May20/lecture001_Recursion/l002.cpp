4335b320a526eb152c66f70d94bf327f958b2c1e
rajneeshkumar146 <rajneeshkumar146@gmail.com>
2020-05-05 23:14:41 +0530

    update

--- 2019/LevelUp_1May20/lecture001_Recursion/l002.cpp
+++ 2019/LevelUp_1May20/lecture001_Recursion/l002.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-int coinChangePermutation_INF(vector<int> &arr, int tar, string ans)
+int coinChangePermutation_INF(vector<int> &arr, int lidx, int tar, string ans)
 {
     if (tar == 0)
     {
@@ -11,14 +11,14 @@ int coinChangePermutation_INF(vector<int> &arr, int tar, string ans)
     }
 
     int count = 0;
-    for (int ele : arr)
-        if (tar - ele >= 0)
-            count += coinChangePermutation_INF(arr, tar - ele, ans + to_string(ele) + " ");
+    for (int i = lidx; i < arr.size(); i++)
+        if (tar - arr[lidx] >= 0)
+            count += coinChangePermutation_INF(arr, 0, tar - arr[lidx], ans + to_string(arr[i]) + " ");
 
     return count;
 }
 
-int coinChangePermutation(vector<int> &arr, int tar, string ans)
+int coinChangePermutation(vector<int> &arr, int lidx, int tar, string ans)
 {
     if (tar == 0)
     {
@@ -27,11 +27,13 @@ int coinChangePermutation(vector<int> &arr, int tar, string ans)
     }
 
     int count = 0;
-    for (int i = 0; i < arr.size(); i++)
-    {   int temp = arr[i];
-        if (arr[i] > 0 && tar - arr[i] >= 0 )
-        {   arr[i] = -arr[i];
-            count += coinChangePermutation(arr, tar - temp, ans + to_string(temp) + " ");
+    for (int i = lidx; i < arr.size(); i++)
+    {
+        int temp = arr[i];
+        if (arr[i] > 0 && tar - arr[i] >= 0)
+        {
+            arr[i] = -arr[i];
+            count += coinChangePermutation(arr, 0, tar - temp, ans + to_string(temp) + " ");
             arr[i] = -arr[i];
         }
     }
@@ -65,24 +67,71 @@ int coinChangeCombination(vector<int> &arr, int lidx, int tar, string ans)
     int count = 0;
     for (int i = lidx; i < arr.size(); i++)
         if (tar - arr[i] >= 0)
-            count += coinChangeCombination(arr, i + 1, tar - arr[i], ans + to_string(arr[i]) + " ");
+            count += coinChangeCombination(arr, i + 1, tar - arr[i], ans + to_string(i) + " ");
 
     return count;
+}
+
+//Queen's===========================================================
+
+int queensCombination(vector<bool> &rooms, int room, int qpsf, int tnq, string ans) // qpsf: queen place so far.
+{
+    if (qpsf == tnq)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int r = room; r < rooms.size(); r++)
+        count += queensCombination(rooms, r + 1, qpsf + 1, tnq, ans + "Q" + to_string(qpsf) + "R" + to_string(r) + " ");
+
+    return count;
+}
+
+int queensPermutation(vector<bool> &rooms, int room, int qpsf, int tnq, string ans) // qpsf: queen place so far.
+{
+    if (qpsf == tnq)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int r = room; r < rooms.size(); r++)
+        if (!rooms[r])
+        {
+            rooms[r] = true;
+            count += queensPermutation(rooms, 0, qpsf + 1, tnq, ans + "Q" + to_string(qpsf) + "R" + to_string(r) + " ");
+            rooms[r] = false;
+        }
+
+    return count;
+}
+
+void queenProblem()
+{
+    vector<bool> rooms(16, false);
+    int tnq = 4;
+    cout << queensCombination(rooms, 0, 0, tnq, "") << endl;
+    // cout << queensPermutation(rooms, 0, 0, tnq, "") << endl;
 }
 
 void coinChange()
 {
     vector<int> arr{2, 3, 5, 7};
+    // vector<int> arr{1,1,1,1,1};
     int tar = 10;
-    // cout << coinChangePermutation_INF(arr, tar, "") << endl;
-    cout << coinChangePermutation(arr, tar, "") << endl;
+    // cout << coinChangePermutation_INF(arr,0, tar, "") << endl;
+    // cout << coinChangePermutation(arr, 0, tar, "") << endl;
 
     // cout << coinChangeCombination_INF(arr,0, tar, "") << endl;
-    // cout << coinChangeCombination(arr,0, tar, "") << endl;
+    cout << coinChangeCombination(arr, 0, tar, "") << endl;
 }
 
 int main()
 {
-    coinChange();
+    // coinChange();
+    queenProblem();
     return 0;
 }
--- 2019/LevelUp_1May20/lecture001_Recursion/l002.java
+++ 2019/LevelUp_1May20/lecture001_Recursion/l002.java
@@ -1,6 +1,7 @@
 public class l002{
      public static void main(String[] args){
-        coinChange();
+        // coinChange();
+        queenProblem();
      }
 
      public static void coinChange(){
@@ -8,6 +9,9 @@ public class l002{
          int tar=10;
 
 
+        //  System.out.println(coinChangePermutation_INF(arr,0,tar,""));
+        //  System.out.println(coinChangePermutation(arr,0,tar,""));
+        //  System.out.println(coinChangeCombination_INF(arr,0,tar,""));
          System.out.println(coinChangeCombination(arr,0,tar,""));
 
      }
@@ -24,14 +28,14 @@ public class l002{
     
          int count=0;
          if(tar-arr[lidx]>=0)
-             count+=coinChangePermutation_INF(arr,lidx+1,tar-arr[lidx],ans+ arr[lidx]+" ");
+             count+=coinChangePermutation_INF(arr,0,tar-arr[lidx],ans+ arr[lidx]+" ");
          count+=coinChangePermutation_INF(arr,lidx+1,tar,ans);
          
          return count;
     }
 
     
-    public static int coinChangePermutation(int[] arr, int tar, String ans){
+    public static int coinChangePermutation(int[] arr,int lidx, int tar, String ans){
         if(lidx==arr.length || tar==0){
             if(tar==0){
                 System.out.println(ans);
@@ -41,13 +45,13 @@ public class l002{
         }
     
          int count=0;
-        if (arr[i] > 0 && tar - arr[i] >= 0 ){
-            int temp = arr[i];
-            arr[i] = -arr[i];
-            count+=coinChangePermutation_INF(arr,lidx+1,tar-temp,ans + temp+" ");
-            arr[i] = -arr[i];
+        if (arr[lidx] > 0 && tar - arr[lidx] >= 0 ){
+            int temp = arr[lidx];
+            arr[lidx] = -arr[lidx];
+            count+=coinChangePermutation(arr,0,tar-temp,ans + temp+" ");
+            arr[lidx] = -arr[lidx];
         }
-            count+=coinChangePermutation_INF(arr,lidx+1,tar,ans);
+            count+=coinChangePermutation(arr,lidx+1,tar,ans);
          
          return count;
     }
@@ -64,7 +68,7 @@ public class l002{
 
      int count=0;
      if(tar-arr[lidx]>=0)
-         count+=coinChangeCombination_INF(arr,lidx+1,tar-arr[lidx],ans+ arr[lidx]+" ");
+         count+=coinChangeCombination_INF(arr,lidx,tar-arr[lidx],ans+ arr[lidx]+" ");
      count+=coinChangeCombination_INF(arr,lidx+1,tar,ans);
      
      return count;
@@ -87,6 +91,58 @@ public class l002{
         
         return count;
     }
+
+   //Queens.====================================================================
+
+public static int queensCombination2D(boolean[][] rooms, int room, int tnq, String ans) // qpsf: queen place so far.
+{
+    if (tnq==0)
+    {
+        System.out.println(ans);
+        return 1;
+    }
+
+    int count = 0;
+    for (int r = room; r < rooms.length*rooms[0].length; r++){
+        int x = r / rooms[0].length;
+        int y = r % rooms[0].length;
+        count += queensCombination2D(rooms, r + 1,tnq-1, ans + "(" + x + ", " + y + ") ");
+    }
+    return count;
+}
+
+public static int queensPermutation2D(boolean[][] rooms,int tnq, String ans) // qpsf: queen place so far.
+{
+    if (tnq==0)
+    {
+        System.out.println(ans);
+        return 1;
+    }
+
+    int count = 0;
+    for (int r = 0; r < rooms.length; r++){
+        int x = r / rooms[0].length;
+        int y = r % rooms[0].length;
+
+        if (!rooms[x][y])
+        {
+            rooms[x][y] = true;
+            count += queensPermutation2D(rooms,tnq-1,  ans + "(" + x + ", " + y + ") ");
+            rooms[x][y] = false;
+        }
+    }
+    return count;
+}
+
+public static void queenProblem()
+{
+    boolean[][] rooms=new boolean[4][4];
+    int tnq = 4;
+
+    System.out.println(queensCombination2D(rooms,0,tnq,""));
+    // System.out.println(queensPermutation2D(rooms,tnq,""));
+}
+
 
 
 
