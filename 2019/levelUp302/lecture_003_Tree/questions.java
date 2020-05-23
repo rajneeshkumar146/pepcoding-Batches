a6d3a66e1edb4b0cfbb9dfc8e280cfeda195e4fa
rajneeshkumar146 <rajneeshkumar146@gmail.com>
2020-05-23 23:03:46 +0530

    commit

--- 2019/levelUp302/lecture_003_Tree/questions.java
+++ 2019/levelUp302/lecture_003_Tree/questions.java
@@ -44,19 +44,86 @@ public class question{
             blockNode=path.get(i);
         }  
         return ans;
-    }   
+    }     
 
-    public List<Integer> distanceK_02(TreeNode root, TreeNode target, int K) {
+    // Leetcode: 112.=======================================================
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        if(root.left==null && root.right==null && sum-root.val==0)
+          return true;
     
+        boolean res=false;
+        res=res || hasPathSum(root.left,sum-root.val);
+        res=res || hasPathSum(root.right,sum-root.val);
+        return res;
     }
+
+    //Leetcode: 113.========================================================
     
-    public List<Integer> distanceK_02(TreeNode root, TreeNode target, int K) {
-        List<Integer> ans=new ArrayList<>();
-        return ans;
-    }      
+    public void pathSum(TreeNode root, int sum,List<List<Integer>> res,List<Integer> smallAns) {
+        if(root==null) return;
+        if(root.left==null && root.right==null && sum-root.val==0){
+            List<Integer> base=new ArrayList<>(smallAns);// res.push_back(smallAns); res.back().push_back(root.val);
+            base.add(root.val);
+            res.add(base);
+            return;
+        }
+        
+        smallAns.add(root.val);
+        pathSum(root.left,sum-root.val,res,smallAns);
+        pathSum(root.right,sum-root.val,res,smallAns);
+        smallAns.remove(smallAns.size()-1);
+    }
 
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if(root==null) return new ArrayList<>();
 
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> smallAns=new ArrayList<>();
+        pathSum(root,sum,res,smallAns);
+        return res;
+    }
 
+    //geeks: https://www.geeksforgeeks.org/find-maximum-path-sum-two-leaves-binary-tree/
 
+    public static int maxPathSum(Node root) {
+        max_leafToLeafSum=(int) -1e8;
+        leafToLeaf(root);
+        return max_leafToLeafSum;
+    }
+   
+   
+   public static int leafToLeaf(Node node){
+       if(node==null) return 0;
+
+       int leftNodeToLeafSum = leafToLeaf(node.left);
+       int rightNodeToLeafSum = leafToLeaf(node.right);
+
+       if(node.left!=null && node.right!=null){
+           max_leafToLeafSum=Math.max(max_leafToLeafSum,leftNodeToLeafSum + rightNodeToLeafSum + node.data);
+           return Math.max(leftNodeToLeafSum , rightNodeToLeafSum) + node.data;
+       }
+
+       return (node.left==null? rightNodeToLeafSum: leftNodeToLeafSum) + node.data;
+   }
+
+   //Leetcode 124.========================================================
+
+   int max_nodeToNodeSum = (int)-1e8;
+   public int maxPathSum(TreeNode root) {
+       maxPathSum_(root);
+       return max_nodeToNodeSum;
+   }     
+  public int maxPathSum_(TreeNode node){
+   if(node==null) return 0;
+
+   int leftNodeToNodeSum = maxPathSum_(node.left);
+   int rightNodeToNodeSum = maxPathSum_(node.right);
+   
+   int max_=Math.max(leftNodeToNodeSum,rightNodeToNodeSum) + node.val;
+   max_nodeToNodeSum=Math.max(Math.max(max_nodeToNodeSum,node.val),Math.max(leftNodeToNodeSum + node.val + rightNodeToNodeSum, max_));
+
+   return Math.max(max_,node.val);
+}
 
 }
--- 2019/try.cpp
+++ 2019/try.cpp
@@ -5,16 +5,26 @@
 #include <algorithm>
 #include <unordered_map>
 #include <sstream>
-#include <iomanip>  
+#include <iomanip>
 using namespace std;
+
+bool comp(string &s1, string &s2)
+{
+    return true;
+}
 
 int main()
 {
-double pi = 3.14159265359;
+    // double pi = 3.14159265359;
 
-stringstream stream;
-stream << fixed << setprecision(5) << pi;
-string s = stream.str();
+    // stringstream stream;
+    // stream << fixed << setprecision(5) << pi;
+    // string s = stream.str();
+    // cout<<s<<endl;
 
-cout<<s<<endl;
+    vector<string> ar{"asd", "sadfsa", "sa", "sdfgsdg"};
+    sort(ar.begin(), ar.end(), comp);
+    for(string s: ar) cout<<s<<endl;
+
+    return 0;
 }
