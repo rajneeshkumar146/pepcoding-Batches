8cc089ab50dc29f44fb527f13537e08ee2a118d2
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-07-29 23:18:12 +0530

    update

--- 2019/levelUp_juneBatch/lecture_006_DP/l001.java
+++ 2019/levelUp_juneBatch/lecture_006_DP/l001.java
@@ -635,20 +635,212 @@ public class l001 {
         return ans;
     }
 
+    // Leetcode 1143
+    public static int LCSubseq(String str1, int n, String str2, int m, int[][] dp) {
+        if (n == 0 || m == 0)
+            return 0;
+        if (dp[n][m] != -1)
+            return dp[n][m];
+        if (str1.charAt(n - 1) == str2.charAt(m - 1))
+            return dp[n][m] = LCSubseq(str1, n - 1, str2, m - 1, dp) + 1;
+        else
+            return dp[n][m] = Math.max(LCSubseq(str1, n - 1, str2, m, dp), LCSubseq(str1, n, str2, m - 1, dp));
+    }
+
+    public static int LCSubseq_DP(String str1, int n, String str2, int m, int[][] dp) {
+
+        int N = n, M = m;
+        for (n = 0; n <= N; n++) {
+            for (m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+                if (str1.charAt(n - 1) == str2.charAt(m - 1))
+                    dp[n][m] = dp[n - 1][m - 1] + 1;
+                else
+                    dp[n][m] = Math.max(dp[n - 1][m], dp[n][m - 1]);
+
+            }
+        }
+        return dp[N][M];
+    }
+
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        // int ans = LCSubseq(text1, text1.length(), text2, text2.length(), dp);
+        int ans = LCSubseq_DP(text1, text1.length(), text2, text2.length(), dp);
+
+        display2D(dp);
+        return ans;
+    }
+
+    // Leetcode 1035
+    public static int maxUncrossedLines(int[] A, int[] B) {
+        int N = A.length;
+        int M = B.length;
+
+        int[][] dp = new int[N + 1][M + 1];
+
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+
+                if (n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+                if (A[n - 1] == B[m - 1])
+                    dp[n][m] = dp[n - 1][m - 1] + 1;
+                else
+                    dp[n][m] = Math.max(dp[n - 1][m], dp[n][m - 1]);
+            }
+
+        }
+
+        return dp[N][M];
+    }
+
+    public static int editDistance(String word1, String word2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = ((n == 0) ? m : n);
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        if (word1.charAt(n - 1) == word2.charAt(m - 1))
+            return dp[n][m] = editDistance(word1, word2, n - 1, m - 1, dp);
+        else {
+            int insert_ = editDistance(word1, word2, n, m - 1, dp);
+            int delete_ = editDistance(word1, word2, n - 1, m, dp);
+            int replace_ = editDistance(word1, word2, n - 1, m - 1, dp);
+
+            return dp[n][m] = Math.min(Math.min(insert_, replace_), delete_) + 1;
+        }
+    }
+
+    public static int editDistance_DP(String word1, String word2, int n, int m, int[][] dp) {
+        int N = n;
+        int M = m;
+        for (n = 0; n <= N; n++) {
+            for (m = 0; m <= M; m++) {
+
+                if (n == 0 || m == 0) {
+                    dp[n][m] = n == 0 ? m : n;
+                    continue;
+                }
+                if (word1.charAt(n - 1) == word2.charAt(m - 1))
+                    dp[n][m] = dp[n - 1][m - 1];
+                else
+                    dp[n][m] = Math.min(Math.min(dp[n - 1][m], dp[n][m - 1]), dp[n - 1][m - 1]) + 1;
+            }
+
+        }
+
+        return dp[N][M];
+    }
+
+    public static int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        return editDistance(word1, word2, word1.length(), word2.length(), dp);
+    }
+
+    public static int countPalindromicSubsequence(String str, int i, int j, int[][] dp) {
+        if (i > j)
+            return dp[i][j] = 0;
+
+        if (i == j)
+            return dp[i][j] = 1;
+
+        if (dp[i][j] != 0)
+            return dp[i][j];
+
+        int a = countPalindromicSubsequence(str, i + 1, j - 1, dp);
+        int b = countPalindromicSubsequence(str, i, j - 1, dp);
+        int c = countPalindromicSubsequence(str, i + 1, j, dp);
+
+        if (str.charAt(i) == str.charAt(j))
+            return dp[i][j] = (a + 1) + (b + c - a);
+        else
+            return dp[i][j] = b + c - a;
+    }
+
+    public static int countPalindromicSubsequence_DP(String str, int i, int j, int[][] dp) {
+
+        for (int gap = 0; gap < str.length(); gap++) {
+            for (i = 0, j = gap; j < str.length(); i++, j++) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int a = dp[i + 1][j - 1];// countPalindromicSubsequence(str, i + 1, j - 1, dp);
+                int b = dp[i][j - 1];// countPalindromicSubsequence(str, i, j - 1, dp);
+                int c = dp[i + 1][j];// countPalindromicSubsequence(str, i + 1, j, dp);
+
+                if (str.charAt(i) == str.charAt(j))
+                    dp[i][j] = (a + 1) + (b + c - a);
+                else
+                    dp[i][j] = b + c - a;
+            }
+        }
+
+        return dp[0][str.length() - 1];
+    }
+    //leetcode 940
+    public static int distinctSubseqII(String str) {
+        int mod = (int) 1e9 + 7;
+        str = '$' + str;
+        int n = str.length();
+        long[] dp = new long[n];
+
+        int[] loc = new int[26];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = (2 * dp[i - 1]) % mod;
+            int idx = str.charAt(i) - 'a';
+            
+            if (loc[idx] != -1) {
+                dp[i] = dp[i] % mod - dp[loc[idx] - 1] % mod + mod;
+            }
+
+            loc[idx] = i;
+        }
+
+        return (int) dp[n - 1];
+
+    }
+
+    public static void countPalindromicSubsequence(String str) {
+        int n = str.length();
+        int[][] dp = new int[n][n];
+        // System.out.println(countPalindromicSubsequence(str, 0, n - 1, dp));
+        System.out.println(countPalindromicSubsequence_DP(str, 0, n - 1, dp));
+
+        display2D(dp);
+    }
+
     // ===================================================================================================
 
     public static void String_set() {
-        String str = "effbccbade";
-        int n = str.length();
-        int[][] dp = new int[n][n];
+        // String str = "effbccbade";
+        // int n = str.length();
+        // int[][] dp = new int[n][n];
 
         // longestPalindromeSubstring("effbccbad");
         // System.out.println(longestPlaindromeSubsequence(str, 0, n - 1, dp));
-        System.out.println(longestPlaindromeSubsequence_DP(str, 0, n - 1, dp));
+        // System.out.println(longestPlaindromeSubsequence_DP(str, 0, n - 1, dp));
 
         // pair[][] dp = new pair[n][n];
         // pair ans = longestPlaindromeSubsequence_02(str, 0, n - 1, dp);
         // System.out.println(ans.str + "@" + ans.len);
+
+        // longestCommonSubsequence("AGGTAB", "GXTXAYB");
+        countPalindromicSubsequence("baccbab");
 
         // display2D(dp);
     }
