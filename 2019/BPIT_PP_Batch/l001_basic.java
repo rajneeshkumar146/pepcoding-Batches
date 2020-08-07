2bf6baee240b3fd5f386bc12a43f64bcb69a885b
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-08-07 16:31:09 +0530

    update

--- 2019/BPIT_PP_Batch/l001_basic.java
+++ 2019/BPIT_PP_Batch/l001_basic.java
@@ -8,7 +8,8 @@ public class l001_basic{
     //    int sr=0,sc=0,er=2,ec=2;
     //    System.out.println(mazePath(sr,sc,er,ec,""));
     //    System.out.println(mazePathMulti(sr,sc,er,ec,""));1
-    floodFill();
+    // floodFill();
+       System.out.println(allPermuation_WithoutDuplicate("aaa",""));
     }
 
     public static int mazePath(int sr,int sc,int er,int ec,String ans){
@@ -98,6 +99,80 @@ public class l001_basic{
         // System.out.println(floodFill_(sr,sc,er,ec,vis,dirFour,dirFourS,""));
         System.out.println(floodFillMultiJump_(sr,sc,er,ec,vis,dirFour,dirFourS,""));
     }
+
+    public static int allPermuation(String que,String ans){
+        if(que.length()==0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count=0;
+        for(int i=0;i<que.length();i++){
+            String str=que.substring(0,i)+que.substring(i+1);
+            count+=allPermuation(str,ans + que.charAt(i));
+        }
+
+        return count;
+    }
+
+    public static int allPermuation_WithoutDuplicate(String que,String ans){
+        if(que.length()==0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count=0;
+        boolean[] vis=new boolean[26];
+        for(int i=0;i<que.length();i++){
+            String str=que.substring(0,i)+que.substring(i+1);
+            if(!vis[que.charAt(i)-'a'])
+               count+=allPermuation_WithoutDuplicate(str,ans + que.charAt(i));
+            
+            vis[que.charAt(i)-'a']=true;
+        }
+
+        return count;
+    }
+
+    public List<String> letterCombinations_set2(String digits) {
+        if(digits.length()==0) return new ArrayList<>();
+        String[] codes={".,/?", "@#$%", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz","*+-","&^!~"};
+        
+        List<String> ans=new ArrayList<>();
+        letterCombinations(digits,0,codes,ans,"");
+        return ans;
+    }
+
+    public static int letterCombinations_set2(String digits,int idx,String[] codes,List<String> ans,String res){
+        if(idx==digits.length()){
+            ans.add(res);
+            return 1;
+        }
+        
+        int cidx = (digits.charAt(idx)-'0');
+        String code=codes[cidx];
+        
+        int count=0;
+        for(int i=0;i<code.length();i++){
+            count+=letterCombinations_set2(digits,idx+1,codes,ans,res+code.charAt(i));
+        }
+
+        if(idx<digits.length()-1){
+            cidx = cidx*10 + (digits.charAt(idx+1)-'0');
+            if(cidx>=10 && cidx<=11){
+                code=codes[cidx];
+                for(int i=0;i<code.length();i++){
+                    count+=letterCombinations_set2(digits,idx+2,codes,ans,res+code.charAt(i));
+                }
+            } 
+        }
+        
+        return count;
+    }
+
+    
+
+
 
 
 
