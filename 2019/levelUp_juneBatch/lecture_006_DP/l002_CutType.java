dff286bbb00ec517c39e4e8a3776048f87d2aecb
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-08-07 20:44:52 +0530

    update

---
+++ 2019/levelUp_juneBatch/lecture_006_DP/l002_CutType.java
@@ -0,0 +1,108 @@
+public class l002_CutType{
+
+    public static void display(int[] dp) {
+        for (int ele : dp)
+            System.out.print(ele + " ");
+        System.out.println();
+    }
+
+    public static void display2D(int[][] dp) {
+        for (int[] ar : dp) {
+            display(ar);
+        }
+        System.out.println();
+    }
+
+
+    public static int MCM_Rec(int[] arr,int si,int ei,int[][] dp){
+        if(si+1 == ei) return dp[si][ei] = 0;
+        if(dp[si][ei] != 0) return dp[si][ei]; 
+
+        int myAns=(int)1e8;
+        for(int cut=si+1;cut<ei;cut++){
+            int leftRes=MCM_Rec(arr,si,cut,dp);
+            int rightRes=MCM_Rec(arr,cut,ei,dp);
+            
+            int recRes = leftRes +  arr[si]*arr[cut]*arr[ei]  + rightRes;
+            if(recRes<myAns) myAns = recRes; 
+        }
+
+        return dp[si][ei] = myAns;
+    }
+
+    public static int MCM_DP(int[] arr,int si,int ei,int[][] dp){
+
+        for(int gap = 1; gap < arr.length;gap++){
+            for(si=0,ei=gap;ei<arr.length;si++,ei++){
+                if(si+1 == ei) {
+                    dp[si][ei] = 0;
+                    continue;
+                }
+                int myAns=(int)1e8;
+                for(int cut=si+1;cut<ei;cut++){
+                    int leftRes = dp[si][cut];//MCM_Rec(arr,si,cut,dp);
+                    int rightRes = dp[cut][ei];//MCM_Rec(arr,cut,ei,dp);
+            
+                    int recRes = leftRes +  arr[si]*arr[cut]*arr[ei]  + rightRes;
+                    if(recRes<myAns) myAns = recRes; 
+                }
+
+            dp[si][ei] = myAns;
+            }
+        }
+    
+        return dp[0][arr.length-1];
+    }
+
+    public static int MCM_Rec_02(int[] arr,int si,int ei,int[][] dp,String[][] sdp){
+        if(si+1 == ei) {
+            sdp[si][ei] = ((char)(si+'A')+"");
+            return dp[si][ei] = 0;
+        }
+            
+        if(dp[si][ei] != 0) return dp[si][ei]; 
+
+        dp[si][ei]=(int)1e8;
+        for(int cut=si+1;cut<ei;cut++){
+            int leftRes=MCM_Rec_02(arr,si,cut,dp,sdp);
+            int rightRes=MCM_Rec_02(arr,cut,ei,dp,sdp);
+            
+            int recRes = leftRes +  arr[si] * arr[cut] * arr[ei]  + rightRes;
+            if(recRes < dp[si][ei]) {
+                sdp[si][ei] = "(" + sdp[si][cut] + sdp[cut][ei] + ")";
+                dp[si][ei] = recRes; 
+            }
+        }
+        return dp[si][ei];
+    }
+
+    public static void MCM(){
+        int[] arr={10, 20, 30, 40, 30};
+        int n=arr.length;
+
+        int[][] dp=new int[n][n];
+        String[][] sdp=new String[n][n];
+        
+        // System.out.println(MCM_Rec(arr,0,n-1,dp));
+        // System.out.println(MCM_DP(arr,0,n-1,dp));
+        System.out.println(MCM_Rec_02(arr,0,n-1,dp,sdp));
+        
+        System.out.println(sdp[0][n-1]);
+        display2D(dp);
+        for(String[] s: sdp){
+            for(String e:s) System.out.print(e+" ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        MCM();
+
+    }
+
+
+
+
+
+
+}
