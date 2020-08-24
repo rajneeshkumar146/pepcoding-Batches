d58c74095d97c6dffb23343c20eca4fbbc15d9e4
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-08-24 20:47:33 +0530

    update

--- 2019/levelUp_juneBatch/lecture_007_HashMapAndHeap/questionStack.cpp
+++ 2019/levelUp_juneBatch/lecture_007_HashMapAndHeap/questionStack.cpp
@@ -111,7 +111,7 @@ vector<int> nsor(vector<int> &arr)
     return ans;
 }
 
-vector<int> ngol(vector<int> &arr)
+vector<int> nsol(vector<int> &arr)
 {
     stack<int> st;
     st.push(-1);
@@ -214,39 +214,183 @@ int longestValidParentheses(string s)
 }
 
 //Leetcode 1249
-string minRemoveToMakeValid(string s) {
-        stack<int> st;
-        
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            if(s[i]==')'){
-                if(st.size()!=0) st.pop();
-                else s[i] = '#';
-                
-            }else if(s[i]=='(') st.push(i);
+string minRemoveToMakeValid(string s)
+{
+    stack<int> st;
+
+    int n = s.length();
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == ')')
+        {
+            if (st.size() != 0)
+                st.pop();
+            else
+                s[i] = '#';
         }
-        
-        while(st.size()!=0){
-            s[st.top()]='#';
-            st.pop();
-        }
-        
-        string ans = "";
-        for(int i=0;i<n;i++){
-            if(s[i]!='#') ans+=s[i];
-        }
-        
-        return ans;
-        
-        
+        else if (s[i] == '(')
+            st.push(i);
     }
 
-void solve()
-{
+    while (st.size() != 0)
+    {
+        s[st.top()] = '#';
+        st.pop();
+    }
+
+    string ans = "";
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] != '#')
+            ans += s[i];
+    }
+
+    return ans;
 }
 
-int main()
+//Leetcode 84
+
+int largestRectangleArea_01(vector<int> &heights)
 {
-    solve();
-    return 0;
+    vector<int> sl = nsol(heights);
+    vector<int> sr = nsor(heights);
+
+    int area = 0;
+    for (int i = 0; i < heights.size(); i++)
+    {
+        int w = sr[i] - sl[i] - 1;
+        int h = heights[i];
+
+        area = max(area, w * h);
+    }
+
+    return area;
+}
+
+int largestRectangleArea_02(vector<int> &heights)
+{
+    int n = heights.size();
+    if (n == 0)
+        return 0;
+
+    stack<int> st;
+    st.push(-1);
+    int area = 0;
+    for (int i = 0; i < heights.size(); i++)
+    {
+        while (st.top() != -1 && heights[i] <= heights[st.top()])
+        {
+            int h = heights[st.top()];
+            st.pop();
+            int w = i - st.top() - 1;
+
+            area = max(area, h * w);
+        }
+        st.push(i);
+    }
+
+    while (st.top() != -1)
+    {
+        int h = heights[st.top()];
+        st.pop();
+        int w = n - st.top() - 1;
+
+        area = max(area, h * w);
+    }
+    return area;
+}
+
+int maximalRectangle(vector<vector<char>> &matrix)
+{
+    if (matrix.size() == 0 || matrix[0].size() == 0)
+        return 0;
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    vector<int> heights(m, 0);
+    int area = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            heights[j] = matrix[i][j] == '1' ? heights[j] + 1 : 0;
+        }
+
+        area = max(area, largestRectangleArea_02(heights));
+    }
+
+    return area;
+}
+
+int trap_01(vector<int> &height)
+{
+    int water = 0;
+    int n = height.size();
+    vector<int> left(n,0);
+    vector<int> right(n,0);
+    
+    int prev = 0;
+    for(int i = 0; i < n; i++){
+       left[i] = max(prev,height[i]);
+       prev = left[i];
+    } 
+
+    prev = 0;
+    for(int i =  n - 1;i>=0; i--){
+       right[i] = max(prev,height[i]);
+       prev = right[i];
+    } 
+
+    for(int i=0;i<n;i++){
+        water += min(left[i],right[i])-height[i];
+    }
+
+    return water;
+}
+
+int trap_02(vector<int> &heights)
+{
+    int n = heights.size();
+    if (n == 0)
+        return 0;
+
+    stack<int> st;
+    int water = 0;
+    for (int i = 0; i < heights.size(); i++)
+    {
+        while (st.size()!=0 && heights[st.top()] < heights[i])
+        {
+            int h = heights[st.top()];
+            st.pop();
+            if(st.size()==0) break;
+            int w = i - st.top() - 1;
+
+            water += (min(heights[st.top()],heights[i]) - h) * w;
+        }
+        st.push(i);
+    }
+
+    return water;
+}
+
+int trap_03(vector<int> &heights)
+{
+    int n = heights.size();
+    if (n == 0)
+        return 0;
+    int li = 0, ri = n - 1, lmax = 0, rmax = 0; 
+    int water = 0;
+    while (li<=ri)
+    {
+        lmax = max(lmax, heights[li]);
+        rmax = max(rmax, heights[ri]);
+
+        // water += lmax <= rmax ? lmax - heights[li++] : rmax - heights[ri--];
+        if(lmax <= rmax)
+          water += lmax - heights[li++];
+        else
+          water += rmax - heights[ri--];
+    }
+
+    return water;
 }
