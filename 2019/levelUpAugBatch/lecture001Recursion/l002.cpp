f8ac1c01e9b5d230521b2aaca8f5e68a70a08a87
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-08-31 21:46:18 +0530

    update

--- 2019/levelUpAugBatch/lecture001Recursion/l002.cpp
+++ 2019/levelUpAugBatch/lecture001Recursion/l002.cpp
@@ -60,15 +60,63 @@ int floodfill_jump(int sr, int sc, int er, int ec, string ans)
     return count;
 }
 
+class pathPair
+{
+public:
+    string path;
+    int len;
+
+    pathPair(string path, int len)
+    {
+        this->path = path;
+        this->len = len;
+    }
+};
+
+pathPair floodfillShortestPath_jump(int sr, int sc, int er, int ec)
+{
+    if (sr == er && sc == ec)
+    {
+        pathPair p("", 0);
+        return p;
+    }                                       
+
+    int count = 0;
+    vis[sr][sc] = 1;
+    pathPair ans("", 1e8);
+    for (int jump = 1; jump <= max(er, ec); jump++)
+    {
+        for (int d = 0; d < dir.size(); d++)
+        {
+            int r = sr + jump * dir[d][0];
+            int c = sc + jump * dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < vis.size() && c < vis[0].size() && vis[r][c] == 0)
+            {
+                pathPair recAns = floodfillShortestPath_jump(r, c, er, ec);
+                if (recAns.len + 1 < ans.len)
+                {
+                    ans.len = recAns.len + 1;
+                    ans.path = dirS[d] + to_string(jump) + " " + recAns.path;
+                }
+            }
+        }
+    }
+
+    vis[sr][sc] = 0;
+    return ans;
+}
+
 void pathSet()
 {
     int n = 3;
     int m = 3;
     vis.resize(n, vector<int>(m, 0));
     // cout << floodfill(0, 0, 2, 2, "") << endl;
-    cout << floodfill_jump(0, 0, 2, 2, "") << endl;
+    // cout << floodfill_jump(0, 0, 2, 2, "") << endl;
+    pathPair ans = floodfillShortestPath_jump(0, 0, 2, 2);
+    cout << ans.path << " -> " << ans.len << endl;
 }
-
 
 void solve()
 {
--- 2019/levelUpAugBatch/lecture001Recursion/l002.java
+++ 2019/levelUpAugBatch/lecture001Recursion/l002.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class l002{
 
@@ -65,8 +66,203 @@ public class l002{
         return count;
     }
 
+    static int[] xMove = {2, 1, -1, -2, -2, -1, 1, 2}; 
+    static int[] yMove = {1, 2, 2, 1, -1, -2, -2, -1}; 
+    
+    public static boolean KnightTour(int[][] board,int sr,int sc,int steps){
+      
+      board[sr][sc] = steps;
+      
+      if(steps == 63){
+        return true;
+      }
+
+      for(int d = 0 ; d < xMove.length;d++){
+        int r = sr + xMove[d];
+        int c = sc + yMove[d];
+
+        if(r>=0 && c>=0 && r < 8 && c < 8 &&  board[r][c] == -1){
+          boolean res = KnightTour(board,r,c,steps + 1);
+          if(res) return true;
+        }
+      }
+
+      board[sr][sc] = -1;
+      return false;
+    }
+
+    public static void KnightTour(){
+      int[][] board = new int[8][8];
+      for(int[] a : board) Arrays.fill(a,-1);
+
+      KnightTour(board,0,0,0);
+      for(int[] a : board){
+        for(int ele: a){
+          System.out.print(ele + " ");
+        }
+        System.out.println();
+      }
+    }
+
+    //CoinChange.==============================================================
+
+    public static int coinChangePermutation_01(int[] coins,int tar,String ans){
+      if(tar==0){
+        System.out.println(ans);
+        return 1;
+      }
+       
+      int count = 0;
+      for(int i = 0;i<coins.length;i++){
+        if(tar - coins[i]>=0){
+          count += coinChangePermutation_01(coins,tar-coins[i],ans  + coins[i]);
+        }
+      }
+
+      return count;
+    }
+
+    public static int coinChangeCombination_01(int[] coins,int idx,int tar,String ans){
+      if(tar==0){
+        System.out.println(ans);
+        return 1;
+      }
+       
+      int count = 0;
+      for(int i = idx;i<coins.length;i++){
+        if(tar - coins[i]>=0){
+          count += coinChangeCombination_01(coins,i,tar-coins[i],ans  + coins[i]);
+        }
+      }
+
+      return count;
+    }
+
+    public static int coinChangeCombinationSingle_01(int[] coins,int idx,int tar,String ans){
+      if(tar==0){
+        System.out.println(ans);
+        return 1;
+      }
+       
+      int count = 0;
+      for(int i = idx;i<coins.length;i++){
+        if(tar - coins[i]>=0){
+          count += coinChangeCombinationSingle_01(coins,i+1,tar-coins[i],ans  + coins[i]);
+        }
+      }
+
+      return count;
+    }
+
+    public static int coinChangePermutationSingle_01(int[] coins,int tar,boolean[] vis,String ans){
+      if(tar==0){
+        System.out.println(ans);
+        return 1;
+      }
+       
+      int count = 0;
+      for(int i = 0;i<coins.length;i++){
+        if(!vis[i] && tar - coins[i]>=0){
+          vis[i] = true;
+          count += coinChangePermutationSingle_01(coins,tar-coins[i],vis,ans  + coins[i]);
+          vis[i] = false;
+        }
+      }
+
+      return count;
+    }
+
+    public static int coinChangeCombinationSingleSubseq_01(int[] coins,int idx,int tar,String ans){
+      if(tar==0 || idx >= coins.length){
+        if(tar==0){
+          System.out.println(ans);
+          return 1;
+        }
+        return 0;
+      }
+       
+      int count = 0;
+      if(tar - coins[idx]>=0)
+        count += coinChangeCombinationSingleSubseq_01(coins,idx+1,tar-coins[idx],ans  + coins[idx]);
+      
+      count += coinChangeCombinationSingleSubseq_01(coins,idx+1,tar,ans);
+  
+      return count;
+    }
+
+    public static int coinChangeCombinationSubseq_01(int[] coins,int idx,int tar,String ans){
+      if(tar==0 || idx >= coins.length){
+        if(tar==0){
+          System.out.println(ans);
+          return 1;
+        }
+        return 0;
+      }
+       
+      int count = 0;
+      if(tar - coins[idx]>=0)
+        count += coinChangeCombinationSubseq_01(coins,idx,tar-coins[idx],ans  + coins[idx]);
+      
+      count += coinChangeCombinationSubseq_01(coins,idx+1,tar,ans);
+  
+      return count;
+    }
+
+    public static int coinChangePermutationSubSeq_01(int[] coins,int idx,int tar,String ans){
+      if(tar==0 || idx >= coins.length){
+        if(tar==0){
+          System.out.println(ans);
+          return 1;
+        }
+        return 0;
+      }
+       
+      int count = 0;
+      if(tar - coins[idx]>=0)
+        count += coinChangePermutationSubSeq_01(coins,idx+1,tar-coins[idx],ans  + coins[idx]);
+      
+      count += coinChangePermutationSubSeq_01(coins,idx+1,tar,ans);
+  
+      return count;
+    }
+
+    public static int coinChangePermutationSingleSubSeq_01(int[] coins,int idx,int tar,String ans){
+      if(tar==0 || idx >= coins.length){
+        if(tar==0){
+          System.out.println(ans);
+          return 1;
+        }
+        return 0;
+      }
+       
+      int count = 0;
+      if(tar - coins[idx]>=0)
+        count += coinChangePermutationSingleSubSeq_01(coins,idx+1,tar-coins[idx],ans  + coins[idx]);
+      
+      count += coinChangePermutationSingleSubSeq_01(coins,idx+1,tar,ans);
+  
+      return count;
+    }
 
 
+
+
+
+    public static void coinChange(){
+      int[] coins = {2,3,5,7};
+      int tar = 10;
+      boolean[] vis = new boolean[4];
+      // System.out.println(coinChangePermutation_01(coins,tar,""));
+      // System.out.println(coinChangeCombination_01(coins,0,tar,""));
+      System.out.println(coinChangeCombinationSingle_01(coins,0,tar,""));
+      // System.out.println(coinChangePermutationSingle_01(coins,tar,vis,""));
+     
+
+      System.out.println("=========================");
+
+      System.out.println(coinChangeCombinationSingleSubseq_01(coins,0,tar,""));
+    }
+    
     public static void pathSet(){
         // System.out.println(mazePath_HVD(0,0,2,2));
         // System.out.println(mazePath_HVD_01(0,0,2,2,""));
@@ -74,7 +270,9 @@ public class l002{
     }
 
     public static void solve(){
-        pathSet();
+        // pathSet();
+        // KnightTour();
+        coinChange();
     }
 
     public static void main(String[] args){
