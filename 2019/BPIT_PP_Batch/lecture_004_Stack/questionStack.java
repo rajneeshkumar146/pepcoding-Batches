4baa160a1f3333f08b2ae0e10f92d4b978b406b4
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-09-05 13:56:12 +0530

    update

--- 2019/BPIT_PP_Batch/lecture_004_Stack/questionStack.java
+++ 2019/BPIT_PP_Batch/lecture_004_Stack/questionStack.java
@@ -273,5 +273,118 @@ public class questionStack{
     }
 
 
+    public int trap(int[] height) {
+      if(height.length == 0) return 0;
+      int n = heights.length;
 
+      int[] lmax = new int[n];
+      int[] rmax = new int[n];
+      
+      int prev = 0;
+      for(int i=0;i<n;i++){
+          lmax[i]= Math.max(prev,height[i]);
+          prev = lmax[i];
+      }
+
+      prev = 0;
+      for(int i=n-1;i>=0;i--){
+          rmax[i]= Math.max(prev,height[i]);
+          prev = rmax[i];
+      }
+
+
+      int water = 0;
+      int prev = 0;
+      for(int i=0;i<n;i++){
+          h = Math.min(lmax[i],rmax[i]);
+          water += h-height[i];
+      }
+
+      return water;
+    }
+
+    public int trap(int[] height) {
+        if(height.length == 0) return 0;
+        int n = height.length;
+  
+        Stack<Integer> st = new Stack<>();
+        int water = 0;
+        for(int i=0;i<n;i++){
+            while(st.size()!=0 && height[st.peek()] <= height[i]){
+                int h = height[st.peek()];
+                st.pop();
+
+                if(st.size()==0) break;
+
+                int W = (i - st.peek() - 1);
+                
+                int h1 = height[i];
+                int h2 = height[st.peek()];
+                int H = Math.min(h1,h2) - h;
+                
+                water += H * W;   
+            }
+
+            st.push(i);
+        }
+
+        return water;
+    }
+
+    public int trap(int[] height) {
+        if(height.length == 0) return 0;
+        int n = height.length;
+  
+        int water = 0, lmax = 0,rmax = 0, li = 0, ri = n-1;
+        while(li < ri){
+            lmax = Math.max(lmax,height[li]);
+            rmax = Math.max(rmax,height[ri]);
+
+            if(lmax <= rmax) water += lmax - height[li++];
+            else water += rmax - height[ri--];
+        }
+
+        return water;
+    }
+
+    class MinStack {
+        Stack<Long> st = new Stack<>();
+        long minSf = 0; 
+        public MinStack() {
+            
+        }
+        
+        public void push(int val) {
+            long x= val;
+            if(st.size()==0){
+                st.push(x);
+                minSf = x;
+                return;
+            }
+
+            if(x < minSf){
+                st.push( ( x - minSf ) + x);
+                minSf = x;
+            }else{
+                st.push(x);
+            }
+        }
+        
+        public void pop() {   
+            if(st.peek() < minSf){
+                minSf = (minSf - st.peek()) + minSf;
+            }
+
+            st.pop();
+        }
+        
+        public int top() {
+            if(st.peek() < minSf) return (int)minSf;
+            return (int)((long)st.peek());
+        }
+        
+        public int getMin() {
+            return (int)minSf;
+        }
+    }
 }
