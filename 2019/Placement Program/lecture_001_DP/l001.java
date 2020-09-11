43e7f4e34783960d200023311591783bddbdec00
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-09-11 14:31:09 +0530

    update

--- 2019/Placement Program/lecture_001_DP/l001.java
+++ 2019/Placement Program/lecture_001_DP/l001.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Arrays;
 public class l001{
 
     public static void print(int[] arr){
@@ -262,22 +264,242 @@ public class l001{
         return dp[0][0];
     }
 
-    public static void mazePath(){
-        int n = 3;
-        int m = 3;
+    public static int boardPath(int si,int ei,int[] dp){
+        if(si==ei){
+            return dp[si] = 1;
+        }
+        if(dp[si]!=0) return dp[si];
 
-        int[][] dp = new int[n][m];
-        int[][] dir = {{1,0},{0,1},{1,1}};
+        int count=0;
+        for(int dice = 1;dice <= 6 && si+dice<=ei ; dice++){
+            count+=boardPath(si+dice,ei,dp);
+        }
+
+        return dp[si] = count;
+    }
+
+    public static int boardPath_DP(int si,int ei,int[] dp){ 
+        for(si = ei; si>=0;si--){
+            if(si==ei){
+                dp[si] = 1;
+                continue;
+            }
+
+            int count=0;
+            for(int dice = 1;dice <= 6 && si+dice<=ei ; dice++){
+                count += dp[si+dice];
+            }
+    
+            dp[si] = count;
+        }
+
+        return dp[0];
+    }
+
+    public static int boardPath_Opti(int si,int ei){ 
+        LinkedList<Integer> ll = new LinkedList<>();
+        for(si = ei; si>=0;si--){
+            if(si >= ei-1){
+                ll.addFirst(1);
+                continue;
+            }
+
+            if(ll.size()<=6) 
+              ll.addFirst(2*ll.getFirst());
+            else
+               ll.addFirst((2 * ll.getFirst()) - ll.removeLast());
+        }
+
+        return ll.getFirst();
+    }
+
+    public static int boardPath_Opti2(int ei){ 
+        int[] ans = {1,1,2,4,8,16,32};
+        int idx = 6;
+        if(ei<=6) return ans[ei];
+
+        for(int i=7;i<=ei;i++){
+            int val = 2*ans[idx];
+            idx = (idx+1) % 7;
+            val -= ans[idx];
+
+            ans[idx] = val;     
+        }
+
+        return ans[idx];
+    }
+
+    //Leetcode 64
+    public int minPathSum(int[][] grid,int sr,int sc,int[][] dp) {
+        if(sr == grid.length - 1 && sc == grid[0].length - 1){
+            return dp[sr][sc] = grid[sr][sc];
+        }
+
+        if(dp[sr][sc]!=0) return dp[sr][sc];
+        
+        int minAns = (int) 1e9;
+        if(sr + 1 < grid.length) minAns = Math.min(minAns, minPathSum(grid,sr+1,sc,dp));
+        if(sc + 1 < grid[0].length) minAns = Math.min(minAns, minPathSum(grid,sr,sc+1,dp));
+
+        return dp[sr][sc]=minAns;
+    }
+
+    
+    public int minPathSum_DP(int[][] grid,int sr,int sc,int[][] dp) {
+        for(sr = grid.length-1;sr>=0;sr--){
+            for(sc = grid[0].length-1;sc>=0;sc--){
+                if(sr == grid.length - 1 && sc == grid[0].length - 1){
+                    dp[sr][sc] = grid[sr][sc];
+                }
+                
+                int minAns = (int) 1e9;
+                if(sr + 1 < grid.length) minAns = Math.min(minAns, dp[sr+1][sc]);
+                if(sc + 1 < grid[0].length) minAns = Math.min(minAns,dp[sr][sc+1]);
+        
+                dp[sr][sc]=minAns;
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        int ans= minPathSum(grid,0,0,dp);         
+        return ans;
+    }
+
+    public static int goldMine(int[][] grid,int sr,int sc,int[][] dp,int[][] dir){
+        if(sc==grid[0].length-1){
+            return dp[sr][sc] = grid[sr][sc];
+        }
+
+        if(dp[sr][sc]!=0) return dp[sr][sc];
+
+        int maxAns = 0;
+        for(int d=0;d<dir.length;d++){
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+            
+            if(r>=0 && c>=0 && r< grid.length && c<grid[0].length)
+              maxAns = Math.max(maxAns,goldMine(grid,r,c,dp,dir));
+        }
+
+        return dp[sr][sc] = maxAns + grid[sr][sc];
+    }
+
+    public static void goldMineP_DP(int[][] grid,int sr,int sc,int[][] dp,int[][] dir){
+        for(sr = grid.length-1;sr>=0;sr--){
+            for(sc = grid[0].length-1;sc>=0;sc--){
+                if(sr == grid.length-1 && sc == grid[0].length-1){
+                     dp[sr][sc] = grid[sr][sc];
+                     continue;
+                }
+        
+                int maxAns = 0;
+                for(int d=0;d<dir.length;d++){
+                    int r = sr + dir[d][0];
+                    int c = sc + dir[d][1];
+                    
+                    if(r>=0 && c>=0 && r< grid.length && c<grid[0].length)
+                      maxAns = Math.max(maxAns,dp[r][c]);
+                }
+        
+                dp[sr][sc] = maxAns + grid[sr][sc];
+            }
+        }
+
+        int maxRes =0;
+        for(int i=0;i<grid.length;i++)
+          maxRes = Math.max(maxRes, dp[i][0]);
+
+        System.out.println(maxRes);
+        
+    }
+
+
+
+    public static void goldMine(int[][] grid){
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n][m]; 
+        
+        int maxAns = 0;
+        int[][] dir = {{-1,1},{0,1},{1,1}};
+
+        for(int i=0;i<n;i++)
+          maxAns = Math.max(maxAns, goldMine(grid,i,0,dp,dir));
+        
+        System.out.println(maxAns);
+    }
+
+    // https://www.geeksforgeeks.org/count-number-of-ways-to-partition-a-set-into-k-subsets/
+
+    public static int countWays(int n,int k,int[][] dp){
+        // if(n < k) return 0; 
+        if(n==k || k==1){
+            return dp[n][k] = 1;
+        }
+
+        if(dp[n][k] != -1) return dp[n][k];
+
+        int ownSet = countWays(n-1,k-1,dp);
+        int partOfSet = countWays(n-1,k,dp) * k;
+
+        return dp[n][k] = ownSet + partOfSet;
+    }
+
+    
+    public static int countWays_DP(int N,int K,int[][] dp){ 
+        for(int k=1;k<=K;k++){
+            for(int n = k;n<=N;n++){
+                if(n==k || k==1){
+                    dp[n][k] = 1;
+                    continue;
+                }
+        
+                int ownSet = dp[n-1][k-1];//countWays(n-1,k-1,dp);
+                int partOfSet = dp[n-1][k]* k;
+        
+                dp[n][k] = ownSet + partOfSet;
+            }
+        }
+
+        return dp[N][K];
+    }
+
+    //https://practice.geeksforgeeks.org/problems/mobile-numeric-keypad/0
+
+    public static void mazePath(){
+        // int n = 3;
+        // int m = 3;
+
+        // int[][] dp = new int[n][m];
+        // int[][] dir = {{1,0},{0,1},{1,1}};
 
         // System.out.println(mazePath_HVD(0,0,n-1,m-1,dp,dir));
         // System.out.println(mazePath_HVD_Multi(0,0,n-1,m-1,dp,dir));
 
         // System.out.println(mazePath_HVD_DP(0,0,n-1,m-1,dp,dir));
-        System.out.println(mazePath_HVD_Multi_DP(0,0,n-1,m-1,dp,dir));
+        // System.out.println(mazePath_HVD_Multi_DP(0,0,n-1,m-1,dp,dir));
 
+        // int n = 1;
+        // int[] dp = new int[n+1];
+        // System.out.println(boardPath(0,n,dp));
+        // System.out.println(boardPath_DP(0,n,dp));
+        // System.out.println(boardPath_Opti2(n));
 
+        int n = 5;
+        int k = 3;
+        int[][] dp = new int[n+1][k+1];
+        for(int[] a : dp) Arrays.fill(a,0);
+        System.out.println(countWays_DP(n,k,dp));
+
+        // print(dp);
         print2D(dp);
     }
+
+
     
     public static void twoPointerSet(){
         // fibo();
