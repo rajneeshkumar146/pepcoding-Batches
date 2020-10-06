991a7b5dfdace78643c4563e102aa8c3408f15e3
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-10-06 11:48:53 +0530

    update

--- 2019/BPIT_PP_Batch/lecture_005_DP/l003_CutType.java
+++ 2019/BPIT_PP_Batch/lecture_005_DP/l003_CutType.java
@@ -68,10 +68,146 @@ public class l003_CutType{
         print2D(dp);
     }
 
-    // for you -> https://www.geeksforgeeks.org/minimum-maximum-values-expression/
+    //https://www.geeksforgeeks.org/minimum-maximum-values-expression/
+
+    public static class minMaxPair{
+        int minVal = (int) 1e8;
+        int maxVal = 0;
+
+        minMaxPair(int minVal,int maxVal){
+            this.minVal = minVal;
+            this.maxVal = maxVal;
+        }
+
+        minMaxPair(){
+
+        }
+
+        public String toString(){
+            return "(" + this.minVal + ", " + this.maxVal +  ")";
+        }
+    }
+
+    
+    public static int evaluate(char ch,int v1, int v2){
+        if(ch == '+') return v1 + v2;
+        else if(ch == '+') return v1 - v2;    
+        else return v1 * v2;
+    }
+
+    public static minMaxPair minMaxValue(String str,int si,int ei,minMaxPair[][] dp){
+        if(si == ei){
+            int val = str.charAt(si) - '0';
+            return dp[si][ei] = new minMaxPair(val,val);
+        }
+
+        if(dp[si][ei] != null) return dp[si][ei];
+        
+        
+        minMaxPair myAns = new minMaxPair();
+
+        for(int cut = si + 1; cut < ei; cut+=2){
+            minMaxPair leftTree = minMaxValue(str,si , cut - 1,dp);
+            minMaxPair rightTree = minMaxValue(str,cut + 1, ei,dp);
+
+            
+            char ch = str.charAt(cut);
+            myAns.minVal = Math.min(myAns.minVal,evaluate(ch,leftTree.minVal,rightTree.minVal));
+            myAns.maxVal = Math.max(myAns.maxVal,evaluate(ch,leftTree.maxVal,rightTree.maxVal));
+        }
+
+        return dp[si][ei] = myAns;
+    }
+
+    public static minMaxPair minMaxValue_DP(String str,int SI,int EI,minMaxPair[][] dp){
+        int n = str.length();
+        for(int gap = 0 ; gap < n; gap++){
+            for(int si = 0, ei = gap; ei < n ; si++,ei++){
+                if(si == ei){
+                    int val = str.charAt(si) - '0';
+                    dp[si][ei] = new minMaxPair(val,val);
+                    continue;
+                }
+                
+                minMaxPair myAns = new minMaxPair();
+        
+                for(int cut = si + 1; cut < ei; cut+=2){
+                    minMaxPair leftTree = dp[si][cut-1];//minMaxValue(str,si , cut - 1,dp);
+                    minMaxPair rightTree = dp[cut+1][ei];//minMaxValue(str,cut + 1, ei,dp);
+        
+                    
+                    char ch = str.charAt(cut);
+                    myAns.minVal = Math.min(myAns.minVal,evaluate(ch,leftTree.minVal,rightTree.minVal));
+                    myAns.maxVal = Math.max(myAns.maxVal,evaluate(ch,leftTree.maxVal,rightTree.maxVal));
+                }
+        
+                dp[si][ei] = myAns;
+            }
+        }
+
+        return dp[SI][EI];
+    }
+
+    public static minMaxPair evalCombination(minMaxPair p1,minMaxPair p2,char operator){
+
+        int a = evaluate(operator,p1.minVal,p2.minVal);
+        int b = evaluate(operator,p1.minVal,p2.maxVal);
+        int c = evaluate(operator,p1.maxVal,p2.minVal);
+        int d = evaluate(operator,p1.maxVal,p2.maxVal);
+
+        minMaxPair p = new minMaxPair();
+        p.minVal = Math.min(Math.min(a,b),Math.min(c,d));
+        p.maxVal = Math.max(Math.max(a,b),Math.max(c,d));
+
+        return p;
+    }
+
+    public static minMaxPair minMaxValue_02(int[] numArr,char[] chArr,int si,int ei,minMaxPair[][] dp){
+        if(si == ei){
+            int val = numArr[si];
+            return dp[si][ei] = new minMaxPair(val,val);
+        }
+
+        if(dp[si][ei] != null) return dp[si][ei];
+        
+        minMaxPair myAns = new minMaxPair();
+
+        for(int cut = si; cut < ei; cut++){
+            minMaxPair leftTree = minMaxValue(str,si , cut,dp);
+            minMaxPair rightTree = minMaxValue(str,cut + 1, ei,dp);
+
+            char ch = chArr[cut];
+            minMaxPair p = evalCombination(operator,leftTree,rightTree);
+
+            myAns.minVal = Math.min(myAns.minVal, p.minVal);
+            myAns.maxVal = Math.max(myAns.maxVal, p.maxVal);
+        }
+
+        return dp[si][ei] = myAns;        
+    }
+
+
+
+    public static void minMaxValue(){
+        String str = "1+2*3+4*5+2*3+3+3+3*3*8*7";
+
+        int n = str.length();
+        minMaxPair[][] dp = new minMaxPair[n][n];
+        minMaxPair ans = minMaxValue(str,0,n-1,dp);
+
+        System.out.println(ans.minVal + " , " + ans.maxVal);
+
+        for(minMaxPair[] d : dp){
+            for(minMaxPair e: d){
+                System.out.print(e);
+            }
+            System.out.println();
+        }
+    }
 
     public static void solve(){
-        mcm();
+        // mcm();
+        minMaxValue();
     }
 
     public static void main(String[] args){
