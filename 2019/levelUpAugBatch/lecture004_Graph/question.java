1bec7d6bec0162a04c960fc9e8660b7eacd91391
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-10-11 15:23:53 +0530

    update

--- 2019/levelUpAugBatch/lecture004_Graph/question.java
+++ 2019/levelUpAugBatch/lecture004_Graph/question.java
@@ -175,6 +175,7 @@ public class question{
     }
 }
 
+//1091
 public int shortestPathBinaryMatrix(int[][] grid) {
     if(grid.length == 0 ||  grid[0].length == 0) return -1;
    
@@ -216,3 +217,232 @@ public int shortestPathBinaryMatrix(int[][] grid) {
 
     return -1;
 }
+
+// 286 Solution 1
+public void wallsAndGates(int[][] rooms) {
+    if(rooms.length == 0 || rooms[0].length == 0) return;
+    int n = rooms.length;
+    int m = rooms[0].length;
+    
+    LinkedList<Integer> que = new LinkedList<>();
+    
+    for(int i = 0;i<n;i++){
+        for(int j = 0; j<m;j++){
+            if(rooms[i][j] == 0){
+                que.addLast(i*m + j);
+            }
+        }
+    }
+    
+    int level = 0;
+    int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+
+    while(que.size()!=0){
+        int size = que.size();
+        while(size-->0){
+            int idx = que.removeFirst();
+            int r = idx / m;
+            int c = idx % m;
+            
+            for(int d = 0;d< 4;d++){
+                
+                int x = r + dir[d][0];
+                int y = c + dir[d][1];
+                
+                if(x>=0 && y>=0 && x< n && y<m && rooms[x][y] == 2147483647 ){
+                    rooms[x][y] = level + 1; 
+                    que.addLast(x*m+y);
+                }
+            }
+        }
+        level++;
+    }   
+}
+
+//286 Solution 2
+public void wallsAndGates(int[][] rooms) {
+    if(rooms.length == 0 || rooms[0].length == 0) return;
+    int n = rooms.length;
+    int m = rooms[0].length;
+    
+    LinkedList<Integer> que = new LinkedList<>();
+    boolean[][] vis = new boolean[n][m];
+    
+    for(int i = 0;i<n;i++){
+        for(int j = 0; j<m;j++){
+            if(rooms[i][j] == 0){
+                que.addLast(i*m + j);
+                vis[i][j] = true;
+            }
+        }
+    }
+    
+    int level = 0;
+    int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+
+    while(que.size()!=0){
+        int size = que.size();
+        while(size-->0){
+            int idx = que.removeFirst();
+            int r = idx / m;
+            int c = idx % m;
+            
+            rooms[r][c] = level;
+            for(int d = 0;d< 4;d++){
+                
+                int x = r + dir[d][0];
+                int y = c + dir[d][1];
+                
+                if(x>=0 && y>=0 && x< n && y<m && !vis[x][y] && rooms[x][y] != -1){
+                    vis[x][y] = true;
+                    que.addLast(x*m+y);
+                }
+            }
+        }
+        level++;
+    }   
+}
+
+//Leetcode 994
+public int orangesRotting(int[][] grid) {
+    if(grid.length==0 || grid[0].length==0) return 0;
+    
+    int n = grid.length;
+    int m = grid[0].length;
+    
+    int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+    LinkedList<Integer> que = new LinkedList<>();
+    int freshOranges = 0;
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<m;j++){
+            if(grid[i][j] == 2) que.addLast(i*m+j);
+            if(grid[i][j] == 1) freshOranges++;
+        }
+    }
+
+    if(freshOranges == 0) return 0;
+    int time = 0;
+    while(que.size() != 0){
+        int size = que.size();
+        while(size-->0){
+            int idx = que.removeFirst();
+            int r = idx / m;
+            int c = idx % m;
+
+            for(int d = 0; d < 4; d++){
+                int x = r + dir[d][0];
+                int y = c + dir[d][1];
+
+                if(x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == 1){
+                    grid[x][y] = 2;
+                    freshOranges--;
+                    que.addLast(x * m + y);
+                    if(freshOranges == 0) return time + 1;
+                }
+            }
+        }
+        time++;
+    }
+
+    return -1;
+}
+
+
+//Leetcode 296
+public void BFS(int i,int j,int[][] grid,int[][] dir,int[][] dis){
+    int n = grid.length;
+    int m = grid[0].length;
+    
+    boolean[][] vis = new boolean[n][m];
+    LinkedList<Integer> que = new LinkedList<>();
+    que.addLast(i*m+j);
+    vis[i][j] = true;
+    
+    int level = 0;
+    while(que.size() != 0){
+        int size = que.size();
+        while(size-- > 0){
+            int idx = que.removeFirst();
+            int r = idx / m;
+            int c = idx % m;
+            
+            dis[r][c] += level;
+            
+            for(int d = 0;d < 4; d++){
+                int x = r + dir[d][0];
+                int y = c + dir[d][1];
+                
+                if(x >= 0 && y >= 0 && x < n && y < m && !vis[x][y]){
+                    vis[x][y] = true;
+                    que.addLast(x * m + y);
+                }
+            }
+        }
+        
+        level++;
+    }
+}
+
+public int minTotalDistance(int[][] grid) {
+   if(grid.length == 0 || grid[0].length == 0) return 0; 
+    int n = grid.length;
+    int m = grid[0].length;
+    
+    int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    int[][] dis = new int[n][m];
+    
+    for(int i = 0;i<n;i++){
+        for(int j = 0; j<m;j++){
+            if(grid[i][j] == 1){
+                BFS(i,j,grid,dir,dis);            
+            }
+        }
+    }
+    
+    int ans = (int)1e8;
+    for(int i = 0;i<n;i++){
+        for(int j = 0; j<m;j++){
+            ans = Math.min(ans , dis[i][j]);
+        }
+    }
+    
+    return ans;
+}
+
+public int getInter(ArrayList<Integer> list){
+    Collections.sort(list);
+    
+    int res = 0;
+    int i = 0;
+    int j = list.size() - 1;
+    while( i < j ){
+        res += list.get(j--) - list.get(i++);
+    }
+    
+    return res;
+    
+}
+
+public int minTotalDistance(int[][] grid) {
+   if(grid.length == 0 || grid[0].length == 0) return 0; 
+    int n = grid.length;
+    int m = grid[0].length;
+    
+    ArrayList<Integer> xpoints = new ArrayList<>();
+    ArrayList<Integer> ypoints = new ArrayList<>();
+    
+    for(int i = 0;i<n;i++){
+    for(int j = 0; j<m;j++){
+        if(grid[i][j] == 1){
+            xpoints.add(i);
+            ypoints.add(j);
+        }
+     }
+   }
+    
+    return getInter(xpoints) + getInter(ypoints);
+}
+
+// Leetcode 815
+
