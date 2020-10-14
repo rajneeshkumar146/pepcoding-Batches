507642d6b9b4944aaf1fec49a3289b06f42a73fd
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-10-14 21:20:19 +0530

    update

--- 2019/levelUpAugBatch/lecture004_Graph/l002_Directional.java
+++ 2019/levelUpAugBatch/lecture004_Graph/l002_Directional.java
@@ -73,4 +73,42 @@ public class l002_Directional{
             System.out.println(ans);
         }
     }
+
+    // Topo DFS Cycle.==============================================
+
+    // try to find a cycle, is found return true otherwise return false.
+    public static boolean TopoDFS(int src,int[] vis,ArrayList<Integer> ans){
+        vis[src] = 1;
+        for(Integer e : graph[src]){
+            if(vis[e] == 0)
+               if(TopoDFS(e,vis,ans)) return true;
+            else if(vis[e] == 1) return true;
+        }
+
+        ans.add(src);
+        vis[src] = 2;
+        return  false;
+    }
+
+    // 0 -> not visited, 1 -> part of path, 2 -> visited and not a part of path.
+    public static void TopoDFS(){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] vis = new int[N];
+
+        boolean isCycleFound = false;
+        for(int i = 0;i<N;i++){
+            if(vis[i] == 0){
+                if(TopoDFS(i,vis,ans)){
+                    isCycleFound = true;
+                    break;
+                }
+            }
+        }
+
+        if(!isCycleFound) System.out.println(ans);
+        else System.out.println("Cycle"); 
+    }
+
+
+
 }
--- 2019/levelUpAugBatch/lecture004_Graph/question.java
+++ 2019/levelUpAugBatch/lecture004_Graph/question.java
@@ -446,3 +446,63 @@ public int minTotalDistance(int[][] grid) {
 
 // Leetcode 815
 
+
+//Leetcode 207
+public boolean canFinish(int N, int[][] arr){
+    ArrayList<Integer>[] graph = new ArrayList[N];
+    for(int i=0;i<N;i++) graph[i] = new ArrayList<>();
+    
+    int[] indegree = new int[N];
+    for(int[] a: arr){
+        indegree[a[1]]++;
+        graph[a[0]].add(a[1]);
+    }
+    
+    LinkedList<Integer> que = new LinkedList<>();
+    for(int i=0;i<N;i++) if(indegree[i]==0) que.addLast(i);
+
+    int count = 0;
+    while(que.size()!=0){
+        int vtx = que.removeFirst();
+        count++;
+        for(int e : graph[vtx]){
+            if(--indegree[e] == 0) que.addLast(e);
+        }
+    }
+    
+    
+    return count == N;        
+}
+
+
+//Leetcode 210
+public int[] findOrder(int N, int[][] arr) {
+    ArrayList<Integer>[] graph = new ArrayList[N];
+     for(int i=0;i<N;i++) graph[i] = new ArrayList<>();
+     
+     int[] indegree = new int[N];
+     for(int[] a: arr){
+         indegree[a[1]]++;
+         graph[a[0]].add(a[1]);
+     }
+     
+     LinkedList<Integer> que = new LinkedList<>();
+     for(int i=0;i<N;i++) if(indegree[i]==0) que.addLast(i);
+
+     int[] ans = new int[N];
+     int idx = N - 1;
+     while(que.size()!=0){
+         int vtx = que.removeFirst();
+         ans[idx--] = vtx;
+         
+         for(int e : graph[vtx]){
+             if(--indegree[e] == 0) que.addLast(e);
+         }
+     }
+     
+     
+     if(idx == -1) return ans;
+     return new int[0];
+ }
+
+
