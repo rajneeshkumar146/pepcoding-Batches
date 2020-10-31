5b5a244fb236c92d7af1ecd7e225b239434c8ba3
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-10-31 15:11:27 +0530

    update

--- 2019/BPIT_PP_Batch/lecture_005_DP/questions.java
+++ 2019/BPIT_PP_Batch/lecture_005_DP/questions.java
@@ -32,3 +32,136 @@ public int numWays(int n, int k) {
     return dp[n];
 }
 
+// 121
+public int maxProfit(int[] arr) {
+    if(arr.length==0) return 0;
+
+    int T0 = 0;
+    int T1 = -(int)1e9;
+
+    for(int val : arr){
+        T0 = Math.max(T0,T1 + val);
+        T1 = Math.max(T1,0 - val);
+    }
+
+    return T0;
+}
+
+//122
+public int maxProfit(int[] arr) {
+    if(arr.length==0) return 0;
+
+    int T0 = 0;
+    int T1 = -(int)1e9;
+
+    for(int val : arr){
+        T0 = Math.max(T0,T1 + val);
+        T1 = Math.max(T1,T0 - val);
+    }
+
+    return T0;        
+}
+
+//309
+public int maxProfit(int[] arr) {
+    if(arr.length==0) return 0;
+
+    int T0 = 0;
+    int T1 = -(int)1e9;
+    int T2 = 0;
+
+    for(int val : arr){
+        int temp = T0;
+        T0 = Math.max(T0,T1 + val);
+        T1 = Math.max(T1,T2 - val);
+        T2 = temp;
+    }
+
+    return T0;        
+}
+
+//123
+public int maxProfit(int[] arr) {
+    if(arr.length==0) return 0;
+
+    int T10 = 0;   // ek transaction pe holding zero 
+    int T11 = -(int)1e9; // ek transaction pe holding ek ki hai.
+   
+    int T20 = 0;// do transaction pe holding zero 
+    int T21 = -(int)1e9; // do transaction pe holding ek ki hai.
+    
+    for(int val : arr){
+        T20 = Math.max(T20,T21 + val);
+        T21 = Math.max(T21,T10 - val);
+
+        T10 = Math.max(T10,T11 + val);
+        T11 = Math.max(T11, 0 - val);
+    }
+
+    return T20;        
+}
+
+//188
+public int maxProfit(int k, int[] prices) {
+    if(prices.length==0) return 0;
+    
+    if(k > (prices.length >>> 1 )){
+        
+    int T0 = 0;
+    int T1 = -(int)1e9;
+
+    for(int val : prices){
+        T0 = Math.max(T0,T1 + val);
+        T1 = Math.max(T1,T0 - val);
+    }
+
+    return T0; 
+    }
+    
+    int[] Ti0 = new int[k + 1];
+    int[] Ti1 = new int[k + 1];
+    Arrays.fill(Ti1,-(int)1e9);
+
+    for(int val : prices){
+        for(int K = k;K > 0; K--){
+            Ti0[K] = Math.max(Ti0[K],Ti1[K] + val);
+            Ti1[K] = Math.max(Ti1[K],Ti0[K - 1] - val);
+        }
+    }
+
+    return Ti0[k];
+}
+
+//714
+public int maxProfit(int[] prices, int fee) {
+        
+    int T0 = 0;
+    int T1 = -(int)1e9;
+
+    for(int val : prices){
+        int T0_prev = T0; 
+        T0 = Math.max(T0,T1 + val);
+        T1 = Math.max(T1,T0_prev - val - fee);
+    }
+
+    return T0; 
+        
+}
+
+//https://practice.geeksforgeeks.org/problems/consecutive-1s-not-allowed1912/1
+long countStrings(int n) {
+    if(n == 0) return n;
+    
+    long mod = (int)1e9 + 7;
+    long zeros = 1;
+    long onces = 1;
+    for(int i = 1;i < n;i++){
+        long prevZero = zeros;
+        zeros = (zeros%mod + onces%mod)%mod;
+        onces = prevZero%mod;
+    }
+    
+    return (zeros%mod + onces%mod)%mod;
+}
+
+//https://www.geeksforgeeks.org/count-possible-ways-to-construct-buildings/
