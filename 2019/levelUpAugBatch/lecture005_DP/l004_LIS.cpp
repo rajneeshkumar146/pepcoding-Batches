8098df27ebdc404c07d31efa8f7456e28fad9a88
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-11-02 22:03:34 +0530

    update

--- 2019/levelUpAugBatch/lecture005_DP/l004_LIS.cpp
+++ 2019/levelUpAugBatch/lecture005_DP/l004_LIS.cpp
@@ -99,15 +99,92 @@ int LBS(vector<int> &arr)
     vector<int> DP_LIS(n, 0);
     vector<int> DP_LDS(n, 0);
 
-    LIS(arr,DP_LIS);
-    LDS(arr,DP_LDS);
+    LIS(arr, DP_LIS);
+    LDS(arr, DP_LDS);
 
     int maxLen = 0;
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++)
+    {
         maxLen = max(maxLen, DP_LIS[i] + DP_LDS[i] - 1);
     }
 
     return maxLen;
+}
+
+int LISumSubsequnece(vector<int> &arr, vector<int> &dp)
+{
+    int n = arr.size();
+    int maxSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = arr[i];
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[i] > arr[j])
+                dp[i] = max(dp[i], dp[j] + arr[i]);
+        }
+        maxSum = max(maxSum, dp[i]);
+    }
+
+    return maxSum;
+}
+
+//https://www.geeksforgeeks.org/maximum-sum-increasing-subsequence-dp-14/
+//https://www.geeksforgeeks.org/maximum-sum-bi-tonic-sub-sequence/
+
+int minNoOfDeletions(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> dp(n, 0);
+
+    int maxLen = 0;
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = 1;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[i] >= arr[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+        maxLen = max(maxLen, dp[i]);
+    }
+
+    return n - maxLen;
+}
+
+//673
+int findNumberOfLIS(vector<int> &nums)
+{
+    int n = nums.size();
+    if(n==0) return 0;
+
+    vector<int> dp(n,0);
+    vector<int> count(n,0);
+    
+    int maxLen = 0,maxCount = 0;
+    for(int i=0;i<n;i++){
+        dp[i] = 1;
+        count[i] = 1;
+        for(int j=i-1;j>=0;j--){
+            if(nums[i] > nums[j]){
+                if(dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                    count[i] = count[j];
+                }else if(dp[j] + 1 == dp[i]){
+                    count[i] += count[j];
+                }
+            }
+        }
+
+        if(dp[i] > maxLen){
+            maxLen = dp[i];
+            maxCount = count[i];
+        }else if(dp[i] == maxLen){
+            maxCount += count[i];
+        }
+    }
+
+    return maxCount;
 }
 
 void solve()
