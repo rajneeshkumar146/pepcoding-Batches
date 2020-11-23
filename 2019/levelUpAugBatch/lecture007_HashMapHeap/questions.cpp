2c5b1bfe66c538ce67de320466cf3d68959b3765
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-11-23 22:21:54 +0530

    update

--- 2019/levelUpAugBatch/lecture007_HashMapHeap/questions.cpp
+++ 2019/levelUpAugBatch/lecture007_HashMapHeap/questions.cpp
@@ -1,18 +1,102 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <unordered_map>
 
 using namespace std;
 
 //Leetcode 215
 int findKthLargest(vector<int> &nums, int k)
 {
-    priority_queue<int,vector<int>,greater<int>> pq;
-    for(int ele : nums){
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int ele : nums)
+    {
         pq.push(ele);
-        if(pq.size() > k) pq.pop();
+        if (pq.size() > k)
+            pq.pop();
     }
 
     return pq.top();
 }
 
+//350
+vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+{
+    unordered_map<int, int> map;
+    for (int ele : nums1)
+        map[ele]++;
+    vector<int> ans;
+    for (int ele : nums2)
+    {
+        if (map.find(ele) != map.end())
+        {
+            ans.push_back(ele);
+            map[ele]--;
+            if (map[ele] == 0)
+                map.erase(ele);
+        }
+    }
+
+    return ans;
+}
+
+int longestConsecutive(vector<int> &nums)
+{
+    unordered_map<int, int> map;
+    for (int ele : nums)
+        map[ele]++;
+
+    int len = 0;
+    for (int ele : nums)
+    {
+        if (map.find(ele) == map.end())
+            continue;
+
+        int n = ele;
+        int prev = n - 1;
+        int next = n + 1;
+
+        while (map.find(prev) != map.end())
+        {
+            map.erase(prev);
+            prev--;
+        }
+
+        while (map.find(next) != map.end())
+        {
+            map.erase(next);
+            next++;
+        }
+
+        map.erase(ele);
+        len = max(len, next - prev - 1);
+    }
+
+    return len;
+}
+
+//347
+vector<int> topKFrequent(vector<int> &nums, int k)
+{
+    unordered_map<int, int> map;
+    for (int ele : nums)
+        map[ele]++;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    for (pair<int, int> key : map)
+    {
+        pq.push({key.second, key.first});
+        if (pq.size() > k)
+            pq.pop();
+    }
+
+    vector<int> ans;
+    while (pq.size() != 0)
+    {
+        ans.push_back(pq.top().second);
+        pq.pop();
+    }
+
+    return ans;
+}
--- 2019/levelUpAugBatch/lecture007_HashMapHeap/questions.java
+++ 2019/levelUpAugBatch/lecture007_HashMapHeap/questions.java
@@ -1,4 +1,5 @@
 import java.util.PriorityQueue;
+import java.util.HashSet;
 public class questions{
 
     //Leetcode 215
@@ -12,6 +13,7 @@ public class questions{
         return pq.peek();
     }
 }
+
 
 // 703
 class KthLargest {
@@ -31,4 +33,130 @@ class KthLargest {
         if(pq.size() > this.K) pq.remove();
         return pq.peek();
     }
+}
+
+//349
+public int[] intersection(int[] nums1, int[] nums2) {
+    HashMap<Integer,Integer> map = new HashMap<>();
+    for(int ele : nums1){
+        map.put(ele,map.getOrDefault(ele,0) + 1);
+    }
+    
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    for(int ele : nums2){
+        if(map.containsKey(ele)){
+            ans.add(ele);
+            map.remove(ele);
+        }
+    }
+
+    int[] arr = new int[ans.size()];
+    int i = 0;
+    
+    for(int ele:ans) arr[i++] = ele;
+    
+    return arr;
+}
+//350
+public int[] intersect(int[] nums1, int[] nums2) {
+    HashMap<Integer,Integer> map = new HashMap<>();
+    for(int ele : nums1){
+        map.put(ele,map.getOrDefault(ele,0) + 1);
+    }
+    
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    for(int ele : nums2){
+        if(map.containsKey(ele)){
+            ans.add(ele);
+            map.put(ele,map.get(ele) - 1);
+            if(map.get(ele) == 0) map.remove(ele);
+        }
+    }
+
+    int[] arr = new int[ans.size()];
+    int i = 0;
+    
+    for(int ele:ans) arr[i++] = ele;
+    
+    return arr;
+}
+
+//128
+public int longestConsecutive(int[] nums) {
+    HashMap<Integer,Integer> map = new HashMap<>();
+    for(int ele : nums) map.put(ele,1);
+    
+    int len = 0;
+    for(int ele : nums){
+        if(!map.containsKey(ele)) continue;
+
+        map.remove(ele);
+        int prev = ele - 1;
+        int next = ele + 1;
+
+        while(map.containsKey(prev)){
+            map.remove(prev); prev--;
+        }
+
+        while(map.containsKey(next)){
+            map.remove(next); next++;
+        }
+
+        len = Math.max(len, next - prev - 1);
+    }
+
+    return len;
+}
+
+//347
+public int[] topKFrequent(int[] nums, int k) {
+        
+    HashMap<Integer,Integer>map = new HashMap<>();
+    for(int ele : nums) map.put(ele,map.getOrDefault(ele,0) + 1);
+    
+    // data,freq
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+        return a[1] - b[1];//other - this
+    });
+    
+    for(int ele : map.keySet()){
+        pq.add(new int[]{ele,map.get(ele)});
+        if(pq.size() > k) pq.remove(); 
+    }
+    
+    int[] ans = new int[k];
+    int idx = 0;
+    while(pq.size() != 0){
+        int[] d = pq.remove();
+        ans[idx++] = d[0];
+    }
+    
+    return ans;   
+}
+// 347
+public int[] topKFrequent(int[] nums, int k) {
+        
+    HashMap<Integer,Integer>map = new HashMap<>();
+    for(int ele : nums) map.put(ele,map.getOrDefault(ele,0) + 1);
+    
+    // data,freq
+    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+        return map.get(a) - map.get(b);//other - this
+    });
+    
+    for(int ele : map.keySet()){
+        pq.add(ele);
+        if(pq.size() > k) pq.remove(); 
+    }
+    
+    int[] ans = new int[k];
+    int idx = 0;
+    while(pq.size() != 0){
+        ans[idx++] = pq.remove();
+    }
+    
+    return ans;
+    
 }
