eb296237f97bf4f7c4de5b65c45826a84c736034
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-12-02 17:45:45 +0530

    update

--- 2020/GTBIT/L001_foundation/l001_Basic/l002Pattern.java
+++ 2020/GTBIT/L001_foundation/l001_Basic/l002Pattern.java
@@ -84,6 +84,34 @@ public class l002Pattern{
         }
     }
 
+    public static void patter6(int row){
+        int nst = (row + 1) / 2; // no of star.
+        int nsp = 1;
+        for(int r = 1;r <= row; r++){
+            for(int cst = 1;cst <= nst;cst++){
+                System.out.print("*\t");
+            }
+
+            for(int csp = 1;csp <= nsp;csp++){
+                System.out.print("\t");
+            }
+
+            for(int cst = 1;cst <= nst;cst++){
+                System.out.print("*\t");
+            }
+    
+            if(r <= row / 2){
+                nst--;
+                nsp+=2;
+            }else{
+                nsp-=2;
+                nst++;
+            }
+    
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
