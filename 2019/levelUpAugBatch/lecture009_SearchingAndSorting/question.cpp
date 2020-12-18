4d3ddc7f43450cf1ae8b009a36b3d983dbb89958
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-12-18 22:14:49 +0530

    update

---
+++ 2019/levelUpAugBatch/lecture009_SearchingAndSorting/question.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    if (matrix.size() == 0 || matrix[0].size() == 0)
+        return false;
+    int n = matrix.size(), m = matrix[0].size();
+
+    int si = 0, ei = n * m - 1;
+    while (si <= ei)
+    {
+        int mid = (si + ei) / 2;
+        int val = matrix[mid / m][mid % m];
+
+        if (val == target)
+            return true;
+        else if (val < target)
+            si = mid + 1;
+        else
+            ei = mid - 1;
+    }
+    return false;
+}
+
+// https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
+
+long long totalInversionCount(long long arr[], vector<long long> &sortedArray, long long si, long long mid, long long ei)
+{
+    long long count = 0;
+    long long i = si, j = mid, k = si;
+
+    while (i < mid && j <= ei)
+    {
+        if (arr[i] <= arr[j])
+        {
+            sortedArray[k++] = arr[i++];
+            // arr[i] > arr[j] and i > j; count += ei - j + 1;
+        }
+        else
+        {
+            sortedArray[k++] = arr[j++];
+            count += mid - i;
+        }
+    }
+
+    while (i < mid)
+        sortedArray[k++] = arr[i++];
+    while (j <= ei)
+        sortedArray[k++] = arr[j++];
+
+    while (si <= ei)
+        arr[si] = sortedArray[si++];
+    return count;
+}
+
+long long inversionCount(long long arr[], vector<long long> &sortedArray, long long si, long long ei)
+{
+    if (si >= ei)
+        return 0;
+
+    long long mid = (si + ei) / 2;
+    long long count = 0;
+
+    count += inversionCount(arr, sortedArray, si, mid);
+    count += inversionCount(arr, sortedArray, mid + 1, ei);
+
+    count += totalInversionCount(arr, sortedArray, si, mid + 1, ei);
+    return count;
+}
+
+long long int inversionCount(long long arr[], long long N)
+{
+    if (N == 0)
+        return 0;
+    vector<long long> sortedArray(N, 0);
+    return inversionCount(arr, sortedArray, 0, N - 1);
+}
+
+int binarySearch(vector<int> &arr, int data)
+{
+    int si = 0, ei = arr.size();
+    while (si < ei)
+    {
+        int mid = (si + ei) / 2;
+        if (data > arr[mid])
+            si = mid + 1;
+        else
+            ei = mid;
+    }
+
+    return si;
+}
+
+vector<int> findClosestElements(vector<int> &arr, int k, int x)
+{
+    int n = arr.size();
+
+    if (x <= arr[0])
+    {
+        vector<int> ans(arr.begin(), arr.begin() + k);
+        return ans;
+    }
+    else if (x >= arr[n - 1])
+    {
+        vector<int> ans(arr.begin() + n - k, arr.end());
+        return ans;
+    }
+    else
+    {
+
+        // int idx = binarySearch(arr, x); // where we suppose to find the x element.
+        int idx = (lower_bound(arr.begin(), arr.end(), x) - arr.begin());
+        
+        int si = max(0, idx - k);
+        int ei = min(n - 1, idx + k);
+
+        while ((ei - si + 1) > k)
+        {
+            if ((x - arr[si]) > (arr[ei] - x))
+                si++;
+            else
+                ei--;
+        }
+
+        vector<int> ans(arr.begin() + si, arr.begin() + ei + 1);
+        return ans;
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
--- 2019/levelUpAugBatch/lecture009_SearchingAndSorting/question.java
+++ 2019/levelUpAugBatch/lecture009_SearchingAndSorting/question.java
@@ -38,4 +38,109 @@ public class question{
         
       return new int[]{binarySearchFirstIndex(arr,data),binarySearchLastIndex(arr,data)};
 }
+
+// 74
+public boolean searchMatrix(int[][] matrix, int target) {
+    if(matrix.length == 0 || matrix[0].length == 0) return false;
+    int n = matrix.length, m = matrix[0].length;
+    int si = 0, ei = n * m - 1;
+    while(si <= ei){
+        int mid = (si + ei) / 2;
+        int val = matrix[mid / m][mid % m];
+
+        if(val == target) return true;
+        else if(val < target) si = mid + 1;
+        else ei = mid - 1;
+    }
+    return false;
+}
+
+
+static long totalInversionCount(long[] arr, long[] sortedArray,  int si,  int mid,  int ei)
+{
+     long count = 0;
+     int i = si, j = mid, k = si;
+
+    while (i < mid && j <= ei)
+    {
+        if (arr[i] <= arr[j]){
+            sortedArray[k++] = arr[i++];
+            // arr[i] > arr[j] and i > j; count += ei - j + 1;
+        }else{
+            sortedArray[k++] = arr[j++];
+            count += mid - i;  
+        }
+    }
+
+    while (i < mid)
+        sortedArray[k++] = arr[i++];
+    while (j <= ei)
+        sortedArray[k++] = arr[j++];
+
+    while (si <= ei)
+        arr[si] = sortedArray[si++];
+    return count;
+}
+
+static long inversionCount(long[] arr, long[] sortedArray, int si,int ei)
+{
+    if (si >= ei)
+        return 0;
+
+    int mid = (si + ei) / 2;
+    long count = 0;
+
+    count += inversionCount(arr, sortedArray, si, mid);
+    count += inversionCount(arr, sortedArray, mid + 1, ei);
+
+    count += totalInversionCount(arr, sortedArray, si, mid+1, ei);
+    return count;
+}
+
+static long inversionCount(long[] arr, long N)
+{
+    if (N == 0)
+        return 0;
+    int n = (int)N;
+    long[] sortedArray = new long[n];
+    return inversionCount(arr, sortedArray, 0, n - 1);
+}
+
+public int binarySearch(int[] arr,int data){
+    int si = 0, ei = arr.length;
+    while(si < ei){
+        int mid = (si + ei) / 2; 
+        if(data > arr[mid]) si = mid + 1;
+        else ei = mid;
+    }
+
+    return si;
+}
+
+public List<Integer> findClosestElements(int[] A, int k, int x) {
+    List<Integer> arr = new ArrayList<>();
+    for(int ele : A) arr.add(ele);
+    
+    int n = A.length;
+    
+    if(x <= A[0]) return arr.subList(0,k);
+    else if(x >= A[n - 1]) return arr.subList(n - k, n);
+    else{
+        
+        int idx = binarySearch(A,x);   // where we suppose to find the x element.
+        int si = Math.max(0,idx - k);
+        int ei = Math.min(n - 1, idx + k);
+        
+        while((ei - si + 1) > k){
+            if((x - A[si]) > (A[ei] - x)) si++;
+            else ei--;
+        }
+        return arr.subList(si, ei + 1);
+    }
+}
+
+
+
+
+
 }
