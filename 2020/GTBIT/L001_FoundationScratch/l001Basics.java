a6d8b69d30d648c2e26184b8ed8d5fbdc231c1bc
rajneeshkumar146 <rajneeshkumar146@gmai.com>
2020-12-22 21:55:59 +0530

    update

--- 2020/GTBIT/L001_FoundationScratch/l001Basics.java
+++ 2020/GTBIT/L001_FoundationScratch/l001Basics.java
@@ -26,6 +26,20 @@ public class l001Basics{
        System.out.println("see you soon!");
     }
 
+    public static void gradingSystem(int marks){
+        if(marks > 90){
+            System.out.println("excellent");
+        }else if(marks > 80){
+            System.out.println("good");
+        }else if(marks > 70){
+            System.out.println("fair");
+        }else if(marks > 60){
+            System.out.println("meets expectations");
+        }else{
+            System.out.println("below par");
+        }
+    }
+
     public static void main(String[] args){
     //     System.out.println("Hello");
     //     System.out.println("Pepcoder");
