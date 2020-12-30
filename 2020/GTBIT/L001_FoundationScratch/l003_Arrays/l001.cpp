8774e5e800c94184fdcd68e9883037d24183ff1c
rajneeshkumar146 <rajneeshkumar146@gmail.com>
2020-12-30 17:59:45 +0530

    update

--- 2020/GTBIT/L001_FoundationScratch/l003_Arrays/l001.cpp
+++ 2020/GTBIT/L001_FoundationScratch/l003_Arrays/l001.cpp
@@ -85,6 +85,108 @@ void reverse(vector<int> &arr, int si, int ei)
     }
 }
 
+void addTwoArrays(vector<int> &a, vector<int> &b)
+{
+    int n = a.size(), m = b.size();
+    vector<int> ans(max(n, m) + 1, 0);
+
+    int i = n - 1, j = m - 1, k = ans.size() - 1;
+    int carry = 0;
+    while (k >= 0)
+    {
+        int sum = carry;
+        if (i >= 0)
+            sum += a[i--];
+        if (j >= 0)
+            sum += b[j--];
+        // int sum = carry + (i >= 0 ? a[i--] : 0) + (j >= 0 ? b[j--] : 0);
+
+        ans[k--] = sum % 10;
+        carry = sum / 10;
+    }
+
+    for (int l = 0; l < ans.size(); l++)
+    {
+        if (l == 0 && ans[l] == 0)
+            continue;
+        cout << ans[l] << endl;
+    }
+}
+
+void subTwoArrays(vector<int> &a, vector<int> &b)
+{
+    int n = a.size(), m = b.size();
+    vector<int> ans(max(n, m), 0);
+
+    int i = n - 1, j = m - 1, k = ans.size() - 1;
+    int borrow = 0;
+    while (k >= 0)
+    {
+        int sum = borrow + (j >= 0 ? b[j--] : 0) - (i >= 0 ? a[i--] : 0);
+        if (sum < 0)
+        {
+            sum += 10;
+            borrow = -1;
+        }
+        else
+            borrow = 0;
+
+        ans[k--] = sum;
+    }
+
+    bool is_You_Find_A_Non_Zero_Number = false;
+    for (int l = 0; l < ans.size(); l++)
+    {
+        if (!is_You_Find_A_Non_Zero_Number && ans[l] == 0)
+            continue;
+        is_You_Find_A_Non_Zero_Number = true;
+        cout << ans[l] << endl;
+    }
+}
+
+void subArray(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i; j < arr.size(); j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
+
+vector<int> prefixSumArray(vector<int> &arr)
+{
+    vector<int> psum(arr.size() + 1, 0);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        psum[i + 1] = psum[i] + arr[i];
+    }
+    return psum;
+}
+
+int resolveQuery(){
+    int n; cin >> n;
+    vector<int> arr(n,0);
+    for(int i=0;i<n;i++) cin >> arr[i];
+
+    vector<int> psum = prefixSumArray(arr);
+    int q; cin >> q;
+
+    while(q-- > 0){
+        int si, ei ;
+        cin >> si >> ei;
+        
+
+        cout << psum[ei + 1] - psum[si] << endl;
+    }
+}
+
 void display(vector<int> &arr)
 {
     for (int i = 0; i < arr.size(); i++)
@@ -104,11 +206,12 @@ void input(vector<int> &arr)
 int main()
 {
     // test1();
-    int n; cin >> n;
-    vector<int> arr(n,0);
-    
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+
     input(arr);
-    reverse(arr,0,n-1);
+    reverse(arr, 0, n - 1);
     display(arr);
 
     return 0;
