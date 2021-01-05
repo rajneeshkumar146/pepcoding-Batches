82689d72cb1f1ea8e36ddeb2cdfc8302952e3ecb
rajneeshkumar146 <rajneeshkumar146@gmail.com>
2021-01-05 22:52:25 +0530

    update

--- 2019/LevelUpDecBatch/l001_Recursion/l002.cpp
+++ 2019/LevelUpDecBatch/l001_Recursion/l002.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+void fun(int a, int b)
+{
+    if (a == 5)
+    {
+        b = 5;
+        return;
+    }
+
+    a++;
+    fun(a, b);
+    cout << b << endl;
+}
+
+int main()
+{
+    int b = 1;
+    fun(1, b);
+    return 0;
+}
--- 2019/LevelUpDecBatch/l001_Recursion/questions.java
+++ 2019/LevelUpDecBatch/l001_Recursion/questions.java
@@ -95,4 +95,5 @@ class questions {
         }
     }
 
+
 }
