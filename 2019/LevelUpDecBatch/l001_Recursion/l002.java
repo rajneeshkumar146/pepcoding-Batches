d719d93a99b2561135560e4e1d9bee386ce4ad3b
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-01-07 19:59:44 +0530

    update

--- 2019/LevelUpDecBatch/l001_Recursion/l002.java
+++ 2019/LevelUpDecBatch/l001_Recursion/l002.java
@@ -96,6 +96,42 @@ public class l002 {
         return count;
     }
 
+    public static class pair {
+        int longestPathLength = 0;
+        String longestPath = "";
+
+        public pair(int longestPathLength, String longestPath) {
+            this.longestPathLength = longestPathLength;
+            this.longestPath = longestPath;
+        }
+    }
+
+    public static pair longestPathRecu(int sr, int sc, int dr, int dc, boolean[][] vis, int[][] dir, String[] dirS) {
+        if (sr == dr && sc == dc) {
+            return new pair(0, "");
+        }
+
+        pair myAns = new pair(-1, "");
+        vis[sr][sc] = true;
+
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r <= dr && c <= dc && !vis[r][c]) {
+                pair recAns = longestPathRecu(r, c, dr, dc, vis, dir, dirS);
+                if (recAns.longestPathLength + 1 > myAns.longestPathLength) {
+                    myAns.longestPathLength = recAns.longestPathLength + 1;
+                    myAns.longestPath = dirS[d] + recAns.longestPath;
+                }
+            }
+        }
+
+        vis[sr][sc] = false;
+
+        return myAns;
+    }
+
     public static void floodfill() {
         boolean[][] vis = new boolean[3][3]; // vector<vector<bool>> vis(3,vector<bool>(3,false));
         int[][] dir = { { 1, 0 }, { -1, 0 }, { 1, 1 }, { -1, -1 }, { 0, 1 }, { 0, -1 }, { -1, 1 }, { 1, -1 } };
---
+++ 2019/LevelUpDecBatch/l001_Recursion/l003CoinPermutation.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int coinChangePermutationInfi(vector<int> &arr, int tar, string ans)
+{
+    if (tar == 0)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int ele : arr)
+    {
+        if (tar - ele >= 0)
+            count += coinChangePermutationInfi(arr, tar - ele, ans + to_string(ele));
+    }
+
+    return count;
+}
+
+int coinChangeCombinationInfi(vector<int> &arr, int idx, int tar, string ans)
+{
+    if (tar == 0)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int i = idx; i < arr.size(); i++)
+    {
+        if (tar - arr[i] >= 0)
+            count += coinChangeCombinationInfi(arr, i, tar - arr[i], ans + to_string(arr[i]));
+    }
+
+    return count;
+}
+
+int coinChangeCombination(vector<int> &arr, int idx, int tar, string ans)
+{
+    if (tar == 0)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int i = idx; i < arr.size(); i++)
+    {
+        if (tar - arr[i] >= 0)
+            count += coinChangeCombination(arr, i + 1, tar - arr[i], ans + to_string(arr[i]));
+    }
+
+    return count;
+}
+
+int coinChangePermutation(vector<int> &arr, int tar, string ans)
+{
+    if (tar == 0)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+
+    int count = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int ele = arr[i];
+        if (arr[i] > 0 && tar - ele >= 0)
+        {
+            arr[i] = -arr[i];
+            count += coinChangePermutation(arr, tar - ele, ans + to_string(ele));
+            arr[i] = -arr[i];
+        }
+    }
+
+    return count;
+}
+
+int coinChangeCombinationSubSeq(vector<int> &arr, int idx, int tar, string ans)
+{
+    if (idx == arr.size() || tar == 0)
+    {
+        if (tar == 0)
+        {
+            cout << ans << endl;
+            return 1;
+        }
+        return 0;
+    }
+
+    int count = 0;
+    if (tar - arr[idx] >= 0)
+        count += coinChangeCombinationSubSeq(arr, idx + 1, tar - arr[idx], ans + to_string(arr[idx]));
+    count += coinChangeCombinationSubSeq(arr, idx + 1, tar, ans);
+
+    return count;
+}
+
+int main()
+{
+    vector<int> arr{2, 3, 5, 7};
+    int tar = 10;
+    // cout << coinChangePermutationInfi(arr, tar, "");
+    // cout << coinChangeCombinationInfi(arr, 0,tar, "");
+    // cout << coinChangeCombination(arr, 0, tar, "")<<endl;
+    // cout << coinChangePermutation(arr,tar, "");
+
+    // cout << coinChangeCombinationSubSeq(arr, 0, tar, "") << endl;
+
+    return 0;
+}
