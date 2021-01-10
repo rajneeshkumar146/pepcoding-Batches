ec61e4f85d7f73aaa3d4c450b9bb4b9cc52816fe
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-01-10 20:38:40 +0530

    update

--- 2020/IGDTUW Demo Class/l001.java
+++ 2020/IGDTUW Demo Class/l001.java
@@ -57,13 +57,98 @@ public class l001 {
     public static void printTable(int num) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(num + " X " + i + " = " + num * i);
+        }
+        System.out.println();
+    }
 
+    public static void printTableInRage(int num) {
+        for (int i = 2; i <= num; i++)
+            printTable(i);
+    }
+
+    public static boolean isPrime(int num) {
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+
+    }
+
+    public static void printAllPrimes(int low, int high) {
+        for (int i = low; i <= high; i++) {
+            if (isPrime(i) == true) {
+                System.out.println(i);
+            }
         }
     }
 
-    public static void main(String[] args) {
-        int a = scn.nextInt();
+    public static boolean check(int a, int b, int c) {
+        if (a * a + b * b == c * c)
+            return true;
+        else if (c * c + b * b == a * a)
+            return true;
+        else if (a * a + c * c == b * b)
+            return true;
+        else
+            return false;
 
+    }
+
+    public static void benjaminBulbs(int n) {
+        for (int i = 1; i * i <= n; i++) {
+            System.out.println(i * i);
+        }
+    }
+
+    public static int countOfDigits(int n) {
+        int count = 0;
+        while (n != 0) {
+            n /= 10;
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void reversePrint(int n) {
+        int count = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            System.out.println(rem);
+            n /= 10;
+        }
+    }
+
+    public static int countOfDigits(int n) {
+        int count = 0;
+        while (n != 0) {
+            n /= 10;
+            count++;
+        }
+
+        return count;
+    }
+
+    public static int rotateNumber(int n, int r) {
+        int d = countOfDigits(n);
+        r = (r % d + d) % d;
+
+        int div = (int) Math.pow(10, r);
+        int mul = (int) Math.pow(10, d - r);
+
+        int a = n % div;
+        int b = n / div;
+
+        return a * mul + b;
+    }
+
+    public static void main(String[] args) {
+        // int a = scn.nextInt();
+        // printTableInRage(200);
+        // int num = 13;
+        // System.out.println(isPrime(num));
     }
 
 }
