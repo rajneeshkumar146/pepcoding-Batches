e91789147483480beed6efeae84d345792024aa8
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-01-15 20:53:48 +0530

    update

--- 2020/GTBIT/L001_foundation/l006_Recursion/l002.java
+++ 2020/GTBIT/L001_foundation/l006_Recursion/l002.java
@@ -56,12 +56,78 @@ public class l002 {
         printASCIISS(str, idx + 1, ans);
     }
 
+    public static ArrayList<String> printASCIISS_Ret(String str, int idx) {
+        if (idx == str.length()) {
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        ArrayList<String> recAns = printASCIISS_Ret(str, idx + 1);
+
+        ArrayList<String> myAns = new ArrayList<>(); // nahi anne ki choise dekhli.
+
+        char ch = str.charAt(idx);
+        for (String s : recAns) {
+            myAns.add(ch + s);
+            myAns.add((int) ch + s);
+            myAns.add(s);
+        }
+
+        return myAns;
+    }
+
     public static void subsequence() {
         String str = "abc";
         // printSS(str, 0, "");
         // printSS_02(str,0,new StringBuilder());
         // System.out.println(subseq_03(str, 0));
-        printASCIISS(str,0,"")''
+        printASCIISS(str, 0, "");
+    }
+
+    static String[] codes = { ".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
+
+    public static ArrayList<String> getKPC(String str, int idx) {
+        if (idx == str.length()) {
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        ArrayList<String> recAns = getKPC(str, idx + 1);
+        ArrayList<String> myAns = new ArrayList<>();
+
+        char ch = str.charAt(idx);
+        String code = codes[ch - '0'];
+        int count = 0;
+
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+
+            for (String s : recAns) {
+                myAns.add(c + s);
+            }
+        }
+
+        return myAns;
+    }
+
+    public static int printKPC(String str, int idx, String ans) {
+        if (idx == str.length()) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        char ch = str.charAt(idx);
+        String code = codes[ch - '0'];
+        int count = 0;
+
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+            count += printKPC(str, idx + 1, ans + c);
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
