5c443fd451ccf32004fccd089a01f2e013ecc621
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-01-17 12:52:11 +0530

    update

--- 2019/LevelUpDecBatch/l001_Recursion/leetcodeSolutions.java
+++ 2019/LevelUpDecBatch/l001_Recursion/leetcodeSolutions.java
@@ -86,8 +86,108 @@ public class leetcodeSolutions {
                 }
             }
         }
-
         return true;
+    }
+
+    public static void wordBreak(String str, String ans, HashSet<String> dict) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        for (int len = 1; len <= str.length(); len++) {
+            String word = str.substring(0, len);
+
+            if (dict.contains(word)) {
+                wordBreak(str.substring(len), ans + word + " ", dict);
+            }
+        }
+    }
+
+    public static boolean isPossibleToPlaceH(char[][] arr, String word, int r, int c) {
+
+    }
+
+    public static boolean[] placeH(char[][] arr, String word, int r, int c) {
+        boolean[] charLoc = new boolean[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            if (arr[r][c + i] == '-') {
+                charLoc[i] = true;
+                arr[r][c + i] = word.charAt(i);
+            }
+        }
+
+        return charLoc;
+    }
+
+    public static void unPlaceH(char[][] arr, String word, int r, int c, boolean[] charLoc) {
+        for (int i = 0; i < word.length(); i++) {
+            if (charLoc[i]) {
+                arr[r][c + i] = '-';
+            }
+        }
+    }
+
+    public static boolean isPossibleToPlaceV(char[][] arr, String word, int r, int c) {
+
+    }
+
+    public static boolean[] placeV(char[][] arr, String word, int r, int c) {
+        boolean[] charLoc = new boolean[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            if (arr[r + i][c] == '-') {
+                charLoc[i] = true;
+                arr[r + i][c] = word.charAt(i);
+            }
+        }
+        return charLoc;
+    }
+
+    public static void unPlaceV(char[][] arr, String word, int r, int c, boolean[] charLoc) {
+        for (int i = 0; i < word.length(); i++) {
+            if (charLoc[i]) {
+                arr[r + i][c] = '-';
+            }
+        }
+    }
+
+    public static void displayCharArray(char[][] arr) {
+        for (char[] ar : arr) {
+            for (char ele : ar) {
+                System.out.println(ele + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int crossWord(char[][] arr, String[] words, int vidx) {
+        if (vidx == words.length) {
+            displayCharArray(arr);
+            return 1;
+        }
+
+        String word = words[vidx];
+        int count = 0;
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr[0].length; c++) {
+                if (arr[r][c] == '-' || arr[r][c] == word.charAt(0)) {
+                    if (isPossibleToPlaceH(arr, word, r, c)) {
+                        boolean[] charLoc = placeH(arr, word, r, c);
+                        count += crossWord(arr, words, vidx + 1);
+                        unPlaceH(arr, word, r, c, charLoc);
+                    }
+
+                    if (isPossibleToPlaceV(arr, word, r, c)) {
+                        boolean[] charLoc = placeV(arr, word, r, c);
+                        count += crossWord(arr, words, vidx + 1);
+                        unPlaceV(arr, word, r, c, charLoc);
+                    }
+                }
+            }
+        }
+
+        return count;
+
     }
 
 }
