20e3f7108c01e8855a5a406d80f5b8639004bdc6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-01-28 22:17:47 +0530

    update

--- 2019/LevelUpDecBatch/l002_LinkedList/questions.cpp
+++ 2019/LevelUpDecBatch/l002_LinkedList/questions.cpp
@@ -488,3 +488,48 @@ ListNode *reverseKGroup(ListNode *head, int k)
     ot->next = curr;
     return oh;
 }
+
+//92
+ListNode *reverseBetween(ListNode *head, int m, int n)
+{
+    if (head == nullptr || head->next == nullptr || m == n)
+        return head;
+
+    ListNode *curr = head;
+    ListNode *prev = nullptr;
+
+    int idx = 1;
+
+    while (curr != nullptr)
+    {
+        while (idx >= m && idx <= n)
+        {
+            ListNode *forw = curr->next;
+            curr->next = nullptr;
+            addFirstNode(curr);
+            curr = forw;
+            idx++;
+        }
+
+        if (idx > n)
+        {
+            if (prev != nullptr)
+            {
+                prev->next = th;
+                tt->next = curr;
+                return head;
+            }
+            else
+            {
+                tt->next = curr;
+                return th;
+            }
+        }
+
+        idx++;
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return head;
+}
