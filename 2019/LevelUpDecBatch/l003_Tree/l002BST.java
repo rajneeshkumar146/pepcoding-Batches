521693739b093922a78f2eb97454995e9f7fbcd6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-02-07 13:05:04 +0530

    update

--- 2019/LevelUpDecBatch/l003_Tree/l002BST.java
+++ 2019/LevelUpDecBatch/l003_Tree/l002BST.java
@@ -65,6 +65,92 @@ public class l002BST {
             return findData2(node.right, data);
     }
 
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val)
+            root.left = insertIntoBST(root.left, val);
+        else
+            root.right = insertIntoBST(root.right, val);
+
+        return root;
+    }
+
+    public TreeNode insertIntoBST_02(TreeNode root, int val) {
+        TreeNode curr = root;
+        TreeNode prev = null;
+
+        while (curr != null) {
+            prev = curr;
+            if (curr.val > val) {
+                curr = curr.left;
+            } else {
+
+                curr = curr.right;
+            }
+        }
+
+        TreeNode node = new TreeNode(val);
+        if (prev == null)
+            return node;
+        else if (prev.val < val) {
+            prev.right = node;
+        } else {
+            prev.left = node;
+        }
+
+        return root;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return null;
+
+        if (root.val > key)
+            root.left = insertIntoBST(root.left, key);
+        else if (root.val < key)
+            root.right = insertIntoBST(root.right, key);
+        else {
+            if (root.left == null || root.right == null)
+                return root.left != null ? root.left : root.right;
+
+            int maxValue = maximumEle(root.left);
+            root.val = maxValue;
+
+            root.left = deleteNode(root.left, maxValue);
+        }
+
+        return root;
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode node, TreeNode p, TreeNode q) {
+        TreeNode curr = node;
+        while (curr != null) {
+            if (curr.val > p.val && curr.val > q.val)
+                curr = curr.left;
+            else if (curr.val < p.val && curr.val < q.val)
+                curr = curr.right;
+            else
+                return curr;
+        }
+
+        return null;
+    }
+
+    public TreeNode createBST(int[] arr, int si, int ei) {
+        if(si > ei) return  null;
+
+        int mid = (si + ei) / 2;
+        TreeNode node = new TreeNode(arr[mid]);
+
+        node.left = createBST(arr, si, mid - 1);
+        node.right = createBST(arr, mid + 1, ei);
+
+        return node;
+    }
+
     public static void main(String[] args) {
 
     }
