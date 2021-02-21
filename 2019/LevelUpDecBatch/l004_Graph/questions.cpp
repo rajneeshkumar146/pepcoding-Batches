ebfd194dd1c6995c8491729a7a5f8a6c104c1a7f
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-02-21 12:42:33 +0530

    update

---
+++ 2019/LevelUpDecBatch/l004_Graph/questions.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//200
+
+void dfsIsland(int i, int j, int n, int m, vector<vector<char>> &grid, vector<vector<int>> &dir)
+{
+    grid[i][j] = '0';
+    for (int d = 0; d < dir.size(); d++)
+    {
+        int r = i + dir[d][0];
+        int c = j + dir[d][1];
+        if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == '1')
+        {
+            dfsIsland(r, c, n, m, grid, dir);
+        }
+    }
+}
+
+int numIslands(vector<vector<char>> &grid)
+{
+    if (grid.size() == 0 || grid[0].size() == 0)
+        return 0;
+
+    int count = 0;
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<vector<int>> dir{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] == '1')
+            {
+                count++;
+                dfsIsland(i, j, n, m, grid, dir);
+            }
+        }
+    }
+
+    return count;
+}
+
+//695
+
+int dfsArea(int i, int j, int n, int m, vector<vector<int>> &grid, vector<vector<int>> &dir)
+{
+    grid[i][j] = 0;
+    int area = 1;
+    for (int d = 0; d < dir.size(); d++)
+    {
+        int r = i + dir[d][0];
+        int c = j + dir[d][1];
+        if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1)
+        {
+            area += dfsArea(r, c, n, m, grid, dir);
+        }
+    }
+
+    return area;
+}
+
+int maxAreaOfIsland(vector<vector<int>> &grid)
+{
+    if (grid.size() == 0 || grid[0].size() == 0)
+        return 0;
+
+    int area = 0;
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<vector<int>> dir{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] == 1)
+            {
+                area = max(area, dfsArea(i, j, n, m, grid, dir));
+            }
+        }
+    }
+
+    return area;
+}
+
+//463
+int islandPerimeter(vector<vector<int>> &grid)
+{
+    if (grid.size() == 0 || grid[0].size() == 0)
+        return 0;
+
+    int nbrs = 0, count = 0;
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<vector<int>> dir{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] == 1)
+            {
+                count++;
+                for (int d = 0; d < dir.size(); d++)
+                {
+                    int r = i + dir[d][0];
+                    int c = j + dir[d][1];
+                    if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1)
+                        nbrs++;
+                }
+            }
+        }
+    }
+
+    return count * 4 - nbrs;
+}
