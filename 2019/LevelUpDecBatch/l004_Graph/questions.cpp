7fce4ccc6bfe6e964d37b6a249dcce0e40b1b5c1
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-02-21 13:16:29 +0530

    update

--- 2019/LevelUpDecBatch/l004_Graph/questions.cpp
+++ 2019/LevelUpDecBatch/l004_Graph/questions.cpp
@@ -115,3 +115,53 @@ int islandPerimeter(vector<vector<int>> &grid)
 
     return count * 4 - nbrs;
 }
+
+void surroundedRegionDFS(int i, int j, int n, int m, vector<vector<char>> &grid, vector<vector<int>> &dir)
+{
+    grid[i][j] = '$';
+    for (int d = 0; d < dir.size(); d++)
+    {
+        int r = i + dir[d][0];
+        int c = j + dir[d][1];
+
+        if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 'O')
+            surroundedRegionDFS(r, c, n, m, grid, dir);
+    }
+}
+
+void solve(vector<vector<char>> &grid)
+{
+    if (grid.size() == 0 || grid[0].size() == 0)
+        return;
+
+    int area = 0;
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<vector<int>> dir{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    for (int i = 0; i < n; i++)
+    {
+        if (grid[i][0] == 'O')
+            surroundedRegionDFS(i, 0, n, m, grid, dir);
+        if (grid[i][m - 1] == 'O')
+            surroundedRegionDFS(i, m - 1, n, m, grid, dir);
+    }
+
+    for (int j = 0; j < m; j++)
+    {
+        if (grid[0][j] == 'O')
+            surroundedRegionDFS(0, j, n, m, grid, dir);
+        if (grid[n - 1][j] == 'O')
+            surroundedRegionDFS(n - 1, j, n, m, grid, dir);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] == 'O')
+                grid[i][j] = 'X';
+            if (grid[i][j] == '$')
+                grid[i][j] = 'O';
+        }
+    }
+}
