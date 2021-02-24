cc4a9b817f79155867976ceba7833d6ee480df16
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-02-24 21:02:48 +0530

    update

--- 2020/GTBIT/L001_FoundationScratch/l007_LinkedList/linkedlistClass/client.java
+++ 2020/GTBIT/L001_FoundationScratch/l007_LinkedList/linkedlistClass/client.java
@@ -1,6 +1,6 @@
 public class client {
     public static void main(String[] args) throws Exception {
-        linkedlist2 ll = new linkedlist2();
+        linkedlist ll = new linkedlist2();
 
         for (int i = 1; i <= 10; i++) {
             ll.addLast(i * 10);
