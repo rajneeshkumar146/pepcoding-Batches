97b2f1dedcbbadc96538902cb636bcfd15faa44e
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-01 22:04:06 +0530

    update

--- 2020/GTBIT/L001_FoundationScratch/l007_LinkedList/questions.java
+++ 2020/GTBIT/L001_FoundationScratch/l007_LinkedList/questions.java
@@ -199,4 +199,75 @@ public class questions {
 
         return mergeTwoSortedLists(firstHalf, secondHalf);
     }
+
+    public static LinkedList addTwoLists(LinkedList one, LinkedList two) {
+        one.reversePI();
+        two.reversePI();
+
+        LinkedList ans = new LinkedList();
+
+        Node c1 = one.head;
+        Node c2 = two.head;
+        int carry = 0;
+        while (c1 != null || c2 != null || carry != 0) {
+            int sum = (c1 != null ? c1.data : 0) + (c2 != null ? c2.data : 0) + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            ans.addFirst(sum);
+
+            if (c1 != null)
+                c1 = c1.next;
+            if (c2 != null)
+                c2 = c2.next;
+
+        }
+
+        one.reversePI();
+        two.reversePI();
+
+        return ans;
+    }
+
+    public static int addTwoLists(Node node1, Node node2, int s1, int s2, LinkedList ans) {
+
+        if (s1 == 0 && s2 == 0) {
+            return 0;
+        }
+
+        if (s1 > s2) {
+            int carry = addTwoLists(node1.next, node2, s1 - 1, s2, ans);
+            int sum = carry + node1.data + 0;
+            carry = sum / 10;
+            ans.addFirst(sum % 10);
+            return carry;
+
+        } else {
+            int carry = addTwoLists(node1.next, node2.next, s1 - 1, s2 - 1, ans);
+            int sum = carry + node1.data + node2.data;
+            carry = sum / 10;
+            ans.addFirst(sum % 10);
+            return carry;
+        }
+    }
+
+    public static LinkedList addTwoLists(LinkedList one, LinkedList two) {
+
+        LinkedList ans = new LinkedList();
+
+        int carry = 0;
+
+        if (one.size() > two.size())
+            carry = addTwoLists(one.head, two.head, one.size(), two.size(), ans);
+        else
+            carry = addTwoLists(two.head, one.head, two.size(), one.size(), ans);
+
+        if (carry == 1) {
+            ans.addFirst(1);
+        }
+
+        return ans;
+    }
+
 }
