96f0433bd867392928a91b45999338a43cb6284a
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-02 19:35:03 +0530

    update

--- 2019/LevelUpDecBatch/l004_Graph/questions.cpp
+++ 2019/LevelUpDecBatch/l004_Graph/questions.cpp
@@ -637,3 +637,47 @@ string smallestEquivalentString(string A, string B, string S)
 
     return ans;
 }
+
+//839
+bool isSimilar(string &s1, string &s2)
+{
+    int count = 0;
+    for (int i = 0; i < s1.length(); i++)
+    {
+        if (s1[i] != s2[i] && ++count > 2)
+            return false;
+    }
+
+    return true;
+}
+
+int numSimilarGroups(vector<string> &strs)
+{
+
+    int count = strs.size();
+    int n = strs.size();
+
+    par.resize(n);
+    for (int i = 0; i < n; i++)
+        par[i] = i;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (isSimilar(strs[i], strs[j]))
+            {
+                int p1 = findPar(i);
+                int p2 = findPar(j);
+
+                if (p1 != p2)
+                {
+                    par[p1] = p2;
+                    count--;
+                }
+            }
+        }
+    }
+
+    return count;
+}
--- 2019/LevelUpDecBatch/l004_Graph/questions.java
+++ 2019/LevelUpDecBatch/l004_Graph/questions.java
@@ -50,6 +50,7 @@ public class questions {
     }
 
     int[] par;
+
     int findPar(int u) {
         return par[u] == -1 ? u : (par[u] = findPar(par[u]));
     }
@@ -72,6 +73,44 @@ public class questions {
 
         return new int[0];
 
+    }
+
+    public boolean isSimilar(String s1, String s2) {
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i) && ++count > 2)
+                return false;
+        }
+
+        return true;
+
+    }
+
+    public int numSimilarGroups(String[] strs) {
+        int count = strs.length;
+        int n = strs.length;
+
+        par = new int[n];
+        for (int i = 0; i < n; i++)
+            par[i] = i;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j])) {
+                    int p1 = findPar(i);
+                    int p2 = findPar(j);
+
+                    if (p1 != p2) {
+                        par[p1] = p2;
+                        count--;
+                    }
+                }
+
+            }
+
+        }
+
+        return count;
     }
 
 }
