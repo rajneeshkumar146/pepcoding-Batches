22242158f6375cf4441a6726cab097643ce27602
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-06 13:14:58 +0530

    update

--- 2019/LevelUpDecBatch/l004_Graph/questions.cpp
+++ 2019/LevelUpDecBatch/l004_Graph/questions.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -992,4 +994,66 @@ void mrPresident()
     });
 
     cout << mrPresident(n, edges, k) << endl;
+}
+
+
+//815
+int numBusesToDestination(vector<vector<int>> &routes, int src, int dest)
+{
+
+    if (src == dest)
+        return 0;
+    int n = routes.size();
+    unordered_map<int, vector<int>> busStandMapping;
+    int busNumber = 0;
+    for (vector<int> &busStandList : routes)
+    {
+        for (int busStand : busStandList)
+        {
+            busStandMapping[busStand].push_back(busNumber);
+        }
+        busNumber++;
+    }
+
+    unordered_set<int> isBusStandSeen;
+    vector<bool> isBusSeen(n, false);
+
+    queue<int> que;
+    que.push(src);
+    isBusStandSeen.insert(src);
+
+    int level = 0;
+    while (que.size() != 0)
+    {
+        int size = que.size();
+        while (size-- > 0)
+        {
+            int busStand = que.front();
+            que.pop();
+
+            vector<int> allBuses = busStandMapping[busStand];
+            for (int busNo : allBuses)
+            {
+                if (isBusSeen[busNo])
+                    continue;
+
+                for (int bs : routes[busNo]) // bs is bus stand
+                {
+                    if (isBusStandSeen.find(bs) == isBusStandSeen.end())
+                    {
+                        que.push(bs);
+                        isBusStandSeen.insert(bs);
+
+                        if (bs == dest)
+                            return level + 1;
+                    }
+                }
+
+                isBusSeen[busNo] = true;
+            }
+        }
+        level++;
+    }
+
+    return -1;
 }
