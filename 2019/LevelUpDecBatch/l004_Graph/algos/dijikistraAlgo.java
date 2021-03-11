b410e640866bb978a55e97f1db3f19c29d4e128b
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-11 20:16:16 +0530

    update

--- 2019/LevelUpDecBatch/l004_Graph/algos/dijikistraAlgo.java
+++ 2019/LevelUpDecBatch/l004_Graph/algos/dijikistraAlgo.java
@@ -103,4 +103,36 @@ public class dijikistraAlgo {
             }
         }
     }
+
+    // Bellman Ford.
+    // {{u,v,w}.....}
+    public static void bellmanFord(int src, int[][] edges, int N) {
+        int[] dis = new int[N];
+        Arrays.fill(dis, (int) 1e9);
+
+        dis[src] = 0;
+        boolean isNegativeCycle = false;
+
+        for (int EdgeCount = 1; EdgeCount <= N; EdgeCount++) {
+            int[] ndis = new int[N];
+            for (int i = 0; i < N; i++)
+                ndis[i] = dis[i];
+
+            for (int[] e : edges) {
+                int u = e[0], v = e[1], w = e[2];
+                if (dis[u] != (int) 1e9 && dis[u] + w < ndis[v]) {
+                    if (EdgeCount == N) {
+                        isNegativeCycle = true;
+                        break;
+                    }
+
+                    ndis[v] = dis[u] + w;
+                }
+            }
+
+            dis = ndis;
+        }
+
+        
+    }
 }
