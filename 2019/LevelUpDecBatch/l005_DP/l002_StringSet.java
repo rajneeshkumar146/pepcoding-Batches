2a3555390afa1a1e413825888bb3008be7a15be8
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-21 12:51:58 +0530

    update

--- 2019/LevelUpDecBatch/l005_DP/l002_StringSet.java
+++ 2019/LevelUpDecBatch/l005_DP/l002_StringSet.java
@@ -285,4 +285,64 @@ public class l002_StringSet {
         return minDistance(word1, word2, n, m, dp);
     }
 
+    // 44
+    // -1 -> default, 0 -> false, 1 -> true
+    public int isMatch(String s, String p, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            if (n == 0 && m == 0)
+                return dp[n][m] = 1;
+            else if (m == 1 && p.charAt(m - 1) == '*')
+                return dp[n][m] = 1;
+            else
+                return dp[n][m] = 0;
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        char ch1 = s.charAt(n - 1);
+        char ch2 = p.charAt(m - 1);
+
+        if (ch1 == ch2 || ch2 == '?')
+            return dp[n][m] = isMatch(s, p, n - 1, m - 1, dp);
+        else if (ch2 == '*') {
+            boolean res = false;
+            res = res || (isMatch(s, p, n - 1, m, dp) == 1); // map to character
+            res = res || (isMatch(s, p, n, m - 1, dp) == 1); // map to empty String
+
+            return dp[n][m] = res ? 1 : 0;
+        } else
+            return dp[n][m] = 0;
+    }
+
+    public String removeStars(String s) {
+        if (s.length() == 0)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+
+        int i = 1;
+        while (i < s.length()) {
+            while (i < s.length() && sb.charAt(sb.length() - 1) == '*' && s.charAt(i) == '*')
+                i++;
+            if (i < s.length())
+                sb.append(s.charAt(i));
+            i++;
+        }
+
+        return sb.toString();
+    }
+
+    public boolean isMatch(String s, String p) {
+        p = removeStars(p);
+        int n = s.length();
+        int m = p.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        return isMatch(s, p, n, m, dp) == 1;
+    }
+
 }
