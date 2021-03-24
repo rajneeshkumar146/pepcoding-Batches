7e6a2983afd1ab2f10b20384c0412b779b753ef3
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-24 21:49:11 +0530

    update

--- 2020/pp-mar 14/lecture_001_Basic/l004_Arrays.java
+++ 2020/pp-mar 14/lecture_001_Basic/l004_Arrays.java
@@ -171,12 +171,33 @@ public class l004_Arrays {
         reverseOfArray(arr, r, n - 1);
     }
 
-    public static void sumOfTwoArray(int[] arr1,int[] arr2){
+    public static void sumOfTwoArray(int[] arr1, int[] arr2) {
+        int n = arr1.length;
+        int m = arr2.length;
+        int p = Math.max(n, m) + 1;
+        int[] ans = new int[p];
+
+        int i = n - 1, j = m - 1, k = p - 1, carry = 0;
+        while (k >= 0) {
+            int sum = carry + (i >= 0 ? arr1[i] : 0) + (j >= 0 ? arr2[j] : 0);
+            ans[k] = sum % 10;
+            carry = sum / 10;
+
+            i--;
+            j--;
+            k--;
+        }
+
+        for (int idx = 0; idx < p; idx++) {
+            if (idx == 0 && ans[idx] == 0)
+                continue;
+            System.out.print(ans[idx]);
+        }
 
     }
 
     // arr1 as number > arr2 as number
-    public static void subtractOfTwoArray(int[] arr1,int[] arr2){
+    public static void subtractOfTwoArray(int[] arr1, int[] arr2) {
 
     }
 
