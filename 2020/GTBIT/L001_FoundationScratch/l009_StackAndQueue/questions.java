429d46a50074130d0e012972f2e69f723f915512
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-27 14:52:12 +0530

    update

--- 2020/GTBIT/L001_FoundationScratch/l009_StackAndQueue/questions.java
+++ 2020/GTBIT/L001_FoundationScratch/l009_StackAndQueue/questions.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Arrays;
 
 public class questions {
     public static boolean isBalancedBrackets(String str) {
@@ -23,6 +25,47 @@ public class questions {
         }
 
         return st.size() == 0;
+    }
+     
+    // NG : Next Greater
+    // NS : Next Smaller
+    // OR : on Right
+    // OL : on Left
+    public static int[] NGOR(int[] arr) {
+        int n = arr.length;
+        LinkedList<Integer> st = new LinkedList<>();
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        for (int i = 0; i < n; i++) {
+            while (st.size() != 0 && arr[st.getFirst()] < arr[i]) {
+                ans[st.removeFirst()] = arr[i];
+            }
+
+            st.addFirst(i);
+        }
+        return ans;
+    }
+
+    public static int[] NGOL(int[] arr) {
+        int n = arr.length;
+        LinkedList<Integer> st = new LinkedList<>();
+        int[] ans = new int[n];
+
+    }
+
+    public static int[] NSOR(int[] arr) {
+        int n = arr.length;
+        LinkedList<Integer> st = new LinkedList<>();
+        int[] ans = new int[n];
+
+    }
+
+    public static int[] NSOL(int[] arr) {
+        int n = arr.length;
+        LinkedList<Integer> st = new LinkedList<>();
+        int[] ans = new int[n];
+
     }
 
 }
