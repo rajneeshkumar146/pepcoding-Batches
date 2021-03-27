83e9567892df0d44d8f1dfa6e9d26b0f3d70004a
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-27 17:49:53 +0530

    update

--- 2020/GTBIT/L001_FoundationScratch/l009_StackAndQueue/questions.java
+++ 2020/GTBIT/L001_FoundationScratch/l009_StackAndQueue/questions.java
@@ -26,7 +26,7 @@ public class questions {
 
         return st.size() == 0;
     }
-     
+
     // NG : Next Greater
     // NS : Next Smaller
     // OR : on Right
@@ -51,21 +51,114 @@ public class questions {
         int n = arr.length;
         LinkedList<Integer> st = new LinkedList<>();
         int[] ans = new int[n];
+        Arrays.fill(ans, -1);
 
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.size() != 0 && arr[st.getFirst()] < arr[i]) {
+                ans[st.removeFirst()] = arr[i];
+            }
+
+            st.addFirst(i);
+        }
+        return ans;
     }
 
     public static int[] NSOR(int[] arr) {
         int n = arr.length;
         LinkedList<Integer> st = new LinkedList<>();
         int[] ans = new int[n];
+        Arrays.fill(ans, n);
 
+        for (int i = 0; i < n; i++) {
+            while (st.size() != 0 && arr[st.getFirst()] > arr[i]) {
+                ans[st.removeFirst()] = i;
+            }
+
+            st.addFirst(i);
+        }
+        return ans;
     }
 
     public static int[] NSOL(int[] arr) {
         int n = arr.length;
         LinkedList<Integer> st = new LinkedList<>();
         int[] ans = new int[n];
+        Arrays.fill(ans, -1);
 
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.size() != 0 && arr[st.getFirst()] > arr[i]) {
+                ans[st.removeFirst()] = i;
+            }
+
+            st.addFirst(i);
+        }
+        return ans;
     }
+
+    public static int largestArea(int[] arr) {
+        int n = arr.length;
+        int[] nsol = NSOL(arr);
+        int[] nsor = NSOR(arr);
+
+        int area = 0;
+        for (int i = 0; i < n; i++) {
+            int h = arr[i];
+            int w = nsor[i] - nsol[i] - 1;
+            area = Math.max(area, h * w);
+        }
+
+        return area;
+    }
+
+    public static boolean duplicateBrackets(String str) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ')') {
+                st.push(str.charAt(i));
+                continue;
+            }
+
+            boolean flag = false;
+            while (st.size() != 0 && st.peek() != '(') {
+                flag = true;
+                st.pop();
+            }
+
+            if (!flag && st.size() != 0 && st.peek() == '(')
+                return true;
+
+            if (st.size() != 0)
+                st.pop();
+        }
+
+        return false;
+    }
+
+    public static boolean duplicateBrackets2(String str) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ')') {
+                st.push(str.charAt(i));
+                continue;
+            }
+
+            boolean isThereExpression = false;
+            while (st.peek() != '(') {
+                isThereExpression = true;
+                st.pop();
+            }
+
+            if (!isThereExpression)
+                return true;
+
+            st.pop();
+        }
+
+        return false;
+    }
+
+    // hw : leetcode 42
 
 }
