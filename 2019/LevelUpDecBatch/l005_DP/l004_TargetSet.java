c67302a7745d46ed715b32f4df16077b658120d0
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-28 13:21:43 +0530

    update

--- 2019/LevelUpDecBatch/l005_DP/l004_TargetSet.java
+++ 2019/LevelUpDecBatch/l005_DP/l004_TargetSet.java
@@ -102,9 +102,115 @@ public class l004_TargetSet {
     }
 
     // Leetcode 518,322,377
+    // 377
+    public int combinationSum4(int[] arr, int Tar) {
+        int[] dp = new int[Tar + 1];
+        dp[0] = 1;
+        for (int tar = 0; tar <= Tar; tar++) {
+            for (int ele : arr) {
+                if (tar - ele >= 0) {
+                    dp[tar] += dp[tar - ele];
+                }
+            }
+        }
+
+        return dp[Tar];
+    }
+
+    // 518
+    public int change(int Tar, int[] arr) {
+        int[] dp = new int[Tar + 1];
+        dp[0] = 1;
+        for (int ele : arr) {
+            for (int tar = ele; tar <= Tar; tar++) {
+                if (tar - ele >= 0) {
+                    dp[tar] += dp[tar - ele];
+                }
+            }
+        }
+
+        return dp[Tar];
+
+    }
+
+    // 322
+    public int coinChange(int[] coins, int amount, int[] dp) {
+        if (amount == 0) {
+            return 0;
+        }
+
+        if (dp[amount] != (int) 1e9)
+            return dp[amount];
+
+        int minCoins = (int) 1e8;
+        for (int ele : coins) {
+            if (amount - ele >= 0) {
+                minCoins = Math.min(minCoins, coinChange(coins, amount - ele, dp) + 1);
+
+            }
+        }
+
+        return dp[amount] = minCoins;
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, (int) 1e9);
+
+        int ans = coinChange(coins, amount, dp);
+
+        return ans != (int) 1e8 ? ans : -1;
+    }
+
+    // https://www.geeksforgeeks.org/find-number-of-solutions-of-a-linear-equation-of-n-variables/
+    public static int numberOfSolution(int[] arr, int Tar) {
+        int[] dp = new int[Tar + 1];
+
+        for (int tar = 0; tar <= Tar; tar++) {
+            for (int ele : arr) {
+                if (tar - ele >= 0)
+                    dp[tar] += dp[tar - ele];
+            }
+        }
+
+        return dp[Tar];
+    }
+
+    public static int numberOfSolution(int[] arr, int Tar, int aTar, int idx, int[] coff) {
+        if (Tar == 0) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + "(" + coff[i] + ")");
+                if (i != arr.length - 1)
+                    System.out.print(" + ");
+            }
+
+            System.out.println(" = " + aTar);
+
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = idx; i < arr.length; i++) {
+            if (Tar - arr[i] >= 0) {
+                coff[i]++;
+                count += numberOfSolution(arr, Tar - arr[i], aTar, i, coff);
+                coff[i]--;
+            }
+        }
+
+        return count;
+    }
+
+    public static void numberOfSolution() {
+        int[] arr = { 2, 3, 5 , 7};
+        int tar = 10;
+        System.out.println(numberOfSolution(arr, tar, tar, 0, new int[arr.length]));
+    }
 
     public static void main(String[] args) {
-        coinChnage();
+        // coinChnage();
+        numberOfSolution();
 
     }
 
