aac96079fdba6d98bcdb7499840c466c25733f23
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-03-30 20:12:31 +0530

    update

---
+++ 2020/pp-mar 14/lecture_002_BinaryForms/BTD.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class BTD {
+    // decimal to Binary
+
+    public static Scanner scn = new Scanner(System.in);
+
+    public static int binaryToDecimal(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 2020/pp-mar 14/lecture_002_BinaryForms/DTB.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class DTB {
+    // decimal to Binary
+
+    public static Scanner scn = new Scanner(System.in);
+
+    public static int decimalToBinary(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
