b83256bbdd39de24cfbf042fc6b7c516487e8b96
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-04-03 12:56:37 +0530

    update

--- 2019/LevelUpDecBatch/l005_DP/l002_StringSet.java
+++ 2019/LevelUpDecBatch/l005_DP/l002_StringSet.java
@@ -345,4 +345,29 @@ public class l002_StringSet {
         return isMatch(s, p, n, m, dp) == 1;
     }
 
+    public String longestPalindrome(String str) {
+
+        int n = str.length();
+        int[][] dp = new int[n][n];
+        int len = 0;
+        int si = 0;
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (gap == 0)
+                    dp[i][j] = 1;
+                else if (gap == 1)
+                    dp[i][j] = str.charAt(i) == str.charAt(j) ? 2 : 0;
+                else
+                    dp[i][j] = str.charAt(i) == str.charAt(j) && dp[i + 1][j - 1] != 0 ? dp[i + 1][j - 1] + 2 : 0;
+
+                if (dp[i][j] > len) {
+                    len = dp[i][j];
+                    si = i;
+                }
+            }
+        }
+
+        return str.substring(si, si + len);
+    }
+
 }
