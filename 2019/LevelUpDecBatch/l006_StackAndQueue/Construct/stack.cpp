2564d6534f287401d7ba4cdef35edd1bb3c84030
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-04-10 09:58:20 +0530

    update

---
+++ 2019/LevelUpDecBatch/l006_StackAndQueue/Construct/stack.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+
+class stack
+{
+public:
+    int size()
+    {
+        return -1;
+    }
+
+    bool isEmpty()
+    {
+        return false;
+    }
+
+    void push(int data)
+    {
+    }
+
+    int top()
+    {
+        return -1;
+    }
+
+    int pop()
+    {
+        return -1;
+    }
+};
