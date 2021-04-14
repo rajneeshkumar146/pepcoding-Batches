1e08da87842b59b9ca40a87d64eb008b2831bdec
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-04-14 20:57:55 +0530

    update

--- 2020/pp-mar 14/lecture_005_Recursion/l002.java
+++ 2020/pp-mar 14/lecture_005_Recursion/l002.java
@@ -17,15 +17,37 @@ public class l002 {
     }
 
     public static int maximum(int[] arr, int idx) {
-
+        if (idx == arr.length)
+            return -(int) 1e9;
+        int recMax = maximum(arr, idx + 1);
+        return Math.max(recMax, arr[idx]);
     }
 
     public static int minimum(int[] arr, int idx) {
+        if (idx == arr.length)
+            return (int) 1e9;
 
+        int recMin = minimum(arr, idx + 1);
+        return Math.min(recMin, arr[idx]);
     }
 
     public static boolean findData(int[] arr, int idx, int data) {
+        if (idx == arr.length)
+            return false;
+        if (arr[idx] == data)
+            return true;
+        return findData(arr, idx + 1, data);
+    }
 
+    public static boolean findData2(int[] arr, int idx, int data) {
+        if (idx == arr.length)
+            return false;
+
+        boolean recRes = findData2(arr, idx + 1, data);
+        if (recRes)
+            return true;
+
+        return arr[idx] == data;
     }
 
     public static int firstIdx(int[] arr, int idx, int data) {
@@ -36,8 +58,12 @@ public class l002 {
 
     }
 
+    public static int[] allIndex(int[] arr,int idx,int data,??){   // ?? -> X(ArrayList, Array)
+
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 12, 3, 2,4, 34, 3, 4, 2, 2, 76 };
+        int[] arr = { 12, 3, 2, 4, 34, 3, 4, 2, 2, 76 };
         printReverseArray(arr, 0);
         System.out.println(maximum(arr, 0));
         System.out.println(minimum(arr, 0));
