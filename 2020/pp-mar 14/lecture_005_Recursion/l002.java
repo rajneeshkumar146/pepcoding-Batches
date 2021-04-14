b57d941c4f4f4f05214674f7ce5c7fd1750edcbe
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-04-14 19:44:26 +0530

    update

--- 2020/pp-mar 14/lecture_005_Recursion/l002.java
+++ 2020/pp-mar 14/lecture_005_Recursion/l002.java
@@ -1,16 +1,48 @@
 public class l002 {
 
-    public static void printArray(int[] arr,???){
+    public static void printArray(int[] arr, int idx) {
+        if (idx == arr.length)
+            return;
+
+        System.out.println(arr[idx]);
+        printArray(arr, idx + 1);
+    }
+
+    public static void printReverseArray(int[] arr, int idx) {
+        if (idx == arr.length)
+            return;
+
+        printReverseArray(arr, idx + 1);
+        System.out.println(arr[idx]);
+    }
+
+    public static int maximum(int[] arr, int idx) {
 
     }
 
-    public static void printReverseArray(int[] arr,???){
+    public static int minimum(int[] arr, int idx) {
+
+    }
+
+    public static boolean findData(int[] arr, int idx, int data) {
+
+    }
+
+    public static int firstIdx(int[] arr, int idx, int data) {
+
+    }
+
+    public static int lastIdx(int[] arr, int idx, int data) {
 
     }
 
     public static void main(String[] args) {
-        int[] arr = { 2, 3, 4, 56, 78, 98, -1, -3, -23, 456, 4564, 4 };
-        
-
+        int[] arr = { 12, 3, 2,4, 34, 3, 4, 2, 2, 76 };
+        printReverseArray(arr, 0);
+        System.out.println(maximum(arr, 0));
+        System.out.println(minimum(arr, 0));
+        System.out.println(findData(arr, 0, 2));
+        System.out.println(firstIdx(arr, 0, 2));
+        System.out.println(lastIdx(arr, 0, 2));
     }
 }
