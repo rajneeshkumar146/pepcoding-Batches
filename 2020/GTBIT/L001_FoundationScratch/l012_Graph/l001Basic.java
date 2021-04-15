00cd4f7d699ef6b8a503f9d8fdcaa81c886d8a68
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-04-15 15:54:43 +0530

    update

--- 2020/GTBIT/L001_FoundationScratch/l012_Graph/l001Basic.java
+++ 2020/GTBIT/L001_FoundationScratch/l012_Graph/l001Basic.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class l001Basic {
 
     public static class Edge {
@@ -14,10 +15,26 @@ public class l001Basic {
     public static int N = 7;
     public static ArrayList<Edge>[] graph = new ArrayList[N];
 
+    public static void addEdge(int u, int v, int w) {
+        graph[u].add(new Edge(v, w));
+        graph[v].add(new Edge(u, w));
+    }
+
+    public static void display(){
+
+    }
+
     public static void main(String[] args) {
-
-        addEdge(0,1,10);
-
+        for (int i = 0; i < N; i++)
+            graph[i] = new ArrayList<>();
+        addEdge(0, 1, 10);
+        addEdge(0, 3, 10);
+        addEdge(1, 2, 10);
+        addEdge(2, 3, 10);
+        addEdge(3, 4, 10);
+        addEdge(4, 5, 10);
+        addEdge(4, 6, 10);
+        addEdge(5, 6, 10);
     }
 
 }
