4013b0cd1456da9ed70f5dc26a78a9e105757ef4
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-04-27 19:16:13 +0530

    update

--- 2020/pp-mar 14/lecture_005_Recursion/l004RWU.java
+++ 2020/pp-mar 14/lecture_005_Recursion/l004RWU.java
@@ -144,6 +144,57 @@ public class l004RWU {
         return count;
     }
 
+    public static int permutation(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            String ros = str.substring(0, i) + str.substring(i + 1);
+            count += permutation(ros, ans + str.charAt(i));
+        }
+
+        return count;
+    }
+
+    public static int permutationWithoutDuplicate(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        char prev = '$';
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != prev) {
+                String ros = str.substring(0, i) + str.substring(i + 1);
+                count += permutationWithoutDuplicate(ros, ans + str.charAt(i));
+            }
+            prev = str.charAt(i);
+        }
+
+        return count;
+
+    }
+
+    public static void permutationWithoutDuplicate(String str) {
+        StringBuilder sb = new StringBuilder();
+        int[] freq = new int[26];
+        for (int i = 0; i < str.length(); i++)
+            freq[str.charAt(i) - 'a']++;
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < freq[i]; j++) {
+                sb.append((char) (i + 'a'));
+            }
+        }
+
+        // System.out.println(sb);
+        System.out.println(permutationWithoutDuplicate(sb.toString(),""));
+    }
+
     public static void main(String[] args) {
         // ArrayList<String> ans = new ArrayList<>();
         // System.out.println(subsequence("abc", "", ans));
@@ -152,7 +203,11 @@ public class l004RWU {
         // int[] arr = { 2, 5, 3, 1 };
         // System.out.println(boardPath(arr, 10, ""));
 
-        System.out.println(mazePath_MultiHVD(0, 0, 2, 2, ""));
+        // System.out.println(mazePath_MultiHVD(0, 0, 2, 2, ""));
+
+        // System.out.println(permutation("aba", ""));
+        permutationWithoutDuplicate("abab");
+
     }
 
 }
