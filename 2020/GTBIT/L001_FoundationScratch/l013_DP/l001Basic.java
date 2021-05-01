0473dc7eeca45bb39a8d8157b9b65dbcd63ac9a6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-01 14:56:05 +0530

    upadte

---
+++ 2020/GTBIT/L001_FoundationScratch/l013_DP/l001Basic.java
@@ -0,0 +1,66 @@
+public class l001Basic {
+    public static void print(int[] arr) {
+        for (int ele : arr)
+            System.out.print(ele + " ");
+        System.out.println();
+    }
+
+    public static void print2D(int[][] arr) {
+        for (int[] a : arr) {
+            print(a);
+        }
+
+        System.out.println();
+    }
+
+    public static int fibo_memo(int n, int[] dp) {
+        if (n <= 1) {
+            return dp[n] = n;
+        }
+
+        if (dp[n] != 0)
+            return dp[n];
+
+        int ans = fibo_memo(n - 1, dp) + fibo_memo(n - 2, dp);
+        return dp[n] = ans;
+    }
+
+    public static int fibo_DP(int N, int[] dp) {
+        for (int n = 0; n <= N; n++) {
+            if (n <= 1) {
+                dp[n] = n;
+                continue;
+            }
+
+            int ans = dp[n - 1] + dp[n - 2];// fibo_memo(n - 1, dp) + fibo_memo(n - 2, dp);
+            dp[n] = ans;
+        }
+
+        return dp[N];
+    }
+
+    public static int fibo_Opti(int n) {
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+
+        return a;
+    }
+
+    public static void fibo() {
+        int n = 6;
+        int[] dp = new int[n + 1];
+        fibo_DP(n, dp);
+        System.out.println(fibo_Opti(n));
+
+        print(dp);
+    }
+
+    public static void main(String[] args) {
+        fibo();
+    }
+}
