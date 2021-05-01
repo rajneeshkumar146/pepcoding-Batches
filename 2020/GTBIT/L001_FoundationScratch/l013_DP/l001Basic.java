23038b056f3b18086bfbf12a3073546d69c6a098
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-01 17:48:00 +0530

    upadte

--- 2020/GTBIT/L001_FoundationScratch/l013_DP/l001Basic.java
+++ 2020/GTBIT/L001_FoundationScratch/l013_DP/l001Basic.java
@@ -60,6 +60,106 @@ public class l001Basic {
         print(dp);
     }
 
+    public int climbStairs(int n, int[] dp) {
+        if (n <= 1) {
+            return dp[n] = 1;
+        }
+
+        if (dp[n] != 0)
+            return dp[n];
+
+        int steps = climbStairs(n - 1, dp) + climbStairs(n - 2, dp);
+        return dp[n] = steps;
+    }
+
+    public int climbStairs_DP(int N, int[] dp) {
+        for (int n = 0; n <= N; n++) {
+            if (n <= 1) {
+                dp[n] = 1;
+                continue;
+            }
+
+            int steps = dp[n - 1] + dp[n - 2];// climbStairs(n-1,dp) + climbStairs(n-2,dp);
+            dp[n] = steps;
+        }
+
+        return dp[N];
+    }
+
+    public int climbStairs_Opti(int n) {
+        int a = 1, b = 1;
+        for (int i = 0; i < n; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+
+        return a;
+
+    }
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        // int ans = climbStairs(n,dp);
+        // int ans = climbStairs_DP(n,dp);
+        int ans = climbStairs_Opti(n);
+
+        return ans;
+    }
+
+    public int minCostClimbingStairs(int[] cost, int n, int[] dp) {
+        if (n <= 1) {
+            return dp[n] = cost[n];
+        }
+        if (dp[n] != 0)
+            return dp[n];
+
+        int minCostOfOneStep = minCostClimbingStairs(cost, n - 1, dp);
+        int minCostOfTwoStep = minCostClimbingStairs(cost, n - 2, dp);
+
+        int ans = Math.min(minCostOfOneStep, minCostOfTwoStep) + (n != cost.length ? cost[n] : 0);
+
+        return dp[n] = ans;
+    }
+
+    public int minCostClimbingStairs_DP(int[] cost, int N, int[] dp) {
+
+        for (int n = 0; n <= N; n++) {
+            if (n <= 1) {
+                dp[n] = cost[n];
+                continue;
+            }
+
+            int minCostOfOneStep = dp[n - 1];// minCostClimbingStairs(cost, n - 1, dp);
+            int minCostOfTwoStep = dp[n - 2];// minCostClimbingStairs(cost, n - 2, dp);
+
+            int ans = Math.min(minCostOfOneStep, minCostOfTwoStep) + (n != cost.length ? cost[n] : 0);
+
+            dp[n] = ans;
+        }
+
+        return dp[N];
+    }
+
+    public int minCostClimbingStairs_Opti(int[] cost, int N) {
+
+        int a = cost[0], b = cost[1];
+        for (int i = 2; i <= N; i++) {
+            int minVal = Math.min(a, b) + (i != cost.length ? cost[i] : 0);
+            a = b;
+            b = minVal;
+        }
+        return b;
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        int ans = minCostClimbingStairs(cost, n, dp);
+        return ans;
+
+    }
+
     public static void main(String[] args) {
         fibo();
     }
