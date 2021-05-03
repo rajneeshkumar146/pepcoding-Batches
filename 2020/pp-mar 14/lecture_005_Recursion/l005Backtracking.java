0f2818705e0a97da6f6b95e1f0465d3c009a62f8
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-03 22:05:13 +0530

    update

--- 2020/pp-mar 14/lecture_005_Recursion/l005Backtracking.java
+++ 2020/pp-mar 14/lecture_005_Recursion/l005Backtracking.java
@@ -19,11 +19,77 @@ public class l005Backtracking {
         return count;
     }
 
-    public static void main(String[] args) {
-        int[][] dir = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-        String[] dirS = { "t", "r", "d", "l" };
+    // Mark
+    // for all unvisited Nbr's
+    // ---->call dfs for Nbr's
+    // unMark
 
-        System.out.println(mazePath(0, 0, 2, 2, "", dir, dirS));
+    public static int floodFill(int sr, int sc, int[][] board, String ans, int[][] dir, String[] dirS) {
+        int n = board.length, m = board[0].length;
+        if (sr == n - 1 && sc == m - 1) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        board[sr][sc] = 1;
+
+        int count = 0;
+
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr +  dir[d][0];
+            int c = sc +  dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < board.length && c < board[0].length) {
+                if (board[r][c] == 0)
+                    count += floodFill(r, c, board, ans + dirS[d], dir, dirS);
+            }
+        }
+
+        board[sr][sc] = 0;
+        return count;
+
+    }
+
+    // 0 -> empty cell, 1 -> blocked cell
+    public static int floodFill_Jump(int sr, int sc, int[][] board, String ans, int[][] dir, String[] dirS, int Radius) {
+        int n = board.length, m = board[0].length;
+        if (sr == n - 1 && sc == m - 1) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        board[sr][sc] = 1;
+
+        int count = 0;
+
+        for (int d = 0; d < dir.length; d++) {
+            for (int rad = 1; rad <= Radius; rad++) {
+                int r = sr + rad * dir[d][0];
+                int c = sc + rad * dir[d][1];
+
+                if (r >= 0 && c >= 0 && r < board.length && c < board[0].length) {
+                    if (board[r][c] == 0)
+                        count += floodFill_Jump(r, c, board, ans + rad + dirS[d], dir, dirS, Radius);
+                } else
+                    break;
+            }
+        }
+
+        board[sr][sc] = 0;
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[][] dir4 = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+        String[] dir4S = { "t", "r", "d", "l" };
+
+        int[][] board = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+
+        int[][] dir8 = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 }, { 1, 1 }, { -1, 1 } };
+        String[] dir8S = { "u", "r", "d", "l", "n", "w", "s", "e" };
+
+        int n = 3, m = 3;
+        System.out.println(floodFill_Jump(0, 0, board, "", dir4, dir4S, Math.max(n, m)));
     }
 
 }
