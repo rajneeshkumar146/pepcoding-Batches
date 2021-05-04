ceefb3513e4a9f780283806c2bb17d16139fd136
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-04 21:23:01 +0530

    update

--- 2020/pp-mar 14/lecture_005_Recursion/l005Backtracking.java
+++ 2020/pp-mar 14/lecture_005_Recursion/l005Backtracking.java
@@ -36,8 +36,8 @@ public class l005Backtracking {
         int count = 0;
 
         for (int d = 0; d < dir.length; d++) {
-            int r = sr +  dir[d][0];
-            int c = sc +  dir[d][1];
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
 
             if (r >= 0 && c >= 0 && r < board.length && c < board[0].length) {
                 if (board[r][c] == 0)
@@ -50,10 +50,9 @@ public class l005Backtracking {
 
     }
 
-
-
     // 0 -> empty cell, 1 -> blocked cell
-    public static int floodFill_Jump(int sr, int sc, int[][] board, String ans, int[][] dir, String[] dirS, int Radius) {
+    public static int floodFill_Jump(int sr, int sc, int[][] board, String ans, int[][] dir, String[] dirS,
+            int Radius) {
         int n = board.length, m = board[0].length;
         if (sr == n - 1 && sc == m - 1) {
             System.out.println(ans);
@@ -92,8 +91,8 @@ public class l005Backtracking {
 
         boolean res = false;
         for (int d = 0; d < dir.length; d++) {
-            int r = sr +  dir[d][0];
-            int c = sc +  dir[d][1];
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
 
             if (r >= 0 && c >= 0 && r < board.length && c < board[0].length) {
                 if (board[r][c] == 0)
@@ -105,17 +104,73 @@ public class l005Backtracking {
         return res;
     }
 
+    public static int floodFill_longestLen(int sr, int sc, int[][] board, int[][] dir) {
+        int n = board.length, m = board[0].length;
+        if (sr == n - 1 && sc == m - 1) {
+            return 0;
+        }
+
+        board[sr][sc] = 1;
+
+        int longestLen = -1;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < board.length && c < board[0].length) {
+                if (board[r][c] == 0) {
+                    int recAns = floodFill_longestLen(r, c, board, ans + dirS[d], dir, dirS);
+                    if (recAns != -1 && recAns + 1 > longestLen) {
+                        longestLen = recAns + 1;
+                    }
+                }
+            }
+        }
+
+        board[sr][sc] = 0;
+        return longestLen;
+    }
+
+    public static int floodFill_shortestLen(int sr, int sc, int[][] board, int[][] dir) {
+        int n = board.length, m = board[0].length;
+        if (sr == n - 1 && sc == m - 1) {
+            return 0;
+        }
+
+        board[sr][sc] = 1;
+
+        int shortestLen = (int) 1e9;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < board.length && c < board[0].length) {
+                if (board[r][c] == 0) {
+                    int recAns = floodFill_shortestLen(r, c, board, ans + dirS[d], dir, dirS);
+                    if (recAns != (int)1e9 && recAns + 1 < shortestLen) {
+                        shortestLen = recAns + 1;
+                    }
+                }
+            }
+        }
+
+        board[sr][sc] = 0;
+        return shortestLen;
+    }
+
     public static void main(String[] args) {
         int[][] dir4 = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
         String[] dir4S = { "t", "r", "d", "l" };
 
-        int[][] board = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+        int[][] board = { { 0, 0, 0 }, { 0, 0, 1 }, { 0, 0, 0 } };
 
         int[][] dir8 = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 }, { 1, 1 }, { -1, 1 } };
         String[] dir8S = { "u", "r", "d", "l", "n", "w", "s", "e" };
 
         int n = 3, m = 3;
-        System.out.println(floodFill_2(0, 0, board, "", dir4, dir4S));
+        // System.out.println(floodFill_2(0, 0, board, "", dir4, dir4S));
+        System.out.println(floodFill_shortestLen(0,0,arr,dir));
+    
     }
 
 }
