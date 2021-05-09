a19bb828c794b8e0b10723a2595881ee71f366de
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-09 16:44:32 +0530

    update

--- 2020/GTBIT/L001_FoundationScratch/l013_DP/l002StringSet.java
+++ 2020/GTBIT/L001_FoundationScratch/l013_DP/l002StringSet.java
@@ -16,7 +16,7 @@ public class l002StringSet {
         System.out.println();
     }
 
-    //516
+    // 516
     // LPSS : longestPalindromicSubsequence
     public static int LPSS(String str, int i, int j, int[][] dp) {
         if (i >= j) {
@@ -103,7 +103,7 @@ public class l002StringSet {
         }
     }
 
-    //005, 695
+    // 005, 695
     public static int LPS(String str) {
         int n = str.length();
         int[][] dp = new int[n][n];
@@ -134,6 +134,50 @@ public class l002StringSet {
         }
 
         return count;
+    }
+
+    // LCSS : Longest common subsequnece
+    public static int LCSS(String s1, String s2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = 0;
+        }
+        if (dp[n][m] != -1)
+            return dp[n][m];
+        if (s1.charAt(n - 1) == s2.charAt(m - 1))
+            return dp[n][m] = LCSS(s1, s2, n - 1, m - 1, dp) + 1;
+        else
+            return dp[n][m] = Math.max(LCSS(s1, s2, n - 1, m, dp), LCSS(s1, s2, n, m - 1, dp));
+    }
+
+    public static int LCSS_DP(String s1, String s2, int N, int M, int[][] dp) {
+
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+
+                if (n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+
+                if (s1.charAt(n - 1) == s2.charAt(m - 1))
+                    dp[n][m] = dp[n - 1][m - 1] + 1;
+                else
+                    dp[n][m] = Math.max(dp[n - 1][m], dp[n][m - 1]);
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public static void LongestCommonSubsequnece() {
+        String s1, s2;
+        int n = s1.length(), m = s2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        System.out.println(LCSS(s1, s2, n, m, dp));
     }
 
     public static void main(String[] args) {
--- 2020/GTBIT/L001_FoundationScratch/l013_DP/questions.java
+++ 2020/GTBIT/L001_FoundationScratch/l013_DP/questions.java
@@ -70,5 +70,128 @@ public class questions {
         return mazePath_HDV_DP(0, 0, n - 1, m - 1, dir, dp, obstacleGrid);
     }
 
-    
+    // 1035
+    public int maxUncrossedLines(int[] A, int[] B, int N, int M, int[][] dp) {
+
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+
+                if (n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+
+                if (A[n - 1] == B[m - 1])
+                    dp[n][m] = dp[n - 1][m - 1] + 1;
+                else
+                    dp[n][m] = Math.max(dp[n - 1][m], dp[n][m - 1]);
+            }
+        }
+
+        return dp[N][M];
+
+    }
+
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int n = A.length, m = B.length;
+        int[][] dp = new int[n + 1][m + 1];
+
+        return maxUncrossedLines(A, B, n, m, dp);
+    }
+
+    // 72
+    public int minDistance(String s1, String s2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = (n == 0 ? m : n);
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        int insert = minDistance(s1, s2, n, m - 1, dp);
+        int replace = minDistance(s1, s2, n - 1, m - 1, dp);
+        int delete = minDistance(s1, s2, n - 1, m, dp);
+
+        if (s1.charAt(n - 1) == s2.charAt(m - 1))
+            return dp[n][m] = replace;
+        else
+            return dp[n][m] = Math.min(Math.min(insert, replace), delete) + 1;
+    }
+
+    public int minDistance_DP(String s1, String s2, int N, int M, int[][] dp) {
+
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = (n == 0 ? m : n);
+                    continue;
+                }
+
+                int insert = dp[n][m - 1];// minDistance(s1, s2, n, m - 1, dp);
+                int replace = dp[n - 1][m - 1];// minDistance(s1, s2, n - 1, m - 1, dp);
+                int delete = dp[n - 1][m];// minDistance(s1, s2, n - 1, m, dp);
+
+                if (s1.charAt(n - 1) == s2.charAt(m - 1))
+                    dp[n][m] = replace;
+                else
+                    dp[n][m] = Math.min(Math.min(insert, replace), delete) + 1;
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int ans = minDistance_DP(word1, word2, n, m, dp);
+        return ans;
+    }
+
+    // Cost of insert , replace and delete is {2,1,2};
+    public int minCostToConvert_DP(String s1, String s2, int N, int M, int[][] dp) {
+
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = (n == 0 ? m : n);
+                    continue;
+                }
+
+                int insert = dp[n][m - 1];// minDistance(s1, s2, n, m - 1, dp);
+                int replace = dp[n - 1][m - 1];// minDistance(s1, s2, n - 1, m - 1, dp);
+                int delete = dp[n - 1][m];// minDistance(s1, s2, n - 1, m, dp);
+
+                if (s1.charAt(n - 1) == s2.charAt(m - 1))
+                    dp[n][m] = replace;
+                else
+                    dp[n][m] = Math.min(Math.min(insert, replace), delete) + 1;
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public int minCostToConvert(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int ans = minCostToConvert_DP(word1, word2, n, m, dp);
+        return ans;
+    }
+
+    // https://www.geeksforgeeks.org/edit-distance-and-lcs-longest-common-subsequence/?ref=rp
+    // where we are allowed only two operations insert and delete, find edit
+    // distance in this variation.
+
+
+    // Minimum delete required to make a string palindrome.
+
 }
