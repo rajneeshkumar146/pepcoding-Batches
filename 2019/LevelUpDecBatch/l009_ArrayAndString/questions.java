7da0d6098d8162780f2a90b032e9eadd4dba2fab
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-13 22:07:01 +0530

    update

--- 2019/LevelUpDecBatch/l009_ArrayAndString/questions.java
+++ 2019/LevelUpDecBatch/l009_ArrayAndString/questions.java
@@ -60,4 +60,147 @@ public class questions {
 
         return maxArea;
     }
+
+    // 003
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1)
+            return s.length();
+
+        int n = s.length(), si = 0, ei = 0, count = 0, len = 0;
+        int[] freq = new int[128]; // vector<int> freq(128,0);
+
+        int gsi = 0, gei = 0;
+        while (ei < n) {
+            if (freq[s.charAt(ei++)]++ > 0)
+                count++;
+
+            while (count > 0)
+                if (freq[s.charAt(si++)]-- > 1)
+                    count--;
+
+            if (ei - si > len) {
+                len = ei - si;
+                gsi = si;
+                gei = ei;
+            }
+        }
+
+        // System.out.println(s.substring(gsi,gei));
+
+        return len;
+    }
+
+    // 003
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1)
+            return s.length();
+
+        int n = s.length(), si = 0, ei = 0, count = 0, len = 0;
+        int[] freq = new int[128]; // vector<int> freq(128,0);
+
+        while (ei < n) {
+            if (freq[s.charAt(ei)] == 1)
+                count++;
+            freq[s.charAt(ei)]++;
+            ei++;
+
+            while (count > 0) {
+                if (freq[s.charAt(si)] == 2)
+                    count--;
+                freq[s.charAt(si)]--;
+                si++;
+            }
+
+            len = Math.max(len, ei - si);
+        }
+
+        return len;
+    }
+
+    // 159
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        if (s.length() <= 2)
+            return s.length();
+
+        int n = s.length(), si = 0, ei = 0, len = 0, count = 0;
+        int[] freq = new int[128];
+
+        while (ei < n) {
+            if (freq[s.charAt(ei++)]++ == 0)
+                count++;
+
+            while (count > 2) {
+                if (freq[s.charAt(si++)]-- == 1)
+                    count--;
+            }
+
+            len = Math.max(len, ei - si);
+        }
+
+        return len;
+    }
+
+    // 76
+    public String minWindow(String s, String t) {
+
+        int ns = s.length(), nt = t.length();
+        if (ns < nt)
+            return "";
+        int[] freq = new int[128];
+        for (int i = 0; i < nt; i++)
+            freq[t.charAt(i)]++;
+
+        int si = 0, ei = 0, count = nt, len = (int) 1e9, gsi = 0;
+
+        while (ei < ns) {
+            if (freq[s.charAt(ei++)]-- > 0)
+                count--;
+
+            while (count == 0) {
+                if (ei - si < len) {
+                    len = ei - si;
+                    gsi = si;
+                }
+
+                if (freq[s.charAt(si++)]++ == 0)
+                    count++;
+            }
+        }
+
+        return len == (int) 1e9 ? "" : s.substring(gsi, gsi + len);
+    }
+
+    // 76
+    public String minWindow2(String s, String t) {
+
+        int ns = s.length(), nt = t.length();
+        if (ns < nt)
+            return "";
+        int[] freq = new int[128];
+        for (int i = 0; i < nt; i++)
+            freq[t.charAt(i)]++;
+
+        int si = 0, ei = 0, count = nt, len = (int) 1e9, gsi = 0;
+        while (ei < ns) {
+            if (freq[s.charAt(ei)] > 0)
+                count--;
+            freq[s.charAt(ei)]--;
+            ei++;
+
+            while (count == 0) {
+                if (ei - si < len) {
+                    len = ei - si;
+                    gsi = si;
+                }
+
+                if (freq[s.charAt(si)] == 0)
+                    count++;
+                freq[s.charAt(si)]++;
+                si++;
+            }
+        }
+
+        return len == (int) 1e9 ? "" : s.substring(gsi, gsi + len);
+    }
+
 }
