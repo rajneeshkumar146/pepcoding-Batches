6752a82889b317964bbe09eefaeb2a93b3d77df7
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-18 21:49:50 +0530

    update

---
+++ 2020/pp-mar 14/lecture_006_TAS/questions.java
@@ -0,0 +1,46 @@
+public class questions {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int segregate(int[] arr, int si, int ei, int pivot) {
+        swap(arr, pivot, ei);
+        int p = si - 1, itr = si;
+        while (itr <= ei) {
+            if (arr[itr] <= arr[ei])
+                swap(arr, itr, ++p);
+            itr++;
+        }
+        return p;
+    }
+
+    public static void quickSort(int[] arr, int si, int ei) {
+        if (si > ei)
+            return;
+
+        int pivot = ei;
+        int pIdx = segregate(arr, si, ei, pivot);
+
+        quickSort(arr, si, pIdx - 1);
+        quickSort(arr, pIdx + 1, ei);
+    }
+
+    public static void targetSumPair(int[] arr, int target) {
+        int n = arr.length;
+        quickSort(arr, 0, n - 1);
+
+        int si = 0, ei = n - 1;
+        while (si < ei) {
+            int sum = arr[si] + arr[ei];
+            if (sum == target)
+                System.out.println(arr[si++] + ", " + arr[ei--]);
+            else if (sum < target)
+                si++;
+            else
+                ei--;
+        }
+    }
+}
