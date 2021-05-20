d013cf25718c2cab7fd274ec4856b58db2b8387a
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-20 17:45:21 +0530

    update

--- 2020/GTBIT/LevelUp/l001_Recursion/questions.java
+++ 2020/GTBIT/LevelUp/l001_Recursion/questions.java
@@ -322,4 +322,54 @@ public class questions {
 
         return res;
     }
+
+    // 51
+    boolean[] rows;
+    boolean[] cols;
+    boolean[] diag;
+    boolean[] adiag;
+    List<List<String>> ans = new ArrayList<>();
+
+    public int nqueen_Combination03(boolean[][] boxes, int tnq, int idx) {
+        int n = boxes.length, m = boxes[0].length;
+        if (tnq == 0) {
+            ArrayList<String> smallAns = new ArrayList<>();
+            
+            for (int i = 0; i < n; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < m; j++) {
+                    sb.append(boxes[i][j] ? "Q" : ".");
+                }
+                smallAns.add(sb.toString());
+            }
+
+            ans.add(smallAns);
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = idx; i < n * m; i++) {
+            int r = i / m;
+            int c = i % m;
+            if (!rows[r] && !cols[c] && !diag[r + c] && !adiag[r - c + m - 1]) {
+                rows[r] = cols[c] = diag[r + c] = adiag[r - c + m - 1] = boxes[r][c] = true;
+                count += nqueen_Combination03(boxes, tnq - 1, i + 1);
+                rows[r] = cols[c] = diag[r + c] = adiag[r - c + m - 1] = boxes[r][c] = false;
+            }
+        }
+
+        return count;
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        int m = n;
+        boolean[][] boxes = new boolean[n][m];
+        rows = new boolean[n];
+        cols = new boolean[m];
+        diag = new boolean[n + m - 1];
+        adiag = new boolean[n + m - 1];
+
+        nqueen_Combination03(boxes, n, 0);
+        return ans;
+    }
 }
