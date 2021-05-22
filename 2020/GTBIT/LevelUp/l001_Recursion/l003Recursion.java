8b3ed091c0c58d5e0c547e1593dc87d0b76c43ee
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-22 17:43:09 +0530

    update

--- 2020/GTBIT/LevelUp/l001_Recursion/l003Recursion.java
+++ 2020/GTBIT/LevelUp/l001_Recursion/l003Recursion.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class l003Recursion {
     static String s1 = "send";
     static String s2 = "more";
@@ -6,13 +8,33 @@ public class l003Recursion {
     static boolean[] usedNumber = new boolean[10];
     static int[] mapping = new int[128];
 
+    public static int convertStringToNumber(String str) {
+        int res = 0;
+        for (int i = 0; i < str.length(); i++) {
+            res = res * 10 + mapping[str.charAt(i)];
+        }
+
+        return res;
+    }
+
     public static int crypto(String str, int idx) {
-        if(idx == str.length()){
+        if (idx == str.length()) {
+            if (mapping[s1.charAt(0)] == 0 || mapping[s2.charAt(0)] == 0 || mapping[s3.charAt(0)] == 0)
+                return 0;
+
             int x = convertStringToNumber(s1);
             int y = convertStringToNumber(s2);
             int z = convertStringToNumber(s3);
 
-            if(x + y == z){
+            if (x + y == z) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 'a'; i <= 'z'; i++) {
+                    if (mapping[i] >= 0) {
+                        sb.append((char) i + "-" + mapping[i] + " ");
+                    }
+                }
+
+                System.out.println(sb.toString());
                 return 1;
             }
 
@@ -28,14 +50,14 @@ public class l003Recursion {
 
                 count += crypto(str, idx + 1);
                 usedNumber[i] = false;
-                mapping[ch] = 0;
+                mapping[ch] = -1;
             }
         }
 
         return count;
     }
 
-    public static int crypto() {
+    public static void crypto() {
         String str = s1 + s2 + s3;
         int[] freq = new int[26];
         for (int i = 0; i < str.length(); i++)
@@ -46,5 +68,36 @@ public class l003Recursion {
             if (freq[i] > 0)
                 str += (char) (i + 'a');
 
+        Arrays.fill(mapping, -1);
+
+        System.out.println(crypto(str, 0));
+    }
+
+    public static int wordBreak(String str, int idx, String ans, int len, HashSet<String> dict) {
+        if (idx >= str.length()) {
+            System.out.println(ans);
+            return 1;
+        }
+        int count = 0;
+        for (int i = idx; i <= str.length(); i++) {
+            String word = str.substring(idx, i);
+            if (word.length() > len)
+                break;
+            if (dict.contains(word)) {
+                count += wordBreak(str, i, ans + word + " ", len, dict);
+            }
+        }
+        return count;
+    }
+
+    public static void wordBreak(String str, String ans, HashSet<String> dict) {
+        int len = 0;
+        for (String s : dict)
+            len = Math.max(len, s.length());
+        wordBreak(str, 0, ans, len, dict);
+    }
+
+    public static void main(String[] args) {
+        crypto();
     }
 }
