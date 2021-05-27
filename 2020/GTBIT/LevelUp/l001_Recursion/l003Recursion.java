e3035057c55ed2e56f6f0c2868e5bf24bfe52048
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-27 16:00:41 +0530

    update

--- 2020/GTBIT/LevelUp/l001_Recursion/l003Recursion.java
+++ 2020/GTBIT/LevelUp/l001_Recursion/l003Recursion.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class l003Recursion {
     static String s1 = "send";
@@ -95,6 +96,93 @@ public class l003Recursion {
         for (String s : dict)
             len = Math.max(len, s.length());
         wordBreak(str, 0, ans, len, dict);
+    }
+
+    // Sudoku
+
+    public boolean isSafeToPlaceNumber(char[][] board, int row, int col, int num) {
+
+        int n = board.length, m = board[0].length;
+        // row
+        for (int j = 0; j < m; j++)
+            if ((board[row][j] - '0') == num)
+                return false;
+
+        // col
+        for (int i = 0; i < n; i++)
+            if ((board[i][col] - '0') == num)
+                return false;
+
+        // mat
+        row = (row / 3) * 3;
+        col = (col / 3) * 3;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if ((board[i + row][j + col] - '0') == num)
+                    return false;
+
+        return true;
+    }
+
+    public boolean sudokuSolver(char[][] board, int idx) {
+        if (idx == 81) {
+            return true;
+        }
+
+        int r = idx / 9;
+        int c = idx % 9;
+
+        if (board[r][c] != '.') {
+            if (sudokuSolver(board, idx + 1))
+                return true;
+        } else {
+            for (int num = 1; num <= 9; num++) {
+                if (isSafeToPlaceNumber(board, r, c, num)) {
+                    board[r][c] = (char) (num + '0');
+                    if (sudokuSolver(board, idx + 1))
+                        return true;
+                    board[r][c] = '.';
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean sudokuSolver(char[][] board, ArrayList<Integer> IDX, int idx) {
+        if (idx == IDX.size()) {
+            return true;
+        }
+
+        int r = IDX.get(idx) / 9;
+        int c = IDX.get(idx) % 9;
+
+        for (int num = 1; num <= 9; num++) {
+            if (isSafeToPlaceNumber(board, r, c, num)) {
+                board[r][c] = (char) (num + '0');
+                if (sudokuSolver(board, IDX, idx + 1))
+                    return true;
+                board[r][c] = '.';
+            }
+        }
+
+        return false;
+    }
+
+    public void solveSudoku(char[][] board) {
+        ArrayList<Integer> IDX = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    IDX.add(i * 9 + j);
+                }
+
+            }
+        }
+
+        // sudokuSolver(board, 0);
+        
+
     }
 
     public static void main(String[] args) {
