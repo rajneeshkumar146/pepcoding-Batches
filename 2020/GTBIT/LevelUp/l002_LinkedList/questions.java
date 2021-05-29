a3a30716011865f6f78e78c95d344ae71b8d7858
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-05-29 19:36:34 +0530

    update

--- 2020/GTBIT/LevelUp/l002_LinkedList/questions.java
+++ 2020/GTBIT/LevelUp/l002_LinkedList/questions.java
@@ -159,4 +159,56 @@ public class questions {
         p1.next = p2;
     }
 
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null)
+            return l1 != null ? l1 : l2;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy, c1 = l1, c2 = l2;
+
+        while (c1 != null && c2 != null) {
+            if (c1.val <= c2.val) {
+                prev.next = c1;
+                c1 = c1.next;
+            } else {
+                prev.next = c2;
+                c2 = c2.next;
+            }
+            prev = prev.next;
+        }
+
+        prev.next = c1 != null ? c1 : c2;
+
+        return dummy.next;
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0)
+            return null;
+
+        ListNode head = null;
+        for (ListNode node : lists) {
+            head = mergeTwoLists(head, node);
+        }
+
+        return head;
+    }
+    
+    // T : O(NlogkK), S : O(logK)   -> N = k times of (avg length Of Linkedlist), where k is length of lists.
+    public static ListNode mergeKList(ListNode[] lists, int si, int ei) {
+        if (si == ei)
+            return lists[si];
+        int mid = (si + ei) / 2;
+        ListNode leftMergedList = mergeKList(lists, si, mid);
+        ListNode rightMergedList = mergeKList(lists, mid + 1, ei);
+
+        return mergeTwoLists(leftMergedList, rightMergedList);
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0)
+            return null;
+        return mergeKList(lists, 0, lists.length - 1);
+    }
+
 }
