f6e4c40aa60c51a11eb5a4a611306e21efd2d3a6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-08 21:23:47 +0530

    update

--- 2020/pp-mar 14/lecture_008_StackAndQueue/question.java
+++ 2020/pp-mar 14/lecture_008_StackAndQueue/question.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Arrays;
 
 public class question {
     public static boolean balancedBrackets(String str) {
@@ -44,5 +45,53 @@ public class question {
         }
 
         return false;
+    }
+
+    public static void nextGreater(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        LinkedList<Integer> st = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            while (st.size() != 0 && arr[st.getFirst()] < arr[i])
+                ans[st.removeFirst()] = arr[i];
+
+            st.addFirst(i);
+        }
+
+        for (int ele : ans)
+            System.out.println(ele);
+    }
+
+    public static void mergeOverlappingIntervals(int[][] arr) {
+
+        // c++
+        // sort(arr.begin(), arr.end(),[](int[] a,int[] b){
+        // return a[0] < b[0];
+        // })
+
+        Arrays.sort(arr, (a, b) -> {
+            return a[0] - b[0]; // this - other gives default behaviour
+            // return b[0] - a[0] // other - this gives reverse of default behaviour
+        });
+
+        LinkedList<int[]> st = new LinkedList<>();
+        for (int[] a : arr) {
+            int minStartTime = a[0];
+            int maxEndingTime = a[1];
+            while (st.size() != 0 && a[0] <= st.getFirst()[1]) {
+                minStartTime = st.getFirst()[0];
+                maxEndingTime = Math.max(maxEndingTime, st.getFirst()[1]);
+                st.removeFirst();
+            }
+
+            st.addFirst(new int[] { minStartTime, maxEndingTime });
+        }
+
+        while (st.size() != 0) {
+            int[] a = st.removeLast();
+            System.out.println(a[0] + " " + a[1]);
+        }
     }
 }
