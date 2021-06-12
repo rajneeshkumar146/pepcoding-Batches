d81c4a5d0602cc6f9c89478287fcefbcbcf3b590
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-12 15:42:05 +0530

    update

--- 2020/GTBIT/LevelUp/l003_Tree/l001.java
+++ 2020/GTBIT/LevelUp/l003_Tree/l001.java
@@ -34,10 +34,46 @@ public class l001 {
     }
 
     public static ArrayList<TreeNode> NodeToRootPath(TreeNode root, int data) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
 
+        if (root.val == data) {
+            ArrayList<TreeNode> base = new ArrayList<>();
+            base.add(root);
+            return base;
+        }
+
+        ArrayList<TreeNode> left = NodeToRootPath(root.left, data);
+        if (left.size() != 0) {
+            left.add(root);
+            return left;
+        }
+
+        ArrayList<TreeNode> right = NodeToRootPath(root.right, data);
+        if (right.size() != 0) {
+            right.add(root);
+            return right;
+        }
+
+        return new ArrayList<>();
     }
 
-    public static ???? NodeToRootPath(TreeNode root, int data,ArrayList<TreeNode> ans) {
+    public static boolean NodeToRootPath(TreeNode root, int data, ArrayList<TreeNode> ans) {
+        if (root == null)
+            return false;
+        if (root.val == data) {
+            ans.add(root);
+            return true;
+        }
+
+        boolean res = NodeToRootPath(root.left, data, ans) || NodeToRootPath(root.right, data, ans);
+        if (res)
+            ans.add(root);
+        return res;
+    }
+
+    public static ArrayList<ArrayList<Integer>> nodeToAllLeafPath(TreeNode root) {
 
     }
 
