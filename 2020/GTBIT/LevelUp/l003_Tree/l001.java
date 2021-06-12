783259498b4f021226c90c574034b0e5f38a645d
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-12 14:36:50 +0530

    update

---
+++ 2020/GTBIT/LevelUp/l003_Tree/l001.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class l001 {
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int size(TreeNode root) {
+        return root == null ? 0 : size(root.left) + size(root.right) + 1;
+    }
+
+    public static int height(TreeNode root) {
+        return root == null ? -1 : Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    public static int Maximum(TreeNode root) {
+        return root == null ? -(int) 1e9 : Math.max(Math.max(Maximum(root.left), Maximum(root.right)), root.val);
+    }
+
+    public static boolean find(TreeNode root, int data) {
+        if (root == null)
+            return false;
+        if (root.val == data)
+            return true;
+
+        return find(root.left, data) || find(root.right, data);
+    }
+
+    public static ArrayList<TreeNode> NodeToRootPath(TreeNode root, int data) {
+
+    }
+
+    public static ???? NodeToRootPath(TreeNode root, int data,ArrayList<TreeNode> ans) {
+
+    }
+
+}
