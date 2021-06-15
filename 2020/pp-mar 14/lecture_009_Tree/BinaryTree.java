475ff1542890e0d0ee54e3d17d01ebcd767a9b30
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-15 23:21:38 +0530

    update

--- 2020/pp-mar 14/lecture_009_Tree/BinaryTree.java
+++ 2020/pp-mar 14/lecture_009_Tree/BinaryTree.java
@@ -260,4 +260,52 @@ public class BinaryTree {
         return -1;
     }
 
+    static Node prev = null;
+
+    public static boolean isBST(Node node) {
+        if (node == null)
+            return true;
+
+        if (!isBST(node.left))
+            return false;
+
+        if (prev != null && prev.data > node.data)
+            return false;
+        prev = node;
+
+        if (!isBST(node.right))
+            return false;
+        return true;
+    }
+
+    public static class isBSTPair {
+        boolean isBST = true;
+        int maxEle = -(int) 1e9;
+        int minEle = (int) 1e9;
+    }
+
+    public static isBSTPair isBST_02(Node node) {
+        if (node == null)
+            return new isBSTPair();
+
+        isBSTPair left = isBST_02(node.left);
+        if (!left.isBST)
+            return left;
+
+        isBSTPair right = isBST_02(node.right);
+        if (!right.isBST)
+            return right;
+
+        isBSTPair self = new isBSTPair();
+        self.isBST = false;
+
+        if (left.maxEle < node.data && right.minEle > node.data) {
+            self.maxEle = Math.max(right.maxEle, node.data);
+            self.minEle = Math.min(left.minEle, node.data);
+            self.isBST = true;
+        }
+
+        return self;
+    }
+
 }
