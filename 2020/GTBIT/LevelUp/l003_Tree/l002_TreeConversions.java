c447413758b0019a2c138c23a67eccaba0ca98da
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-19 19:52:54 +0530

    update

--- 2020/GTBIT/LevelUp/l003_Tree/l002_TreeConversions.java
+++ 2020/GTBIT/LevelUp/l003_Tree/l002_TreeConversions.java
@@ -50,14 +50,14 @@ public class l002_TreeConversions {
         return head;
     }
 
-    public void insertAllLeft(LinkedList<Node> st, Node node) {
+    public static void insertAllLeft(LinkedList<Node> st, Node node) {
         while (node != null) {
             st.addFirst(node);
             node = node.left;
         }
     }
 
-    Node bToDLL_02(Node node) {
+    public static Node bToDLL_02(Node node) {
         Node dummy = new Node(-1);
         Node prev = dummy;
 
@@ -94,14 +94,90 @@ public class l002_TreeConversions {
         return head;
     }
 
-    // left : prev, right : next
-    public static void sortDLL(Node head) {
+    public static Node mergeDLL(Node h1, Node h2) {
+        if (h1 == null || h2 == null)
+            return h1 != null ? h1 : h2;
 
+        Node dummy = new Node(-1);
+        Node prev = dummy;
+        Node c1 = h1, c2 = h2;
+
+        while (c1 != null && c2 != null) {
+            if (c1.val <= c2.val) {
+                prev.right = c1;
+                c1.left = prev;
+
+                c1 = c1.right;
+            } else {
+                prev.right = c2;
+                c2.left = prev;
+
+                c2 = c2.right;
+            }
+
+            prev = prev.right;
+        }
+
+        if (c1 != null) {
+            prev.right = c1;
+            c1.left = prev;
+        } else {
+            prev.right = c2;
+            c2.left = prev;
+        }
+
+        Node head = dummy.right;
+        dummy.right = head.left = null;
+        return head;
+    }
+
+    public static Node getMidNode(Node node) {
+        if (node == null || node.right == null)
+            return node;
+
+        Node slow = node, fast = node;
+        while (fast.right != null && fast.right.right != null) {
+            slow = slow.right;
+            fast = fast.right.right;
+        }
+
+        return slow;
     }
 
     // left : prev, right : next
-    public static void SortedDLLToBST(Node head) {
+    public static Node sortDLL(Node head) {
+        if (head == null || head.right == null)
+            return head;
 
+        Node midNode = getMidNode(head);
+        Node nHead = midNode.right;
+        nHead.left = midNode.right = null;
+
+        return mergeDLL(sortDLL(head), sortDLL(nHead));
+    }
+
+    // left : prev, right : next
+    public static Node SortedDLLToBST(Node head) {
+        if (head == null || head.right == null)
+            return head;
+
+        Node root = getMidNode(head);
+        Node leftDLLHead = head;
+        Node rightDLLHead = root.right;
+
+        root.left.right = root.right.left = null;
+        root.left = root.right = null;
+
+        root.left = SortedDLLToBST(leftDLLHead);
+        root.right = SortedDLLToBST(rightDLLHead);
+
+        return root;
+    }
+
+    public static Node BT_To_BST(Node root) {
+        Node head = bToDLL_02(root);
+        head = sortDLL(head);
+        return SortedDLLToBST(head);
     }
 
 }
