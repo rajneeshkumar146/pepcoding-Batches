0cf4e65d632cda5e664c36c5b202031bbf02a0ee
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-22 18:39:24 +0530

    update

--- 2020/pp-mar 14/lecture_009_Tree/l002_BST.java
+++ 2020/pp-mar 14/lecture_009_Tree/l002_BST.java
@@ -145,4 +145,22 @@ public class l002_BST {
         return node;
     }
 
+    public void modify(Node root, int[] arr) {
+        if (root == null)
+            return;
+
+        modify(root.right, arr);
+
+        root.data += arr[0];
+        arr[0] = root.data;
+
+        modify(root.left, arr);
+    }
+
+    public Node modify(Node root) {
+        int[] arr = new int[1];
+        modify(root, arr);
+        return root;
+    }
+
 }
---
+++ 2020/pp-mar 14/lecture_009_Tree/l003_GenericTree.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class l003_GenericTree {
+
+    public static class Node {
+        int data = 0;
+        ArrayList<Node> childs;
+
+        Node(int data) {
+            this.data = data;
+            this.childs = new ArrayList<>();
+        }
+    }
+
+    public static int height(Node root) {
+
+    }
+
+    public static int size(Node root) {
+
+    }
+
+    public static int maximum(Node root) {
+
+    }
+
+    public static int minimum(Node root) {
+
+    }
+
+    public static int sum(Node root) {
+
+    }
+
+    public static boolean find(Node root, int data) {
+
+    }
+
+    public static ArrayList<Node> nodeToRootPath(Node node) {
+
+    }
+
+}
