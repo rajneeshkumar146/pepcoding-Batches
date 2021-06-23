40ba5a06d94373f26d4d39106afda2a6009ce2c7
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-23 22:39:36 +0530

    update

--- 2020/pp-mar 14/lecture_009_Tree/l003_GenericTree.java
+++ 2020/pp-mar 14/lecture_009_Tree/l003_GenericTree.java
@@ -227,4 +227,26 @@ public class l003_GenericTree {
         return ub;
     }
 
+    public static Node getTail(Node node) {
+        while (node.childs.size() != 0) {
+            node = node.childs.get(0);
+        }
+
+        return node;
+    }
+
+    public static void linearize(Node node) {
+
+        for (Node child : node.childs) {
+            linearize(child);
+        }
+
+        for (int i = node.childs.size() - 1; i > 0; i--) {
+            Node tail = getTail(node.childs.get(i - 1));
+            tail.childs.add(node.childs.get(i));
+
+            node.childs.remove(i);
+        }
+    }
+
 }
