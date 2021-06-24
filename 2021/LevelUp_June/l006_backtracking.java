1342058f6dd49f0d85aaf9679a28f6371eddb929
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-24 18:33:32 +0530

    update

--- 2021/LevelUp_June/l006_backtracking.java
+++ 2021/LevelUp_June/l006_backtracking.java
@@ -189,4 +189,46 @@ public class l006_backtracking {
         return count;
     }
 
+    static char[][] box = { { '+', '-', '+', '+', '+', '+', '+', '+', '+', '+' },
+            { '+', '-', '+', '+', '+', '+', '+', '+', '+', '+' }, { '+', '-', '-', '-', '-', '-', '-', '-', '+', '+' },
+            { '+', '-', '+', '+', '+', '+', '+', '+', '+', '+' }, { '+', '-', '+', '+', '+', '+', '+', '+', '+', '+' },
+            { '+', '-', '-', '-', '-', '-', '-', '+', '+', '+' }, { '+', '-', '+', '+', '+', '-', '+', '+', '+', '+' },
+            { '+', '+', '+', '+', '+', '-', '+', '+', '+', '+' }, { '+', '+', '+', '+', '+', '-', '+', '+', '+', '+' },
+            { '+', '+', '+', '+', '+', '+', '+', '+', '+', '+' } };
+
+    public static boolean isPossibleToPlace_H(char[][] board, String word, int r, int c) {
+
+    }
+
+    public static void place_H(char[][] board, String word, int r, int c) {
+
+    }
+
+    public static void unPlace_H(char[][] board, String word, int r, int c) {
+
+    }
+
+    public static boolean isPossibleToPlace_V(char[][] board, String word, int r, int c) {
+
+    }
+
+    public static void place_V(char[][] board, String word, int r, int c) {
+
+    }
+
+    public static void unPlace_V(char[][] board, String word, int r, int c) {
+
+    }
+
+    public static int crossWord(char[][] board, String[] words, int idx) {
+        if (idx == words.length) {
+            return 1;
+        }
+
+        String str = words[idx];
+        int count = 0;
+
+        return count;
+    }
+
 }
