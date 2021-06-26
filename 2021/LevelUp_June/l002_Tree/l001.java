4c2fd820bfeeaaeb7bcb94f0028937cdd903eb6d
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-26 13:20:05 +0530

    update

--- 2021/LevelUp_June/l002_Tree/l001.java
+++ 2021/LevelUp_June/l002_Tree/l001.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class l001 {
 
@@ -136,6 +137,70 @@ public class l001 {
         ArrayList<Integer> ans = new ArrayList<>();
         exactlyOneChild(root);
         return ans;
+    }
+
+    public static int countExactlyOneChild(TreeNode node) {
+        if (node == null || (node.left == null && node.right == null))
+            return 0;
+
+        int leftSinglechildCount = countExactlyOneChild(node.left);
+        int rightSinglechildCount = countExactlyOneChild(node.right);
+
+        int ans = leftSinglechildCount + rightSinglechildCount;
+        if (node.left == null || node.right == null)
+            ans++;
+
+        return ans;
+    }
+
+    public void kdown(TreeNode root, int k, TreeNode block, List<Integer> ans) {
+        if (root == null || k < 0 || root == block)
+            return;
+
+        if (k == 0) {
+            ans.add(root.val);
+            return;
+        }
+
+        kdown(root.left, k - 1, block, ans);
+        kdown(root.right, k - 1, block, ans);
+    }
+
+    public int distanceK(TreeNode root, TreeNode target, int k, List<Integer> ans) {
+        if (root == null)
+            return -1;
+
+        if (root == target) {
+            kdown(root, k, null, ans);
+            return 1;
+        }
+
+        int ld = distanceK(root.left, target, k, ans);
+        if (ld != -1) {
+            kdown(root, k - ld, root.left, ans);
+            return ld + 1;
+        }
+
+        int rd = distanceK(root.right, target, k, ans);
+        if (rd != -1) {
+            kdown(root, k - rd, root.right, ans);
+            return rd + 1;
+        }
+
+        return -1;
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        ArrayList<TreeNode> path = new ArrayList<>();
+        nodeToRootPath_(root, target.val, path);
+
+        List<Integer> ans = new ArrayList<>();
+        TreeNode block = null;
+        for (int i = 0; i < path.size(); i++) {
+            kdown(path.get(i), k - i, block, ans);
+            block = path.get(i);
+        }
     }
 
 }
