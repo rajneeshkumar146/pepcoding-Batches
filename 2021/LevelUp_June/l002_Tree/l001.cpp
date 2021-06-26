43b724778255eadbf50f4ef3ba5dd185edfdb6dd
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-26 11:56:00 +0530

    update

--- 2021/LevelUp_June/l002_Tree/l001.cpp
+++ 2021/LevelUp_June/l002_Tree/l001.cpp
@@ -15,39 +15,137 @@ public:
     }
 };
 
-int size(TreeNode* root) {
-        return root == nullptr ? 0 : size(root->left) + size(root->right) + 1;
+int size(TreeNode *root)
+{
+    return root == nullptr ? 0 : size(root->left) + size(root->right) + 1;
+}
+
+int height(TreeNode *root)
+{
+    return root == nullptr ? -1 : max(height(root->left), height(root->right)) + 1;
+}
+
+int maximum(TreeNode *root)
+{
+    return root == nullptr ? -(int)1e9 : max(root->val, max(maximum(root->left), maximum(root->right)));
+}
+
+int minimum(TreeNode *root)
+{
+    return root == nullptr ? (int)1e9 : min(root->val, min(minimum(root->left), minimum(root->right)));
+}
+
+bool find(TreeNode *root, int data)
+{
+    if (root == nullptr)
+        return false;
+
+    if (root->val == data)
+        return true;
+
+    return find(root->left, data) || find(root->right, data);
+}
+
+bool nodeToRootPath_(TreeNode *root, int data, vector<TreeNode *> &ans)
+{
+
+    if (root == nullptr)
+        return false;
+
+    if (root->val == data)
+    {
+        ans->push_back(root);
+        return true;
     }
 
-    int height(TreeNode* root) {
-        return root == nullptr ? -1 : max(height(root->left), height(root->right)) + 1;
+    // if (nodeToRootPath_(root->left, data, ans))
+    // {
+    //     ans->push_back(root);
+    //     return true;
+    // }
+
+    // if (nodeToRootPath_(root->right, data, ans))
+    // {
+    //     ans->push_back(root);
+    //     return true;
+    // }
+
+    // return false;
+
+    bool res = nodeToRootPath_(root->left, data, ans) || nodeToRootPath_(root->right, data, ans);
+
+    if (res)
+        ans.push_back(root);
+    return res;
+}
+
+vector<TreeNode *> nodeToRootPath_(TreeNode *root, int data)
+{
+    if (root == nullptr)
+    {
+        return {};
     }
 
-    int maximum(TreeNode* root) {
-        return root == nullptr ? -(int) 1e9 : max(root->val, max(maximum(root->left), maximum(root->right)));
+    if (root->val == data)
+    {
+        return {root};
     }
 
-    int minimum(TreeNode* root) {
-        return root == nullptr ? (int) 1e9 : min(root->val,min(minimum(root->left), minimum(root->right)));
+    vector<TreeNode *> left = nodeToRootPath_(root->left, data);
+    if (left.size() != 0)
+    {
+        left.push_back(root);
+        return left;
     }
 
-    bool find(TreeNode* root, int data) {
-        if (root == nullptr)
-            return false;
-
-        if (root->val == data)
-            return true;
-
-        return find(root->left, data) || find(root->right, data);
+    vector<TreeNode *> right = nodeToRootPath_(root->right, data);
+    if (right.size() != 0)
+    {
+        right.push_back(root);
+        return right;
     }
 
-    vector<TreeNode*> nodeToRootPath(TreeNode root, int data) {
+    return {};
+}
 
+vector<TreeNode *> nodeToRootPath(TreeNode *root, int data)
+{
+    return nodeToRootPath_(root, data);
+}
+
+void rootToAllLeafPath(TreeNode *root, vector<vector<int>> &ans, vector<int> &smallAns)
+{
+    if (root == nullptr)
+        return;
+    if (root->left == nullptr && root->right == nullptr)
+    {
+        smallAns.push_back(root->val);
+        ans.push_back(smallAns);
+        smallAns.pop_back();
+
+        return;
     }
+
+    smallAns.push_back(root->val);
+    rootToAllLeafPath(root->left, ans, smallAns);
+    rootToAllLeafPath(root->right, ans, smallAns);
+    smallAns.pop_back();
+}
+
+vector<vector<int>> rootToAllLeafPath(TreeNode *root)
+{
+    vector<vector<int>> ans;
+    vector<int> smallAns;
+
+    rootToAllLeafPath(root, ans, smallAns);
+    return ans;
+}
+
+
+
 
 int main()
 {
-
 
     return 0;
 }
--- 2021/LevelUp_June/l002_Tree/l001.java
+++ 2021/LevelUp_June/l002_Tree/l001.java
@@ -38,8 +38,104 @@ public class l001 {
         return find(root.left, data) || find(root.right, data);
     }
 
-    public static ArrayList<TreeNode> nodeToRootPath(TreeNode root, int data) {
+    boolean nodeToRootPath_(TreeNode root, int data, ArrayList<TreeNode> ans) {
 
+        if (root == null)
+            return false;
+
+        if (root.val == data) {
+            ans.add(root);
+            return true;
+        }
+
+        // if (nodeToRootPath_(root.left, data, ans))
+        // {
+        // ans.add(root);
+        // return true;
+        // }
+
+        // if (nodeToRootPath_(root.right, data, ans))
+        // {
+        // ans.add(root);
+        // return true;
+        // }
+
+        // return false;
+
+        boolean res = nodeToRootPath_(root.left, data, ans) || nodeToRootPath_(root.right, data, ans);
+
+        if (res)
+            ans.add(root);
+        return res;
+    }
+
+    ArrayList<TreeNode> nodeToRootPath_(TreeNode root, int data) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        if (root.val == data) {
+            ArrayList<TreeNode> base = new ArrayList<>();
+            base.add(root);
+            return base;
+        }
+
+        ArrayList<TreeNode> left = nodeToRootPath_(root.left, data);
+        if (left.size() != 0) {
+            left.add(root);
+            return left;
+        }
+
+        ArrayList<TreeNode> right = nodeToRootPath_(root.right, data);
+        if (right.size() != 0) {
+            right.add(root);
+            return right;
+        }
+
+        return new ArrayList<>();
+    }
+
+    public static void rootToAllLeafPath(TreeNode root, ArrayList<ArrayList<Integer>> ans,
+            ArrayList<Integer> smallAns) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null) {
+            ArrayList<Integer> base = new ArrayList<>(smallAns);
+            base.add(root.val);
+            ans.add(base);
+            return;
+        }
+
+        smallAns.add(root.val);
+        rootToAllLeafPath(root.left, ans, smallAns);
+        rootToAllLeafPath(root.right, ans, smallAns);
+        smallAns.remove(smallAns.size() - 1);
+    }
+
+    public static ArrayList<ArrayList<Integer>> rootToAllLeafPath(TreeNode root) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> smallAns = new ArrayList<>();
+
+        rootToAllLeafPath(root, ans, smallAns);
+        return ans;
+    }
+
+    public static void exactlyOneChild(TreeNode root, ArrayList<Integer> ans) {
+        if (root == null || (root.left == null && root.right == null))
+            return;
+
+        if (root.left == null || root.right == null) {
+            ans.add(root.val);
+        }
+
+        exactlyOneChild(root.left, ans);
+        exactlyOneChild(root.right, ans);
+    }
+
+    public static ArrayList<Integer> exactlyOneChild(TreeNode root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        exactlyOneChild(root);
+        return ans;
     }
 
 }
