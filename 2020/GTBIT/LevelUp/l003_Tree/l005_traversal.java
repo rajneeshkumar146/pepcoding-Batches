863278274a4da41fb5ad1fabe67e3b7bf5851bc6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-26 17:54:43 +0530

    update

--- 2020/GTBIT/LevelUp/l003_Tree/l005_traversal.java
+++ 2020/GTBIT/LevelUp/l003_Tree/l005_traversal.java
@@ -42,7 +42,139 @@ public class l005_traversal {
         }
 
         return ans;
+    }
 
+    public static ArrayList<Integer> morrisPreOrderTraversal(TreeNode root) {
+        TreeNode curr = root;
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (curr != null) {
+            TreeNode left = curr.left;
+            if (left == null) {
+                ans.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode rightMostNode = getRightMostNode(left, curr);
+                if (rightMostNode.right == null) { // thread creation area
+                    rightMostNode.right = curr; // thread is created
+                    ans.add(curr.val);
+
+                    curr = curr.left;
+                } else { // thread destroy area
+                    rightMostNode.right = null; // thread break
+
+                    curr = curr.right;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    // validate BST==============================================
+
+    public static boolean isValidBST(TreeNode root, TreeNode[] prev) {
+        if (root == null)
+            return true;
+
+        if (!isValidBST(root.left, prev))
+            return false;
+
+        if (prev[0] != null && prev[0].val >= root.val)
+            return false;
+        prev[0] = root;
+
+        if (!isValidBST(root.right, prev))
+            return false;
+
+        return true;
+    }
+
+    public static boolean isValidBST_02(TreeNode root) {
+        long prev = -(long) 1e13;
+        TreeNode curr = root;
+        while (curr != null) {
+            TreeNode left = curr.left;
+            if (left == null) {
+                if (prev >= curr.val)
+                    return false;
+
+                prev = curr.val;
+                curr = curr.right;
+            } else {
+                TreeNode rightMostNode = getRightMostNode(left, curr);
+                if (rightMostNode.right == null) {
+                    rightMostNode.right = curr;
+                    curr = curr.left;
+                } else {
+                    rightMostNode.right = null;
+                    if (prev >= curr.val)
+                        return false;
+                    prev = curr.val;
+
+                    curr = curr.right;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static int kthSmallest(TreeNode root, int k) {
+        TreeNode curr = root;
+        while (curr != null) {
+            TreeNode left = curr.left;
+            if (left == null) {
+                if (--k == 0)
+                    return curr.val;
+                curr = curr.right;
+            } else {
+                TreeNode rightMostNode = getRightMostNode(left, curr);
+                if (rightMostNode.right == null) {
+                    rightMostNode.right = curr;
+                    curr = curr.left;
+                } else {
+                    rightMostNode.right = null;
+                    if (--k == 0)
+                        return curr.val;
+                    curr = curr.right;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public TreeNode getLeftMostNode(TreeNode node, TreeNode curr) {
+        while (node.left != null && node.left != curr) {
+            node = node.left;
+        }
+
+        return node;
+    }
+
+    public int kthLargest(TreeNode root, int k) {
+        TreeNode curr = root;
+        while (curr != null) {
+            TreeNode right = curr.right;
+            if (right == null) {
+                if (--k == 0)
+                    return curr.val;
+                curr = curr.left;
+            } else {
+                TreeNode leftMostNode = getLeftMostNode(right, curr);
+                if (leftMostNode.left == null) {
+                    leftMostNode.left = curr;
+                    curr = curr.right;
+                } else {
+                    leftMostNode.left = null;
+                    if (--k == 0)
+                        return curr.val;
+                    curr = curr.left;
+                }
+            }
+        }
+
+        return -1;
     }
 
 }
