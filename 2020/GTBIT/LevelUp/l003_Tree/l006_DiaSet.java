a7afa1816cee5a0be31776af664841ccd36c74d6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-27 17:51:51 +0530

    update

--- 2020/GTBIT/LevelUp/l003_Tree/l006_DiaSet.java
+++ 2020/GTBIT/LevelUp/l003_Tree/l006_DiaSet.java
@@ -16,6 +16,16 @@ public class l006_DiaSet {
         }
     }
 
+    public static class Node {
+        int data = 0;
+        Node left = null;
+        Node right = null;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
     public static int size(TreeNode root) {
         return root == null ? 0 : size(root.left) + size(root.right) + 1;
     }
@@ -64,17 +74,83 @@ public class l006_DiaSet {
         return Math.max(lh, rh) + 1;
     }
 
-    //112
+    // 112
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null) return false;
-        
-        if(root.left == null && root.right == null){
-            return targetSum - root.val == 0? true: false;
+        if (root == null)
+            return false;
+
+        if (root.left == null && root.right == null) {
+            return targetSum - root.val == 0 ? true : false;
         }
-        
-        return hasPathSum(root.left, targetSum - root.val) 
-            || hasPathSum(root.right,targetSum - root.val);
-        
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+
+    }
+
+    // 113
+    public void pathSum(TreeNode root, int targetSum, List<Integer> smallAns, List<List<Integer>> ans) {
+        if (root == null)
+            return;
+
+        if (root.left == null && root.right == null && targetSum - root.val == 0) {
+            List<Integer> base = new ArrayList<>(smallAns);
+            base.add(root.val);
+            ans.add(base);
+            return;
+        }
+
+        smallAns.add(root.val);
+
+        pathSum(root.left, targetSum - root.val, smallAns, ans);
+        pathSum(root.right, targetSum - root.val, smallAns, ans);
+
+        smallAns.remove(smallAns.size() - 1);
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> smallAns = new ArrayList<>();
+
+        pathSum(root, targetSum, smallAns, ans);
+        return ans;
+
+    }
+
+    public static class leafToLeafPair {
+        int LTLMaxSum = -(int) 1e9; // Leaves to Leaves Max Sum
+        int NTLMaxSum = -(int) 1e9; // Node to Leaves Max Sum.
+    }
+
+    public static leafToLeafPair maxLeafSum(Node root) {
+        if (root == null) {
+            return new leafToLeafPair();
+        }
+
+        if (root.left == null && root.right == null) {
+            leafToLeafPair base = new leafToLeafPair();
+            base.NTLMaxSum = root.data;
+            return base;
+        }
+
+        leafToLeafPair lRes = maxLeafSum(root.left);
+        leafToLeafPair rRes = maxLeafSum(root.right);
+
+        leafToLeafPair myRes = new leafToLeafPair();
+        myRes.LTLMaxSum = Math.max(lRes.LTLMaxSum, rRes.LTLMaxSum);
+
+        if (root.left != null && root.right != null) {
+            myRes.LTLMaxSum = Math.max(myRes.LTLMaxSum, lRes.NTLMaxSum + root.data + rRes.NTLMaxSum);
+        }
+
+        myRes.NTLMaxSum = Math.max(lRes.NTLMaxSum, rRes.NTLMaxSum) + root.data;
+        return myRes;
+    }
+
+    int maxPathSum(Node root) {
+        int ans = maxLeafSum(root).LTLMaxSum;
+        int ans2 = maxLeafSum(root).NTLMaxSum;
+
+        return ans != -(int) 1e9 ? ans : Math.max(ans, ans2);
     }
 
 }
