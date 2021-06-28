1efd10f7c9235c621c88305e36ff0e6ccd89ec2d
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-28 23:02:51 +0530

    update

--- 2020/pp-mar 14/lecture_010_Graph/l001.java
+++ 2020/pp-mar 14/lecture_010_Graph/l001.java
@@ -19,17 +19,55 @@ public class l001 {
     }
 
     public static void display(ArrayList<Edge>[] graph, int N) {
+        for (int i = 0; i < N; i++) {
+            System.out.print(i + " -> ");
+            for (Edge e : graph[i]) {
+                System.out.print("(" + e.nbr + ", " + e.wt + ") ");
+            }
+            System.out.println();
+        }
 
     }
 
-    public static void removeEdge(int u, int v) {
+    public static int findEdge(ArrayList<Edge>[] graph, int u, int v) {
+        ArrayList<Edge> list = graph[u];
+        for (int i = 0; i < list.size(); i++) {
+            Edge e = list.get(i);
+            if (e.nbr == v)
+                return i;
+        }
 
+        return -1;
     }
 
-    public static void removeVtx(int u){
-        
+    public static void removeEdge(ArrayList<Edge>[] graph, int u, int v) {
+        int i1 = findEdge(graph, u, v);
+        int i2 = findEdge(graph, v, u);
+
+        graph[u].remove(i1);
+        graph[v].remove(i2);
     }
 
+    public static void removeVtx(ArrayList<Edge>[] graph, int u) {
+        ArrayList<Edge> list = graph[u];
+        for (int i = list.size() - 1; i >= 0; i--) {
+            Edge e = list.get(i);
+            removeEdge(graph, e.src, e.nbr);
+        }
+    }
+
+    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis) {
+        if (src == dest)
+            return true;
+
+        vis[src] = true;
+        boolean res = false;
+        for (Edge e : graph[src])
+            if (!vis[e.nbr])
+                res = res || hasPath(graph, e.nbr, dest, vis);
+
+        return res;
+    }
 
     public static void construction() {
         int N = 7;
@@ -45,6 +83,9 @@ public class l001 {
         addEdge(graph, 4, 5, 2);
         addEdge(graph, 4, 6, 8);
         addEdge(graph, 5, 6, 3);
+    }
 
+    public static void main(String[] args) {
+        construction();
     }
 }
