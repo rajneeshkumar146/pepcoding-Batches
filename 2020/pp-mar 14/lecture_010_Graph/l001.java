53b98c21883fed029c0adf50f9d1069fe87a1712
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-06-29 22:30:22 +0530

    update

--- 2020/pp-mar 14/lecture_010_Graph/l001.java
+++ 2020/pp-mar 14/lecture_010_Graph/l001.java
@@ -109,12 +109,43 @@ public class l001 {
 
     public static void lightestPath(ArrayList<Edge>[] graph, int src, int dest) {
 
-        System.out.println("Lightest Path: " + x + " of weight: " + y);
+        // System.out.println("Lightest Path: " + x + " of weight: " + y);
+    }
+
+    public static class pathPair {
+        String psf = "";
+        int wsf = -1;
+    }
+
+    public static pathPair heaviestPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis) {
+        if (src == dest) {
+            pathPair base = new pathPair();
+            base.psf += src;
+            base.wsf = 0;
+
+            return base;
+        }
+
+        vis[src] = true;
+        pathPair myAns = new pathPair();
+        for (Edge e : graph[src]) {
+            if (!vis[e.nbr]) {
+                pathPair recAns = heaviestPath(graph, e.nbr, dest, vis);
+                if (recAns.wsf != -1 && recAns.wsf + e.wt > myAns.wsf) {
+                    myAns.psf = src + recAns.psf;
+                    myAns.wsf = recAns.wsf + e.wt;
+                }
+            }
+        }
+        vis[src] = false;
+        return myAns;
     }
 
     public static void heaviestPath(ArrayList<Edge>[] graph, int src, int dest) {
+        boolean[] vis = new boolean[graph.length];
+        pathPair ans = heaviestPath(graph, src, dest, vis);
 
-        System.out.println("Heaviest Path: " + x + " of weight: " + y);
+        System.out.println("Heaviest Path: " + ans.psf + " of weight: " + ans.wsf);
     }
 
     public static void construction() {
@@ -127,7 +158,7 @@ public class l001 {
         addEdge(graph, 0, 3, 10);
         addEdge(graph, 1, 2, 10);
         addEdge(graph, 2, 3, 40);
-        addEdge(graph, 3, 4, 2);
+        // addEdge(graph, 3, 4, 2);
         addEdge(graph, 4, 5, 2);
         addEdge(graph, 4, 6, 8);
         addEdge(graph, 5, 6, 3);
@@ -135,7 +166,8 @@ public class l001 {
         // display(graph, N);
         boolean[] vis = new boolean[N];
         // System.out.println(printAllPath(graph, 0, 6, vis, ""));
-        preOrder(graph, 0, vis, 0, "");
+        // preOrder(graph, 0, vis, 0, "");
+        heaviestPath(graph, 0, 6);
     }
 
     public static void main(String[] args) {
