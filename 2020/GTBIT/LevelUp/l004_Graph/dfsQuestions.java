36ba4c16780a0f33c10f203e834f0ba411975b3f
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-04 17:48:16 +0530

    update

--- 2020/GTBIT/LevelUp/l004_Graph/dfsQuestions.java
+++ 2020/GTBIT/LevelUp/l004_Graph/dfsQuestions.java
@@ -70,4 +70,64 @@ public class dfsQuestions {
         return maxSize;
     }
 
+    // 463
+    public int islandPerimeter(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int[][] dir = { { 1, 0 }, { 0, 1 } };
+
+        int onceCount = 0, nbrCount = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    for (int d = 0; d < 2; d++) {
+                        int r = i + dir[d][0];
+                        int c = j + dir[d][1];
+
+                        if (r < n && c < m && grid[r][c] == 1)
+                            nbrCount++;
+                    }
+                }
+            }
+        }
+        return 4 * onceCount - 2 * nbrCount;
+    }
+
+    // 130
+    public void surrounded_DFS(char[][] grid, int i, int j, int[][] dir) {
+        grid[i][j] = '$';
+        int n = grid.length, m = grid[0].length;
+        for (int d = 0; d < 4; d++) {
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 'O')
+                surrounded_DFS(grid, r, c, dir);
+        }
+
+    }
+
+    public void surroiunded(char[][] grid) {
+        int n = grid.length, m = grid[0].length;
+
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i == 0 || j == 0 || i == n - 1 || j == m - 1) && grid[i][j] == 'O') {
+                    surrounded_DFS(grid, i, j, dir);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '$')
+                    grid[i][j] = 'O';
+                else
+                    grid[i][j] = 'X';
+            }
+        }
+    }
+
+    // https://www.hackerrank.com/challenges/journey-to-the-moon/problem
 }
