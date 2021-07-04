6c107f4289530dc845f20f8a81c7daf8f9ebcc6f
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-04 16:26:48 +0530

    update

--- 2020/GTBIT/LevelUp/l004_Graph/dfsQuestions.java
+++ 2020/GTBIT/LevelUp/l004_Graph/dfsQuestions.java
@@ -1,3 +1,73 @@
 public class dfsQuestions {
 
+    public void dfs_NumIsland(char[][] grid, int i, int j, int[][] dir) {
+        grid[i][j] = '2';
+        int n = grid.length, m = grid[0].length;
+        for (int d = 0; d < 4; d++) {
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == '1')
+                dfs_NumIsland(grid, r, c, dir);
+        }
+
+    }
+
+    public int numIslands(char[][] grid) {
+        int n = grid.length, m = grid[0].length, componentCount = 0;
+
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    dfs_NumIsland(grid, i, j, dir);
+                    componentCount++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '2') {
+                    grid[i][j] = '1';
+                }
+            }
+        }
+
+        return componentCount;
+    }
+
+    public int dfs_areaIsland(int[][] grid, int[][] dir, int i, int j) {
+        grid[i][j] = 0;
+        int n = grid.length, m = grid[0].length;
+
+        int size = 0;
+        for (int d = 0; d < 4; d++) {
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1)
+                size += dfs_areaIsland(grid, dir, r, c);
+        }
+
+        return size + 1;
+
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int n = grid.length, m = grid[0].length, maxSize = 0;
+
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    int s = dfs_areaIsland(grid, dir, i, j);
+                    maxSize = Math.max(maxSize, s);
+                }
+            }
+        }
+        return maxSize;
+    }
+
 }
