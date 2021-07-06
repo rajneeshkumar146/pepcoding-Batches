59e93ff235468227f11355c78b7f0643c3467ea9
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-06 22:31:14 +0530

    update

--- 2020/pp-mar 14/lecture_010_Graph/l001.java
+++ 2020/pp-mar 14/lecture_010_Graph/l001.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Arrays;
 
 public class l001 {
     public static class Edge {
@@ -394,7 +395,101 @@ public class l001 {
                 }
             }
         }
+    }
 
+    public static int spreadInfection(ArrayList<Edge>[] graph, int infectedPerson, int NoOfDays) {
+        LinkedList<Integer> que = new LinkedList<>();
+        boolean[] vis = new boolean[graph.length];
+
+        que.addLast(infectedPerson);
+
+        int infectedCount = 0, day = 1;
+        while (que.size() != 0) {
+            int size = que.size();
+
+            if (day > NoOfDays)
+                break;
+
+            while (size-- > 0) {
+                int ip = que.removeFirst(); // infectedPerson
+                if (vis[ip])
+                    continue;
+
+                vis[ip] = true;
+                infectedCount++;
+
+                for (Edge e : graph[ip]) {
+                    if (!vis[e.nbr])
+                        que.addLast(e.nbr);
+                }
+
+            }
+
+            day++;
+
+        }
+
+        return infectedCount;
+    }
+
+    public static boolean bipartite(ArrayList<Edge>[] graph, int src, int[] vis) {
+        LinkedList<Integer> que = new LinkedList<>();
+        que.addLast(src);
+        int color = 0; // 0 : red, 1 : green
+        boolean cycle = false, isBipartite = true;
+
+        while (que.size() != 0) {
+            int size = que.size();
+            while (size-- > 0) {
+                int rvtx = que.removeFirst();
+                if (vis[rvtx] != -1) { // cycle
+                    cycle = true;
+                    if (vis[rvtx] != color) { // conflict
+                        isBipartite = false;
+                        break;
+                    }
+
+                    continue; // not any kind oo conflict
+                }
+
+                vis[rvtx] = color;
+
+                for (Edge e : graph[rvtx]) {
+                    if (vis[e.nbr] == -1) {
+                        que.addLast(e.nbr);
+                    }
+                }
+            }
+
+            color = (color + 1) % 2;
+        }
+
+        if (cycle) {
+            if (isBipartite)
+                System.out.println("Even Length Cycle");
+            else
+                System.out.println("Odd Length Cycle");
+        } else if (isBipartite && !cycle) {
+            System.out.println("A-Cycle and Bipartite graph");
+        }
+
+        return isBipartite;
+    }
+
+    public static void bipartite(ArrayList<Edge>[] graph) {
+
+        int N = graph.length;
+        int[] vis = new int[N];
+        Arrays.fill(vis, -1);
+
+        boolean isBipartite = true;
+        for (int i = 0; i < N; i++) {
+            if (vis[i] == -1) {
+                isBipartite = isBipartite && bipartite(graph, i, vis);
+            }
+        }
+
+        System.out.println("Overall Graph is Bipartite: " + isBipartite);
     }
 
     public static void construction() {
