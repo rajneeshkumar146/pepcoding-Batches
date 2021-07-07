d2269e1fb0a7cd4946be4c67bc561d9336ce5e34
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-07 19:02:12 +0530

    update

---
+++ 2020/pp-mar 14/lecture_011_Heap/l001_basic.java
@@ -0,0 +1,64 @@
+import java.util.PriorityQueue;
+
+public class l001_basic {
+    public static void Int_minPQ() {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // by default min Priority Queue
+        for (int i = 10; i >= 1; i--)
+            pq.add(i * 10);
+
+        while (pq.size() != 0)
+            System.out.println(pq.remove());
+    }
+
+    public static void Int_maxPQ() {
+        // this - other, default behaviour.
+        // other - this, reverse of default behaviour.
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            return b - a;
+        });
+
+        for (int i = 10; i >= 1; i--)
+            pq.add(i * 10);
+
+        while (pq.size() != 0)
+            System.out.println(pq.remove());
+    }
+
+    public static void matrixPQ(int[][] arr) {
+
+    }
+
+    public static class mobilePhone {
+        String Company = "";
+        String Model = "";
+        int Ram = 0;
+        int Storage = 0;
+        int BatteryBackup = 0;
+
+        mobilePhone(String Company, String Model, int Ram, int Storage, int BatteryBackup) {
+            this.Company = Company;
+            this.Model = Model;
+            this.Ram = Ram;
+            this.Storage = Storage;
+            this.BatteryBackup = BatteryBackup;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.append("Company: " + this.Company + "\n");
+            sb.append("Model: " + this.Model + "\n");
+            sb.append("Ram: " + this.Ram + "GB\n");
+            sb.append("Storage: " + this.Storage + "GB\n");
+            sb.append("BatteryBackup: " + this.BatteryBackup + "mAH\n");
+            
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Int_minPQ();
+        Int_maxPQ();
+    }
+}
