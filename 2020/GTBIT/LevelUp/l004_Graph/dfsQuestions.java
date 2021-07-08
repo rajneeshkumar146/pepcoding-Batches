c85e73aeac9ed0b5084e553ace6f213ec70cff43
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-08 17:35:12 +0530

    update

--- 2020/GTBIT/LevelUp/l004_Graph/dfsQuestions.java
+++ 2020/GTBIT/LevelUp/l004_Graph/dfsQuestions.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class dfsQuestions {
 
     public void dfs_NumIsland(char[][] grid, int i, int j, int[][] dir) {
@@ -130,4 +132,72 @@ public class dfsQuestions {
     }
 
     // https://www.hackerrank.com/challenges/journey-to-the-moon/problem
+    
+    int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+    String[] dirS = { "D", "U", "R", "L" };
+
+    StringBuilder sb;
+    int n, m;
+
+    public void numDistinctIslands(int[][] grid, int i, int j) {
+        grid[i][j] = 0;
+        for (int d = 0; d < 4; d++) {
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1) {
+                sb.append(dirS[d]);
+                numDistinctIslands(grid, r, c);
+                sb.append("b");
+            }
+        }
+    }
+
+    public int numDistinctIslands(int[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    sb = new StringBuilder();
+                    numDistinctIslands(grid, i, j);
+                    set.add(sb.toString());
+                }
+            }
+        }
+
+        return set.size();
+    }
+
+    public boolean countSubIslands(int[][] grid1, int[][] grid2, int i, int j) {
+        grid2[i][j] = 0;
+        boolean res = true;
+        for (int d = 0; d < 4; d++) {
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r < n && c < m && grid2[r][c] == 1) {
+                res = countSubIslands(grid1, grid2, r, c) && res;
+            }
+        }
+
+        return res && grid1[i][j] == 1;
+    }
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        n = grid1.length;
+        m = grid1[0].length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid2[i][j] == 1) {
+                    count += countSubIslands(grid1, grid2,i,j) ? 1 : 0;
+                }
+            }
+        }
+
+        return count;
+    }
+
 }
--- 2020/GTBIT/LevelUp/l004_Graph/l001.java
+++ 2020/GTBIT/LevelUp/l004_Graph/l001.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class l001 {
 
@@ -102,6 +103,62 @@ public class l001 {
         }
 
         System.out.println(components);
+    }
+
+    // O(E)
+    public static void BFS_forCycle(ArrayList<Edge>[] graph, int src, boolean[] vis) {
+        LinkedList<Integer> que = new LinkedList<>();
+        int level = 0;
+        boolean iscycle = false;
+
+        que.add(src);
+        while (que.size() != 0) {
+            int size = que.size();
+            System.out.print("Min No Edges: " + level + " -> ");
+            while (size-- > 0) {
+                int rvtx = que.removeFirst();
+
+                if (vis[rvtx]) {
+                    iscycle = true;
+                    continue;
+                }
+
+                System.out.print(rvtx + ", ");
+                vis[rvtx] = true;
+                for (Edge e : graph[rvtx]) {
+                    if (!vis[e.v])
+                        que.addLast(e.v);
+                }
+            }
+            System.out.println();
+            level++;
+        }
+    }
+
+    // O(v)
+    public static void BFS_withoutCycle(ArrayList<Edge>[] graph, int src, boolean[] vis) {
+        LinkedList<Integer> que = new LinkedList<>();
+        int level = 0;
+
+        que.add(src);
+        vis[src] = true;
+        while (que.size() != 0) {
+            int size = que.size();
+            System.out.print("Min No Edges: " + level + " -> ");
+            while (size-- > 0) {
+                int rvtx = que.removeFirst();
+
+                System.out.print(rvtx + ", ");
+                for (Edge e : graph[rvtx]) {
+                    if (!vis[e.v]) {
+                        vis[e.v] = true;
+                        que.addLast(e.v);
+                    }
+                }
+            }
+            System.out.println();
+            level++;
+        }
     }
 
     public static void constructGraph() {
