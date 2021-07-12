07fafa92cb10ae0d854ecccbc2d5e7cb7b7e3e8a
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-12 20:50:19 +0530

    update

--- 2020/pp-mar 14/lecture_011_HeapAndHashMap/l002_HMBasics.java
+++ 2020/pp-mar 14/lecture_011_HeapAndHashMap/l002_HMBasics.java
@@ -16,13 +16,60 @@ public class l002_HMBasics {
         // System.out.println(map);
         // ArrayList<String> keys = new ArrayList<>(map.keySet());
         // System.out.println(keys);
-        for(String key : map.keySet()){
+        for (String key : map.keySet()) {
             System.out.println(key + " -> " + map.get(key));
         }
+    }
 
+    public static void frequency(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        // for (int i = 0; i < str.length(); i++) {
+        // char ch = str.charAt(i);
+        // if (map.containsKey(ch))
+        // map.put(ch, map.get(ch) + 1);
+        // else
+        // map.put(ch, 0 + 1);
+        // }
+
+        // for (int i = 0; i < str.length(); i++) {
+        //     char ch = str.charAt(i);
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            map.putIfAbsent(ch,0);
+            map.put(ch, map.get(ch) + 1);
+        }
+
+        for (char ch : map.keySet()) {
+            System.out.println(ch + " -> " + map.get(ch));
+        }
+    }
+
+    public static void IndexOfChar(String str) {
+        HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
+        // for (int i = 0; i < str.length(); i++) {
+        //     char ch = str.charAt(i);
+        //     if (!map.containsKey(ch))
+        //         map.put(ch, new ArrayList<>());
+
+        //     map.get(ch).add(i);
+        // }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            map.putIfAbsent(ch,new ArrayList<>());
+            map.get(ch).add(i);
+        }
+
+        for (char ch : map.keySet()) {
+            System.out.println(ch + " -> " + map.get(ch));
+        }
     }
 
     public static void main(String[] args) {
-        HashMapBasic();
+        // HashMapBasic();
+        IndexOfChar("aaasmnavnbasvnammansvdbamnsbdsmdvbn");
     }
 }
