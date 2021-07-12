4b037d1236fe7116316ca2975882cea7fa95b81a
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-12 21:54:06 +0530

    update

--- 2020/pp-mar 14/lecture_011_HeapAndHashMap/l002_HMBasics.java
+++ 2020/pp-mar 14/lecture_011_HeapAndHashMap/l002_HMBasics.java
@@ -32,13 +32,13 @@ public class l002_HMBasics {
         // }
 
         // for (int i = 0; i < str.length(); i++) {
-        //     char ch = str.charAt(i);
-        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // char ch = str.charAt(i);
+        // map.put(ch, map.getOrDefault(ch, 0) + 1);
         // }
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            map.putIfAbsent(ch,0);
+            map.putIfAbsent(ch, 0);
             map.put(ch, map.get(ch) + 1);
         }
 
@@ -50,16 +50,16 @@ public class l002_HMBasics {
     public static void IndexOfChar(String str) {
         HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
         // for (int i = 0; i < str.length(); i++) {
-        //     char ch = str.charAt(i);
-        //     if (!map.containsKey(ch))
-        //         map.put(ch, new ArrayList<>());
+        // char ch = str.charAt(i);
+        // if (!map.containsKey(ch))
+        // map.put(ch, new ArrayList<>());
 
-        //     map.get(ch).add(i);
+        // map.get(ch).add(i);
         // }
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            map.putIfAbsent(ch,new ArrayList<>());
+            map.putIfAbsent(ch, new ArrayList<>());
             map.get(ch).add(i);
         }
 
@@ -68,8 +68,56 @@ public class l002_HMBasics {
         }
     }
 
+    public static void intersectionWithoutDuplicate(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int ele : arr1)
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+
+        for (int ele : arr2) {
+            if (map.containsKey(ele)) {
+                System.out.println(ele);
+                map.remove(ele);
+            }
+        }
+    }
+
+    public static void intersectionWithDuplicate(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int ele : arr1)
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+
+        for (int ele : arr2) {
+            if (map.containsKey(ele)) {
+                System.out.println(ele);
+                map.put(ele, map.get(ele) - 1);
+                if (map.get(ele) == 0)
+                    map.remove(ele);
+            }
+        }
+    }
+
+    public static void highestFreqChar(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int maxFreq = 0;
+        char ans = '\u0000';
+        for (char ch : map.keySet()) {
+            if (map.get(ch) > maxFreq) {
+                maxFreq = map.get(ch);
+                ans = ch;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
     public static void main(String[] args) {
         // HashMapBasic();
-        IndexOfChar("aaasmnavnbasvnammansvdbamnsbdsmdvbn");
+        // IndexOfChar("aaasmnavnbasvnammansvdbamnsbdsmdvbn");
+        highestFreqChar("zmszeqxllzvheqwrofgcuntypejcxovtaqbnqyqlmrwitc");
     }
 }
