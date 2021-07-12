7b3f7f30a8519bdee7fcce2ad4a362a78d451c04
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-12 22:54:38 +0530

    update

--- 2020/pp-mar 14/lecture_011_HeapAndHashMap/l002_HMBasics.java
+++ 2020/pp-mar 14/lecture_011_HeapAndHashMap/l002_HMBasics.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.ArrayList;
 
 public class l002_HMBasics {
@@ -113,6 +114,37 @@ public class l002_HMBasics {
         }
 
         System.out.println(ans);
+    }
+
+    public static int longestConsecutiveSequence(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int ele : arr)
+            set.add(ele);
+
+        int len = 0, head = 0;
+        for (int ele : arr) {
+            if (!set.contains(ele))
+                continue;
+
+            int left = ele - 1, right = ele + 1;
+            set.remove(ele);
+
+            while (set.contains(left))
+                set.remove(left--);
+
+            while (set.contains(right))
+                set.remove(right++);
+
+            if (right - left - 1 > len) {
+                len = right - left - 1;
+                head = left + 1;
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            System.out.println(head + i);
+        }
+
     }
 
     public static void main(String[] args) {
