4a3aa708791a11bad74eb8b26647a82a7f57de80
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-13 19:42:14 +0530

    update

---
+++ 2020/pp-mar 14/lecture_011_HeapAndHashMap/HeapConstruction/heap.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+
+public class heap {
+    // Data Members======================================
+
+    private ArrayList<Integer> arr;
+    private int noOfEle = 0;
+    private boolean isMaxHeap = true;
+
+    // Constructors=======================================
+
+    private void intialize(boolean isMaxHeap) {
+        this.arr = new ArrayList<>();
+        this.noOfEle = 0;
+        this.isMaxHeap = isMaxHeap;
+    }
+
+    public heap(boolean isMaxHeap) {
+        intialize(isMaxHeap);
+    }
+
+    public heap() {
+        this(true);
+    }
+
+    public heap(int[] data, boolean isMaxHeap) {
+        this(isMaxHeap);
+
+        for (int ele : data)
+            this.arr.add(ele);
+
+        this.noOfEle = this.arr.size();
+
+        for (int i = this.noOfEle - 1; i >= 0; i--) { // NLogN -> N
+            downHeapify(i);
+        }
+    }
+
+    // exceptions.==============================================
+
+    private void UnderFlowPointerException() throws Exception {
+        if (this.noOfEle == 0)
+            throw new Exception("HeapUnderFlowException");
+    }
+
+    // Basics Functions.=========================================
+
+    public int size() {
+        return this.noOfEle;
+    }
+
+    public boolean isEmpty() {
+        return this.noOfEle == 0;
+    }
+
+    // DS Functions.==============================================
+
+    public int compareTo(int t, int o) {
+        if (isMaxHeap) {
+            return this.arr.get(t) - this.arr.get(o);
+        } else {
+            return this.arr.get(o) - this.arr.get(t);
+        }
+    }
+
+    private void swap(int i, int j) {
+        int e1 = this.arr.get(i);
+        int e2 = this.arr.get(j);
+
+        this.arr.set(i, e2);
+        this.arr.set(j, e1);
+    }
+
+    // O(LogN)
+    private void downHeapify(int pi) {
+        int maxIdx = pi, lci = 2 * pi + 1, rci = 2 * pi + 2;
+        if (lci < this.noOfEle && compareTo(lci, maxIdx) > 0)
+            maxIdx = lci;
+        if (rci < this.noOfEle && compareTo(rci, maxIdx) > 0)
+            maxIdx = rci;
+
+        if (maxIdx != pi) {
+            swap(pi, maxIdx);
+            downHeapify(maxIdx);
+        }
+    }
+
+    public int peek() throws Exception {
+        UnderFlowPointerException();
+        return this.arr.get(0);
+    }
+
+    public int remove() throws Exception {
+        UnderFlowPointerException();
+
+    }
+
+    public void add() {
+
+    }
+
+}
