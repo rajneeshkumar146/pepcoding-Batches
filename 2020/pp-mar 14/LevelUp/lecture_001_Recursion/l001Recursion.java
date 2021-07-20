7e51421d1778f174429dd2f1618bfd8ece61980e
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-20 20:58:01 +0530

    update

--- 2020/pp-mar 14/LevelUp/lecture_001_Recursion/l001Recursion.java
+++ 2020/pp-mar 14/LevelUp/lecture_001_Recursion/l001Recursion.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class l001Recursion {
     public static void pppppp(int a) {
         System.out.println("I am Base case: " + a);
@@ -46,69 +48,132 @@ public class l001Recursion {
     }
 
     public static void printIncreasing(int a, int b) {
-
+        if (a > b)
+            return;
+        System.out.println(a);
         printIncreasing(a + 1, b);
-
     }
 
     public static void printDecreasing(int a, int b) {
-
+        if (a > b)
+            return;
         printDecreasing(a + 1, b);
-
+        System.out.println(a);
     }
 
     public static void printIncreasingDecreasing(int a, int b) {
-
+        if (a > b)
+            return;
+        System.out.println(a);
         printIncreasingDecreasing(a + 1, b);
-
+        System.out.println(a);
     }
 
     public static void oddEven(int a, int b) {
-
+        if (a > b)
+            return;
+        if (a % 2 != 0)
+            System.out.println(a);
         oddEven(a + 1, b);
-
+        if (a % 2 == 0)
+            System.out.println(a);
     }
 
-    public static void factorial(int n) {
+    public static int factorial(int n) {
+        if (n == 0)
+            return 1;
 
+        return factorial(n - 1) * n;
     }
 
-    public static void power(int a, int b) {
+    public static int power(int a, int b) {
+        if (b == 0)
+            return 1;
 
+        return power(a, b - 1) * a;
     }
 
     // O(logn)
-    public static void powerBtr(int a, int b) {
+    public static int powerBtr(int a, int b) {
+        if (b == 0)
+            return 1;
+
+        int smallAns = powerBtr(a, b / 2);
+        smallAns *= smallAns;
+
+        return b % 2 == 0 ? smallAns : smallAns * a;
+    }
+
+    public static void printArray(int[] arr, int idx) {
+        if (idx >= arr.length)
+            return;
+
+        System.out.println(arr[idx]);
+        printArray(arr, idx + 1);
+    }
+
+    public static void printArrayReverse(int[] arr, int idx) {
+        if (idx >= arr.length)
+            return;
+
+        printArrayReverse(arr, idx + 1);
+        System.out.println(arr[idx]);
+    }
+
+    public static int maximum(int[] arr, int idx) {
+        if (idx >= arr.length)
+            return -(int) 1e9;
+
+        int maxValue = maximum(arr, idx + 1);
+        return Math.max(maxValue, arr[idx]);
+    }
+
+    public static int minimum(int[] arr, int idx) {
+        if (idx >= arr.length)
+            return (int) 1e9;
+
+        int minValue = minimum(arr, idx + 1);
+        return Math.min(minValue, arr[idx]);
+    }
+
+    public static boolean find(int[] arr, int data, int idx) {
+        if (idx >= arr.length)
+            return false;
+
+        return arr[idx] == data || find(arr, data, idx + 1);
+    }
+
+    public static int firstIndex(int[] arr, int data, int idx) {
+        if (idx >= arr.length)
+            return -1;
+
+        return arr[idx] == data ? idx : firstIndex(arr, data, idx + 1);
+    }
+
+    public static int lastIndex(int[] arr, int data, int idx) {
+        if (idx >= arr.length)
+            return -1;
+
+        int ans = lastIndex(arr, data, idx + 1);
+        if (ans != -1)
+            return ans;
+
+        return arr[idx] == data ? idx : -1;
+    }
+
+    public static int[] allIndex(int[] arr, int data, int idx) {
 
     }
 
-    public static void printArray(int[] arr) {
+    public static ArrayList<String> subsequnce(String str,int idx) {
 
     }
 
-    public static void printArrayReverse(int[] arr) {
+    public static int subsequnce(String str,int idx,String asf,ArrayList<String> ans) {
 
     }
 
-    public static int maximum(int[] arr) {
 
-    }
-
-    public static int minimum(int[] arr) {
-
-    }
-
-    public static boolean find(int[] arr, int data) {
-
-    }
-
-    public static int firstIndex(int[] arr, int data) {
-
-    }
-
-    public static int lastIndex(int[] arr, int data) {
-
-    }
 
     public static void main(String[] args) {
         recursionPattern(1, 6);
