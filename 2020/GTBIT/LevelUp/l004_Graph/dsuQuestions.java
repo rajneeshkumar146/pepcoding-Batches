d7797f77955db13e04fc6397dd3e424796045cb6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-25 17:07:16 +0530

    update

--- 2020/GTBIT/LevelUp/l004_Graph/dsuQuestions.java
+++ 2020/GTBIT/LevelUp/l004_Graph/dsuQuestions.java
@@ -258,4 +258,39 @@ public class dsuQuestions {
         return mcost <= K ? superroad : -1;
     }
 
+    public int regionsBySlashes(String[] grid) {
+        if(grid.length == 0) return 0;
+       int n = grid.length, m = n+1, region = 1;
+       par = new int[m*m];
+       for(int i = 0;i < m*m;i++){
+           par[i] = i;
+           int r = i / m, c = i % m;
+           if(r == 0 || c == 0 || r == m - 1 || c == m - 1)
+              par[i] = 0;
+       }
+        
+        for(int i = 0;i < n;i++){
+            String s = grid[i];
+            for(int j = 0;j < s.length();j++){
+                char ch = s.charAt(j);
+                int p1 = -10000, p2 = -100000;
+                if(ch == '/'){
+                    p1 = findPar(i * m + j + 1);
+                    p2 = findPar((i + 1)* m + j);
+                }else if(ch == '\\'){
+                    p1 = findPar(i * m + j);
+                    p2 = findPar((i + 1)* m + j + 1);
+                }else    // for space 
+                    continue;
+                
+                if(p1 != p2)
+                    par[p1] = p2;
+                else
+                    region++;
+            }
+        }
+        
+        return region;
+    }
+
 }
