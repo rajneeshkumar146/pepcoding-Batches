3bb93a5d4816e4427d65fb45abdf135eb631f3d0
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-07-31 14:59:42 +0530

    upadte

--- 2020/GTBIT/LevelUp/l004_Graph/algo.java
+++ 2020/GTBIT/LevelUp/l004_Graph/algo.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class algo {
     public static class Edge {
@@ -45,7 +46,7 @@ public class algo {
     }
 
     // {{u,v,w}}
-    public static void unionFind(int[][] edges, ArrayList<Edge>[] graph,int n) {
+    public static void unionFind(int[][] edges, ArrayList<Edge>[] graph, int n) {
         par = new int[n];
         size = new int[n];
 
@@ -64,7 +65,7 @@ public class algo {
         }
     }
 
-    public static void kruskalAlgo(int[][] edges,int N) {
+    public static void kruskalAlgo(int[][] edges, int N) {
         Arrays.sort(edges, (a, b) -> {
             return a[2] - b[2];
         });
@@ -73,7 +74,96 @@ public class algo {
         for (int i = 0; i < N; i++)
             graph[i] = new ArrayList<>();
 
-        unionFind(edges, graph,N);
+        unionFind(edges, graph, N);
+    }
+
+    static int[] low, disc;
+    static boolean[] articulation, vis;
+    static int time = 0, rootCalls = 0;
+
+    public static void dfs(ArrayList<Edge>[] graph, int src, int par) {
+        disc[src] = low[src] = time++;
+        vis[src] = true;
+        for (Edge e : graph[src]) {
+            if (!vis[e.v]) {
+                if (par == -1)
+                    rootCalls++;
+
+                dfs(graph, e.v, src);
+                if (disc[src] <= low[e.v])
+                    articulation[src] = true;
+                if (disc[src] < low[e.v])
+                    System.out.println("Articulation Edge: " + src + "->" + e.v);
+
+                low[src] = Math.min(low[src], low[e.v]);
+
+            } else if (e.v != par) {
+                low[src] = Math.min(low[src], disc[e.v]);
+            }
+
+        }
+
+    }
+
+    public static void articulationPointAndBridges(ArrayList<Edge>[] graph) {
+        int N = graph.length;
+        low = new int[N];
+        disc = new int[N];
+        articulation = new boolean[N];
+        vis = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            if (!vis[i]) {
+                dfs(graph, i, -1);
+            }
+        }
+    }
+
+    public static class pair {
+        int vtx, par, w, wsf;
+
+        pair(int vtx, int par, int w, int wsf) {
+            this.vtx = vtx;
+            this.par = par;
+            this.w = w;
+            this.wsf = wsf;
+        }
+    }
+
+    public static void dijikstra_01(ArrayList<Edge>[] graph, int src) {
+        int N = graph.length;
+        ArrayList<Edge>[] ngraph = new ArrayList[N];
+        for (int i = 0; i < N; i++)
+            ngraph[i] = new ArrayList<>();
+
+        boolean[] vis = new boolean[N];
+        PriorityQueue<pair> pq = new PriorityQueue<>((a, b) -> {
+            return a.wsf - b.wsf;
+        });
+
+        int[] dis = new  int[N];
+        int[] par = new  int[N];
+
+        pq.add(new pair(src, -1, 0, 0));
+        while (pq.size() != 0) {
+            pair p = pq.remove();
+
+            if (p.par != -1)
+                addEdge(ngraph, p.vtx, p.par, p.w);
+
+            if (vis[p.vtx])
+                continue;
+
+            vis[p.vtx] = true;
+            
+            dis[p.vtx] = p.wsf;
+            par[p.vtx] = p.par;
+
+            for (Edge e : graph[p.vtx]) {
+                if (!vis[e.v])
+                    pq.add(new pair(e.v, p.vtx, e.w, p.wsf + e.w));
+            }
+        }
     }
 
 }
