f369749a6e4600fe2f0fdb882a21d92062fb301a
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-08-05 22:17:26 +0530

    update

--- 2021/LevelUp_June/l004_Graph/BFSQuestions.java
+++ 2021/LevelUp_June/l004_Graph/BFSQuestions.java
@@ -84,4 +84,49 @@ public class BFSQuestions {
         return -1;
     }
 
+    // 542
+    public int[][] updateMatrix(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0)
+            return grid;
+
+        int n = grid.length, m = grid[0].length;
+
+        LinkedList<Integer> que = new LinkedList<>();
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+        boolean[][] vis = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    que.push(i * m + j);
+                    vis[i][j] = true;
+                }
+            }
+        }
+
+        while (que.size() != 0) {
+            int size = que.size();
+            while (size-- > 0) {
+                int idx = que.removeFirst();
+                int sr = idx / m, sc = idx % m;
+
+                for (int[] d : dir) {
+                    int r = sr + d[0];
+                    int c = sc + d[1];
+
+                    if (r >= 0 && c >= 0 && r < n && c < m && !vis[r][c]) {
+                        grid[r][c] = grid[sr][sc] + 1;
+                        vis[r][c] = true;
+                        que.addLast(r * m + c);
+                    }
+
+                }
+            }
+        }
+
+        return grid;
+    }
+
+    // 1020
+    // 286
 }
