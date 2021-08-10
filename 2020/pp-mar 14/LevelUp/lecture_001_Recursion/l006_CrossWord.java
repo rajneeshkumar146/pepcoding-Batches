986091fa204ddc9a8085a0b8d83467db8fcb9356
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-08-10 22:27:40 +0530

    update

--- 2020/pp-mar 14/LevelUp/lecture_001_Recursion/l006_CrossWord.java
+++ 2020/pp-mar 14/LevelUp/lecture_001_Recursion/l006_CrossWord.java
@@ -16,11 +16,37 @@ public class l006_CrossWord {
             }
             System.out.println();
         }
-
     }
 
     public static boolean isPossiblePlaceH(int r, int c, String word) {
+        int l = word.length();
+        // if (c == 0 && c + l < 10 && box[r][c + l] != '+')
+        // return false;
+        // else if (c != 0 && c + l == 10 && box[r][c - 1] != '+')
+        // return false;
+        // else if (c != 0 && c + l != 10 && (box[r][c - 1] != '+' || box[r][c + l] !=
+        // '+'))
+        // return false;
 
+        // for (int i = 0; i < 10 && c + i < 10; i++) {
+        // if (box[r][c + i] != '-' && box[r][c + i] != word.charAt(i))
+        // return false;
+        // }
+
+        int len = 0, C = c;
+        while (C < 10 && box[r][C++] != '+') {
+            len++;
+        }
+
+        if (len != l || (c > 0 && box[r][c - 1] != '+'))
+            return false;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (box[r][c + i] != '-' && box[r][c + i] != word.charAt(i))
+                return false;
+        }
+
+        return true;
     }
 
     public static int placeH(int r, int c, String word) {
@@ -43,7 +69,21 @@ public class l006_CrossWord {
     }
 
     public static boolean isPossiblePlaceV(int r, int c, String word) {
-            //
+        int l = word.length();
+        int len = 0, R = r;
+        while (R < 10 && box[R++][c] != '+') {
+            len++;
+        }
+
+        if (len != l || (r > 0 && box[r - 1][c] != '+'))
+            return false;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (box[r + i][c] != '-' && box[r + i][c] != word.charAt(i))
+                return false;
+        }
+
+        return true;
     }
 
     public static int placeV(int r, int c, String word) {
@@ -90,6 +130,10 @@ public class l006_CrossWord {
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+        crossWord(0);
     }
 
 }
