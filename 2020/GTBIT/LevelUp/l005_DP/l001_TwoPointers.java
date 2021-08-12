ff577d19dce3c9cb306f018898077197f50ec83e
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-08-12 17:54:54 +0530

    update

--- 2020/GTBIT/LevelUp/l005_DP/l001_TwoPointers.java
+++ 2020/GTBIT/LevelUp/l005_DP/l001_TwoPointers.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class l001_TwoPointers {
     /*
      * 1. Faith 2. Recursive Tree 3. Recursion Code 4. Convert Recursion code into
@@ -282,6 +284,239 @@ public class l001_TwoPointers {
         System.out.println(boardPath_tabu(sp, ep, dp));
         display(dp);
     }
+
+    public int numDecodings_memo(String s, int n, int[] dp) {
+        if (n == 0) {
+            return dp[n] = 1;
+        }
+
+        if (dp[n - 1] != -1)
+            return dp[n];
+
+        int count = 0;
+        if (s.charAt(n - 1) > '0')
+            count += numDecodings_memo(s, n - 1, dp);
+
+        if (n > 1) {
+            int num = (s.charAt(n - 2) - '0') * 10 + (s.charAt(n - 1) - '0');
+            if (num <= 26 && num >= 10)
+                count += numDecodings_memo(s, n - 2, dp);
+        }
+
+        return dp[n] = count;
+    }
+
+    public int numDecodings_memo2(String s, int idx, int[] dp) {
+        if (idx == s.length()) {
+            return dp[idx] = 1;
+        }
+
+        if (dp[idx] != -1)
+            return dp[idx];
+
+        if (s.charAt(idx) == '0') {
+            return dp[idx] = 0;
+        }
+
+        int count = 0;
+        count += numDecodings_memo2(s, idx + 1, dp);
+
+        if (idx < s.length() - 1) {
+            int num = (s.charAt(idx) - '0') * 10 + (s.charAt(idx + 1) - '0');
+            if (num <= 26)
+                count += numDecodings_memo2(s, idx + 2, dp);
+        }
+
+        return dp[idx] = count;
+    }
+
+    public int numDecodings_tabu(String s, int IDX, int[] dp) {
+        for (int idx = s.length(); idx >= 0; idx--) {
+            if (idx == s.length()) {
+                dp[idx] = 1;
+                continue;
+            }
+
+            if (s.charAt(idx) == '0') {
+                dp[idx] = 0;
+                continue;
+            }
+
+            int count = 0;
+            count += dp[idx + 1];
+
+            if (idx < s.length() - 1) {
+                int num = (s.charAt(idx) - '0') * 10 + (s.charAt(idx + 1) - '0');
+                if (num <= 26)
+                    count += dp[idx + 2];
+            }
+
+            dp[idx] = count;
+        }
+
+        return dp[IDX];
+    }
+
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        // int ans = numDecodings_memo(s,n,dp);
+        int ans = numDecodings_memo2(s, 0, dp);
+
+        for (int ele : dp)
+            System.out.print(ele + " ");
+        return ans;
+    }
+
+    // ============================================================
+    public long numDecodingsStar(String s, int idx, long[] dp) {
+        if (idx == s.length()) {
+            return dp[idx] = 1;
+        }
+
+        if (dp[idx] != -1)
+            return dp[idx];
+
+        if (s.charAt(idx) == '0') {
+            return dp[idx] = 0;
+        }
+
+        long count = 0;
+        int mod = (int) 1e9 + 7;
+        char ch1 = s.charAt(idx);
+        if (ch1 == '*') {
+            count = (count + 9 * numDecodingsStar(s, idx + 1, dp)) % mod;
+
+            if (idx < s.length() - 1) {
+                if (s.charAt(idx + 1) == '*')
+                    count = (count + 15 * numDecodingsStar(s, idx + 2, dp)) % mod;
+                else if (s.charAt(idx + 1) >= '0' && s.charAt(idx + 1) <= '6')
+                    count = (count + 2 * numDecodingsStar(s, idx + 2, dp)) % mod;
+                else if (s.charAt(idx + 1) >= '7')
+                    count = (count + numDecodingsStar(s, idx + 2, dp)) % mod;
+            }
+        } else {
+            count = (count + numDecodingsStar(s, idx + 1, dp)) % mod;
+
+            if (idx < s.length() - 1) {
+                if (s.charAt(idx + 1) != '*') {
+                    int num = (s.charAt(idx) - '0') * 10 + (s.charAt(idx + 1) - '0');
+                    if (num <= 26)
+                        count = (count + 1 * numDecodingsStar(s, idx + 2, dp)) % mod;
+                } else {
+                    if (ch1 == '1')
+                        count = (count + 9 * numDecodingsStar(s, idx + 2, dp)) % mod;
+                    else if (ch1 == '2')
+                        count = (count + 6 * numDecodingsStar(s, idx + 2, dp)) % mod;
+                }
+            }
+        }
+
+        return dp[idx] = count;
+    }
+
+    public long numDecodingsStar_tabu(String s, int IDX, long[] dp) {
+        for (int idx = s.length(); idx >= 0; idx--) {
+            if (idx == s.length()) {
+                dp[idx] = 1;
+                continue;
+            }
+
+            if (s.charAt(idx) == '0') {
+                dp[idx] = 0;
+                continue;
+            }
+
+            long count = 0;
+            int mod = (int) 1e9 + 7;
+            char ch1 = s.charAt(idx);
+            if (ch1 == '*') {
+                count = (count + 9 * dp[idx + 1]) % mod;
+
+                if (idx < s.length() - 1) {
+                    if (s.charAt(idx + 1) == '*')
+                        count = (count + 15 * dp[idx + 2]) % mod;
+                    else if (s.charAt(idx + 1) >= '0' && s.charAt(idx + 1) <= '6')
+                        count = (count + 2 * dp[idx + 2]) % mod;
+                    else if (s.charAt(idx + 1) >= '7')
+                        count = (count + dp[idx + 2]) % mod;
+                }
+            } else {
+                count = (count + dp[idx + 1]) % mod;
+
+                if (idx < s.length() - 1) {
+                    if (s.charAt(idx + 1) != '*') {
+                        int num = (s.charAt(idx) - '0') * 10 + (s.charAt(idx + 1) - '0');
+                        if (num <= 26)
+                            count = (count + 1 * dp[idx + 2]) % mod;
+                    } else {
+                        if (ch1 == '1')
+                            count = (count + 9 * dp[idx + 2]) % mod;
+                        else if (ch1 == '2')
+                            count = (count + 6 * dp[idx + 2]) % mod;
+                    }
+                }
+            }
+
+            dp[idx] = count;
+        }
+
+        return dp[IDX];
+    }
+
+    public int numDecodingsStar(String s) {
+        int n = s.length();
+        long[] dp = new long[n + 1];
+        Arrays.fill(dp, -1);
+
+        long ans = numDecodingsStar_tabu(s, 0, dp);
+
+        // for (int ele : dp)
+        // System.out.print(ele + " ");
+        return (int) ans;
+    }
+
+    // https://practice.geeksforgeeks.org/problems/friends-pairing-problem5425/1
+
+    public static long friendsPairing(int n, long[] dp) {
+        if (n <= 1) {
+            return dp[n] = 1;
+        }
+        if (dp[n] != 0)
+            return dp[n];
+
+        int mod = (int) 1e9 + 7;
+        long single = friendsPairing(n - 1, dp);
+        long pairup = friendsPairing(n - 2, dp) * (n - 1);
+
+        return dp[n] = (single + pairup % mod) % mod;
+    }
+
+    public static long friendsPairing_tabu(int N, long[] dp) {
+        int mod = (int) 1e9 + 7;
+        for (int n = 0; n <= N; n++) {
+            if (n <= 1) {
+                dp[n] = 1;
+                continue;
+            }
+
+            long single = dp[n - 1];
+            long pairup = dp[n - 2] * (n - 1);
+
+            dp[n] = (single + pairup % mod) % mod;
+        }
+
+        return dp[N];
+    }
+
+    public long countFriendsPairings(int n) {
+        long[] dp = new long[n + 1];
+        return friendsPairing_tabu(n, dp);
+    }
+
+    // https://www.geeksforgeeks.org/count-the-number-of-ways-to-divide-n-in-k-groups-incrementally/
 
     public static void main(String[] args) {
         board_path();
