a97d45dfdbd4dda75a6883d0ae6e9b22a74a1c41
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-08-19 17:57:46 +0530

    update

--- 2020/GTBIT/LevelUp/l005_DP/l002_StringSet.java
+++ 2020/GTBIT/LevelUp/l005_DP/l002_StringSet.java
@@ -147,6 +147,162 @@ public class l002_StringSet {
 
     // longest Common Substring ??
 
+    // 1458
+    public int maximum(int... arr) {
+        int max = arr[0];
+        for (int ele : arr)
+            max = Math.max(max, ele);
+
+        return max;
+    }
+
+    public int maxDotProduct(int[] nums1, int[] nums2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = -(int) 1e8;
+        }
+
+        if (dp[n][m] != -(int) 1e9)
+            return dp[n][m];
+
+        int val = nums1[n - 1] * nums2[m - 1];
+        int acceptTwoNumbers = maxDotProduct(nums1, nums2, n - 1, m - 1, dp) + val;
+        int a = maxDotProduct(nums1, nums2, n, m - 1, dp);
+        int b = maxDotProduct(nums1, nums2, n - 1, m, dp);
+
+        return dp[n][m] = maximum(acceptTwoNumbers, val, a, b);
+
+    }
+
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -(int) 1e9);
+
+        return maxDotProduct(nums1, nums2, n, m, dp);
+    }
+
+    // 1035
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = 0;
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        if (nums1[n - 1] == nums2[m - 1])
+            return dp[n][m] = maxUncrossedLines(nums1, nums2, n - 1, m - 1, dp) + 1;
+        else
+            return dp[n][m] = Math.max(maxUncrossedLines(nums1, nums2, n, m - 1, dp),
+                    maxUncrossedLines(nums1, nums2, n - 1, m, dp));
+    }
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        return maxUncrossedLines(nums1, nums2, n, m, dp);
+    }
+
+    // 72
+    public int minDistance(String word1, String word2, int n, int m, int[][] dp) {
+        if (n == 0)
+            dp[n][m] = m; // insert
+        else if (m == 0)
+            dp[n][m] = n; // delete
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        if (word1.charAt(n - 1) == word2.charAt(m - 1))
+            return dp[n][m] = minDistance(word1, word2, n - 1, m - 1, dp);
+
+        int insert = minDistance(word1, word2, n, m - 1, dp);
+        int delete = minDistance(word1, word2, n - 1, m, dp);
+        int replace = minDistance(word1, word2, n - 1, m - 1, dp);
+
+        return dp[n][m] = Math.min(Math.min(insert, delete), replace) + 1;
+
+    }
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        return minDistance(word1, word2, n, m, dp);
+    }
+
+    // 583
+    // https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        return n + m - 2 * longestCommonSubsequence(word1, word2);
+    }
+
+    // 115
+
+    public int numDistinct_memo(String s, String t, int n, int m, int[][] dp) {
+        if (m == 0 || n < m) {
+            return dp[n][m] = (m == 0 ? 1 : 0);
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        int a = numDistinct_memo(s, t, n - 1, m - 1, dp);
+        int b = numDistinct_memo(s, t, n - 1, m, dp);
+
+        if (s.charAt(n - 1) == t.charAt(m - 1))
+            return dp[n][m] = a + b;
+        else
+            return dp[n][m] = b;
+    }
+
+    public int numDistinct_Dp(String s, String t, int N, int M, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (m == 0 || n < m) {
+                    dp[n][m] = (m == 0 ? 1 : 0);
+                    continue;
+                }
+
+                int a = dp[n - 1][m - 1];// numDistinct_memo(s, t, n - 1, m - 1, dp);
+                int b = dp[n - 1][m];// numDistinct_memo(s, t, n - 1, m, dp);
+
+                if (s.charAt(n - 1) == t.charAt(m - 1))
+                    dp[n][m] = a + b;
+                else
+                    dp[n][m] = b;
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int ans = numDistinct_memo(s, t, n, m, dp);
+        return ans;
+    }
+
     public static void main(String[] args) {
         longestCommonSubsequence("abcde", "ace");
     }
