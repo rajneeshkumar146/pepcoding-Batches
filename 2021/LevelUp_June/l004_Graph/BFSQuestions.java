eb5a71acd3937f3a958f9e988895c734bb374819
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-08-26 20:55:43 +0530

    update

--- 2021/LevelUp_June/l004_Graph/BFSQuestions.java
+++ 2021/LevelUp_June/l004_Graph/BFSQuestions.java
@@ -252,6 +252,8 @@ public class BFSQuestions {
 
         while (que.size() != 0) {
             pair p = que.remove();
+            if (p.r == er && p.c == ec)
+                return p.steps;
             for (int[] d : dir) {
                 int r = p.r, c = p.c, steps = p.steps;
                 while (r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == 0) {
@@ -269,11 +271,75 @@ public class BFSQuestions {
 
                 que.add(new pair(r, c, steps));
                 dis[r][c] = steps;
-                if (r == er && c == ec)
-                    return steps;
             }
         }
 
         return -1;
+    }
+
+    // 499
+    public static class pair implements Comparable<pair> {
+        int r = 0, c = 0, steps = 0;
+        String psf = "";
+
+        pair(int r, int c, int steps, String psf) {
+            this.r = r;
+            this.c = c;
+            this.steps = steps;
+            this.psf = psf;
+        }
+
+        @Override
+        public int compareTo(pair o) {
+            if (this.steps != o.steps)
+                return this.steps - o.steps;
+            else
+                return this.psf.compareTo(o.psf);
+        }
+    }
+
+    public String findShortestWay(int[][] maze, int[] start, int[] destination) {
+        int n = maze.length, m = maze[0].length, sr = start[0], sc = start[1], er = destination[0], ec = destination[1];
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        String[] dirS = { "d", "u", "r", "l" };
+        pair[][] dis = new pair[n][m];
+        for (int i = 0; i < n * m; i++)
+            dis[i / m][i % m] = new pair(i / m, i % m, (int) 1e8, "");
+
+        PriorityQueue<pair> que = new PriorityQueue<>();
+        pair src = new pair(sr, sc, 0, "");
+
+        que.add(src);
+        dis[sr][sc] = src;
+
+        while (que.size() != 0) {
+            pair p = que.remove();
+            for (int i = 0; i < 4; i++) {
+                int[] d = dir[i];
+
+                int r = p.r, c = p.c, steps = p.steps;
+                while (r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == 0 && !(r == er && c == ec)) {
+                    r += d[0];
+                    c += d[1];
+                    steps++;
+                }
+
+                if (!(r == er && c == ec)) {
+                    r -= d[0];
+                    c -= d[1];
+                    steps--;
+                }
+
+                pair np = new pair(r, c, steps, p.psf + dirS[i]);
+                if (steps > dis[r][c].steps || dis[r][c].compareTo(np) <= 0)
+                    continue;
+
+                que.add(np);
+                dis[r][c] = np;
+            }
+        }
+
+        pair ans = dis[er][ec];
+        return ans.steps != (int) 1e8 ? ans.psf : "impossible";
     }
 }
