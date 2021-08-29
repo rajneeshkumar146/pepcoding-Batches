4204b39894805db74dbbc13c53b49daa3459e1b6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-08-29 11:35:26 +0530

    update

---
+++ 2021/LevelUp_June/l005_DP/l001_twoPointerSet.java
@@ -0,0 +1,150 @@
+public class l001_twoPointerSet {
+    // 1_Faith
+    // 2_Recursive_Tree
+    // 3_RecursiveCode->Memoization
+    // 4_Observation
+    // 5_Tabulation
+    // 6_Optimization
+
+    public static void display(int[] dp) {
+        for (int ele : dp) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+
+    public static void display2D(int[][] dp) {
+        for (int[] d : dp) {
+            display(d);
+        }
+        System.out.println();
+    }
+
+    public static int fibo_memo(int n, int[] dp) {
+        if (n <= 1)
+            return dp[n] = n;
+
+        if (dp[n] != 0)
+            return dp[n];
+
+        int ans = fibo_memo(n - 1, dp) + fibo_memo(n - 2, dp);
+        return dp[n] = ans;
+    }
+
+    public static int fibo_tabu(int N, int[] dp) {
+        for (int n = 0; n <= N; n++) {
+            if (n <= 1) {
+                dp[n] = n;
+                continue;
+            }
+
+            int ans = dp[n - 1] + dp[n - 2];// fibo_memo(n - 1, dp) + fibo_memo(n - 2, dp);
+            dp[n] = ans;
+        }
+
+        return dp[N];
+    }
+
+    public static int fibo_opti(int N) {
+        int a = 0, b = 1;
+        for (int i = 2; i <= N; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+
+        return b;
+    }
+
+    public static void fibo() {
+        int n = 10;
+        int[] dp = new int[n + 1];
+        // System.out.println(fibo_memo(n, dp));
+        // System.out.println(fibo_tabu(n, dp));
+        System.out.println(fibo_opti(n));
+
+        display(dp);
+    }
+
+    public static int mazePath_memo(int sr, int sc, int er, int ec, int[][] dp, int[][] dir) {
+        if (er == sr && ec == sc) {
+            return dp[sr][sc] = 1;
+        }
+
+        if (dp[sr][sc] != 0)
+            return dp[sr][sc];
+
+        int count = 0;
+        for (int[] d : dir) {
+            int r = sr + d[0], c = sc + d[1];
+            if (r >= 0 && c >= 0 && r < dp.length && c < dp[0].length) {
+                count += mazePath_memo(r, c, er, ec, dp, dir);
+            }
+        }
+
+        return dp[sr][sc] = count;
+    }
+
+    public static int mazePath_tabu(int SR, int SC, int ER, int EC, int[][] dp, int[][] dir) {
+        for (int sr = ER; sr >= SR; sr--) {
+            for (int sc = EC; sc >= SC; sc--) {
+                if (ER == sr && EC == sc) {
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+
+                int count = 0;
+                for (int[] d : dir) {
+                    int r = sr + d[0], c = sc + d[1];
+                    if (r >= 0 && c >= 0 && r < dp.length && c < dp[0].length) {
+                        count += dp[r][c];// mazePath_memo(r, c, er, ec, dp, dir);
+                    }
+                }
+
+                dp[sr][sc] = count;
+            }
+        }
+
+        return dp[SR][SC];
+    }
+
+    public static int mazePathJump_tabu(int SR, int SC, int ER, int EC, int[][] dp, int[][] dir) {
+        for (int sr = ER; sr >= SR; sr--) {
+            for (int sc = EC; sc >= SC; sc--) {
+                if (ER == sr && EC == sc) {
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+
+                int count = 0;
+                for (int[] d : dir) {
+                    int r = sr + d[0], c = sc + d[1];
+                    while (r >= 0 && c >= 0 && r < dp.length && c < dp[0].length) {
+                        count += dp[r][c];// mazePath_memo(r, c, er, ec, dp, dir);
+                        r += d[0];
+                        c += d[1];
+                    }
+                }
+                dp[sr][sc] = count;
+            }
+        }
+
+        return dp[SR][SC];
+    }
+
+    public static void mazePath() {
+        int sr = 0, sc = 0, er = 3, ec = 3;
+        int[][] dp = new int[er + 1][ec + 1];
+
+        int[][] dir = { { 1, 0 }, { 0, 1 }, { 1, 1 } };
+        System.out.println(mazePathJump_tabu(sr, sc, er, ec, dp, dir));
+
+        display2D(dp);
+    }
+
+    public static void main(String[] args) {
+        // fibo();
+        mazePath();
+    }
+
+}
