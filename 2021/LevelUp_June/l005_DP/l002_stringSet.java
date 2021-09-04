9aa06d583a76a84635a929d52f3731de2da3bc7a
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-04 13:09:14 +0530

    update

--- 2021/LevelUp_June/l005_DP/l002_stringSet.java
+++ 2021/LevelUp_June/l005_DP/l002_stringSet.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class l002_stringSet {
 
     public static void display(int[] dp) {
@@ -36,7 +38,7 @@ public class l002_stringSet {
                     dp[i][j] = (i == j ? 1 : 0);
                     continue;
                 }
-                
+
                 if (s.charAt(i) == s.charAt(j))
                     dp[i][j] = dp[i + 1][j - 1] + 2;// lpss(s, i + 1, j - 1, dp) + 2;
                 else
@@ -51,6 +53,50 @@ public class l002_stringSet {
         int n = s.length();
         int[][] dp = new int[n][n];
         int ans = lpss(s, 0, n - 1, dp);
+
+        return ans;
+    }
+
+    public static int lcss(String str1, String str2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = 0;
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        if (str1.charAt(n - 1) == str2.charAt(m - 1))
+            dp[n][m] = lcss(str1, str2, n - 1, m - 1, dp) + 1;
+        else
+            dp[n][m] = Math.max(lcss(str1, str2, n - 1, m, dp), lcss(str1, str2, n, m - 1, dp));
+
+        return dp[n][m];
+    }
+
+    public static int lcss_DP(String str1, String str2, int N, int M, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+
+                if (str1.charAt(n - 1) == str2.charAt(m - 1))
+                    dp[n][m] = dp[n - 1][m - 1] + 1;// lcss(str1, str2, n - 1, m - 1, dp) + 1;
+                else
+                    dp[n][m] = Math.max(dp[n - 1][m], dp[n][m - 1]);
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length(), m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        int ans = lcss(text1, text2, n, m, dp);
 
         return ans;
     }
