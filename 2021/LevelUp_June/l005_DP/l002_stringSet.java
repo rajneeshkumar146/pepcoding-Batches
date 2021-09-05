027a7a62e8efb57092bf7f0bd9012af90eefa33b
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-05 10:52:37 +0530

    update

--- 2021/LevelUp_June/l005_DP/l002_stringSet.java
+++ 2021/LevelUp_June/l005_DP/l002_stringSet.java
@@ -100,4 +100,94 @@ public class l002_stringSet {
 
         return ans;
     }
+
+    // 115
+
+    public int numDistinct(String s, String t, int n, int m, int[][] dp) {
+        if (m == 0) {
+            return dp[n][m] = 1;
+        }
+
+        if (n < m) {
+            return dp[n][m] = 0;
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        int a = numDistinct(s, t, n - 1, m - 1, dp);
+        int b = numDistinct(s, t, n - 1, m, dp);
+
+        if (s.charAt(n - 1) == t.charAt(m - 1))
+            return dp[n][m] = a + b;
+        else
+            return dp[n][m] = b;
+    }
+
+    public int numDistinct_DP(String s, String t, int N, int M, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (m == 0) {
+                    dp[n][m] = 1;
+                    continue;
+                }
+
+                if (n < m) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+
+                int a = dp[n - 1][m - 1];// numDistinct(s, t, n - 1, m - 1, dp);
+                int b = dp[n - 1][m];// numDistinct(s, t, n - 1, m, dp);
+
+                if (s.charAt(n - 1) == t.charAt(m - 1))
+                    dp[n][m] = a + b;
+                else
+                    dp[n][m] = b;
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public int numDistinct(String s, String t) {
+        int n = s.length(), m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int ans = numDistinct(s, t, n, m, dp);
+
+        return ans;
+    }
+
+    // 72
+    public int minDistance(String word1, String word2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = (n == 0 ? m : n);
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        int insert = minDistance(word1, word2, n, m - 1, dp);
+        int delete = minDistance(word1, word2, n - 1, m, dp);
+        int replace = minDistance(word1, word2, n - 1, m - 1, dp);
+
+        if (word1.charAt(n - 1) == word2.charAt(m - 1))
+            return dp[n][m] = replace;
+        else
+            return dp[n][m] = Math.min(Math.min(insert, delete), replace) + 1;
+    }
+
+    public int minDistance(String s, String t) {
+        int n = s.length(), m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int ans = minDistance(s, t, n, m, dp);
+
+        return ans;
+    }
 }
