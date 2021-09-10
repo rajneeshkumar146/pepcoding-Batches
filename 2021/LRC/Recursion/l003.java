313b684e8b5a1314e26c40a1a15b8e3c3a258eca
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-10 19:02:15 +0530

    update

--- 2021/LRC/Recursion/l003.java
+++ 2021/LRC/Recursion/l003.java
@@ -28,7 +28,7 @@ public class l003 {
             base.add("");
             return base;
         }
-        
+
         char ch = str.charAt(0);
         int idx = ch - '0';
         String word = words[idx];
@@ -44,6 +44,47 @@ public class l003 {
         }
 
         return myAns;
+    }
+
+    public static ArrayList<String> mazePath(int sr, int sc, int dr, int dc) {
+        if (sr == dr && sc == dc) {
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        ArrayList<String> myRes = new ArrayList<>();
+        if (sc + 1 <= dc) {
+            ArrayList<String> horizontal = mazePath(sr, sc + 1, dr, dc);
+            for (String s : horizontal)
+                myRes.add('h' + s);
+        }
+
+        if (sr + 1 <= dr) {
+            ArrayList<String> vertical = mazePath(sr + 1, sc, dr, dc);
+            for (String s : vertical)
+                myRes.add('v' + s);
+        }
+
+        return myRes;
+    }
+
+    public static ArrayList<String> getStairPath(int n) {
+        if (n == 0) {
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        ArrayList<String> ans = new ArrayList<>();
+        for (int jump = 1; jump <= 3 && n - jump >= 0; jump++) {
+            ArrayList<String> recAns = getStairPath(n - jump);
+            for (String s : recAns) {
+                ans.add(jump + s);
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
