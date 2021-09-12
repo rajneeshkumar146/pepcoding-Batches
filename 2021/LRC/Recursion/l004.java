bf67405fe60ee33db7b5a747cafa2bc5e9245ec0
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-12 14:06:24 +0530

    update

--- 2021/LRC/Recursion/l004.java
+++ 2021/LRC/Recursion/l004.java
@@ -44,9 +44,87 @@ public class l004 {
         return count;
     }
 
+    public static int mazePath(int sr, int sc, int er, int ec, String asf) {
+        if (sr == er && sc == ec) {
+            System.out.println(asf);
+            return 1;
+        }
+
+        int count = 0;
+        if (sc + 1 <= ec)
+            count += mazePath(sr, sc + 1, er, ec, asf + 'h');
+        if (sr + 1 <= er)
+            count += mazePath(sr + 1, sc, er, ec, asf + 'v');
+
+        return count;
+    }
+
+    public static int mazePathJump(int sr, int sc, int er, int ec, String asf) {
+        if (sr == er && sc == ec) {
+            System.out.println(asf);
+            return 1;
+        }
+
+        int count = 0;
+        for (int jump = 1; sc + jump <= ec; jump++)
+            count += mazePathJump(sr, sc + jump, er, ec, asf + 'h' + jump + " ");
+        for (int jump = 1; sr + jump <= er; jump++)
+            count += mazePathJump(sr + jump, sc, er, ec, asf + 'v' + jump + " ");
+
+        return count;
+    }
+
+    public static int mazePath_02(int sr, int sc, int er, int ec, int[][] dir, String[] dirS, String asf) {
+        if (sr == er && sc == ec) {
+            System.out.println(asf);
+            return 1;
+        }
+
+        int count = 0;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r <= er && c <= ec) {
+                count += mazePath_02(r, c, er, ec, dir, dirS, asf + dirS[d]);
+            }
+        }
+
+        return count;
+    }
+
+    public static int mazePathJump_02(int sr, int sc, int er, int ec, int[][] dir, String[] dirS, String asf) {
+        if (sr == er && sc == ec) {
+            System.out.println(asf);
+            return 1;
+        }
+
+        int count = 0;
+        for (int d = 0; d < dir.length; d++) {
+            for (int rad = 1; rad <= Math.max(er, ec); rad++) {
+                int r = sr + rad * dir[d][0];
+                int c = sc + rad * dir[d][1];
+
+                if (r >= 0 && c >= 0 && r <= er && c <= ec) {
+                    count += mazePathJump_02(r, c, er, ec, dir, dirS, asf + dirS[d] + rad + " ");
+                } else
+                    break;
+            }
+
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         // System.out.println(printSubseq("abc", 0, ""));
         // System.out.println(printKPC("572", 0, ""));
-        System.out.println(stairPath(5,""));
+        // System.out.println(mazePath(0, 0, 2, 2, ""));
+
+        int[][] dir = { { 0, 1 }, { 1, 0 },{1,1} };
+        String[] dirS = { "h", "v","d" };
+
+        System.out.println(mazePathJump_02(0, 0, 2, 2, dir, dirS, ""));
+        // System.out.println(mazePathJump(0, 0, 2, 2, ""));
     }
 }
