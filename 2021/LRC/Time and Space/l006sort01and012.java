42e6544abae6e0471a17c3a0351383eb5959d43f
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-18 17:56:33 +0530

    update

--- 2021/LRC/Time and Space/l006sort01and012.java
+++ 2021/LRC/Time and Space/l006sort01and012.java
@@ -14,6 +14,61 @@ public class l006sort01and012 {
 
             itr++;
         }
+    }
+
+    public static void sort012(int[] arr) {
+        int pt1 = -1, n = arr.length, pt2 = n - 1, itr = 0;
+
+        while (itr <= pt2) {
+            if (arr[itr] == 0)
+                swap(arr, ++pt1, itr++);
+            else if (arr[itr] == 1)
+                itr++;
+            else
+                swap(arr, itr, pt2--);
+        }
+    }
+
+    public static void partition(int[] arr, int pivot) {
+        int n = arr.length, pt = -1, itr = 0;
+        while (itr < n) {
+            if (arr[itr] <= pivot)
+                swap(arr, ++pt, itr);
+
+            itr++;
+        }
+    }
+
+    public static int partition_02(int[] arr, int pIdx, int si, int ei) {
+        int pt = si - 1, itr = si;
+        int pivot = arr[pIdx];
+        swap(arr, pIdx, ei);
+
+        while (itr <= ei) {
+            if (arr[itr] <= pivot)
+                swap(arr, ++pt, itr);
+            itr++;
+        }
+
+        return pt;
+    }
+
+    public static void quickSort(int[] arr, int si, int ei) {
+        if (si >= ei)
+            return;
+
+        int pIdx = (si + ei) / 2;
+        pIdx = partition_02(arr, pIdx);
+
+        quickSort(arr, si, pIdx - 1);
+        quickSort(arr, pIdx + 1, ei);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 7, -12, 15, 19, -13, 0, 8, 63, 97, -16, 69, 35, 21, 22 };
+        // partition_02(arr, 1);
+        // for (int ele : arr)
+        // System.out.print(ele + " ");
 
     }
 }
