a9241fea2e7e5768b1ae27ecdeca4d0185952fde
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-23 20:34:43 +0530

    update

--- 2021/LevelUp_June/l005_DP/l006_cutType.java
+++ 2021/LevelUp_June/l005_DP/l006_cutType.java
@@ -136,6 +136,92 @@ public class l006_cutType {
     }
 
     // 312
+    public int maxCoins(int[] nums, int si, int ei, int[][] dp) {
+        if (dp[si][ei] != 0)
+            return dp[si][ei];
+
+        int lele = si == 0 ? 1 : nums[si - 1];
+        int rele = ei == nums.length - 1 ? 1 : nums[ei + 1];
+
+        int maxCoins = 0;
+        for (int cut = si; cut <= ei; cut++) {
+            int lcost = cut == si ? 0 : maxCoins(nums, si, cut - 1, dp);
+            int rcost = cut == ei ? 0 : maxCoins(nums, cut + 1, ei, dp);
+
+            maxCoins = Math.max(maxCoins, lcost + lele * nums[cut] * rele + rcost);
+        }
+
+        return dp[si][ei] = maxCoins;
+    }
+
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        return maxCoins(nums, 0, n - 1, dp);
+
+    }
+
+    public static class pairBoolen {
+        long totalTrue = 0;
+        long totalFalse = 0;
+
+        pairBoolen(long totalTrue, long totalFalse) {
+            this.totalFalse = totalFalse;
+            this.totalTrue = totalTrue;
+        }
+
+        pairBoolen() {
+
+        }
+    }
+
+    public static void evaluateTrue(pairBoolen left, pairBoolen right, pairBoolen res, char operator) {
+        long mod = 1003;
+        long totalTF = ((left.totalTrue + left.totalFalse) * (right.totalTrue + right.totalFalse)) % mod;
+        long totalTrue = 0, totalFalse = 0;
+        if (operator == '|') {
+            totalFalse = (left.totalFalse * right.totalFalse) % mod;
+            totalTrue = (totalTF - totalFalse + mod) % mod;
+        } else if (operator == '^') {
+            totalTrue = (left.totalFalse * right.totalTrue) % mod + (left.totalTrue * right.totalFalse) % mod;
+            totalFalse = (totalTF - totalTrue + mod) % mod;
+
+        } else if (operator == '&') {
+            totalTrue = (left.totalTrue * right.totalTrue) % mod;
+            totalFalse = (totalTF - totalTrue + mod) % mod;
+        }
+
+        res.totalFalse = (res.totalFalse + totalFalse) % mod;
+        res.totalTrue = (res.totalTrue + totalTrue) % mod;
+    }
+
+    public static pairBoolen countWays(String S, int si, int ei, pairBoolen[][] dp) {
+        if (si == ei) {
+            char ch = S.charAt(si);
+            int t = ch == 'T' ? 1 : 0;
+            int f = ch == 'F' ? 1 : 0;
+            return dp[si][ei] = new pairBoolen(t, f);
+        }
+        if (dp[si][ei] != null) {
+            return dp[si][ei];
+        }
+
+        pairBoolen res = new pairBoolen();
+        for (int cut = si + 1; cut < ei; cut += 2) {
+            pairBoolen lres = countWays(S, si, cut - 1, dp);
+            pairBoolen rres = countWays(S, cut + 1, ei, dp);
+
+            evaluateTrue(lres, rres, res, S.charAt(cut));
+        }
+
+        return dp[si][ei] = res;
+    }
+
+    static int countWays(int N, String S) {
+        pairBoolen[][] dp = new pairBoolen[N][N];
+        return (int)countWays(S, 0, N - 1, dp).totalTrue;
+    }
 
     public static void main(String... args) {
         // mcm();
