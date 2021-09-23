f2d9c63bb8bf11e98272955e9254d7b6664f0607
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-23 22:00:28 +0530

    update

--- 2021/LevelUp_June/l005_DP/l006_cutType.java
+++ 2021/LevelUp_June/l005_DP/l006_cutType.java
@@ -220,7 +220,27 @@ public class l006_cutType {
 
     static int countWays(int N, String S) {
         pairBoolen[][] dp = new pairBoolen[N][N];
-        return (int)countWays(S, 0, N - 1, dp).totalTrue;
+        return (int) countWays(S, 0, N - 1, dp).totalTrue;
+    }
+
+    public static int obst(int[] val, int[] freq, int si, int ei, int[][] dp) {
+        if (dp[si][ei] != 0)
+            return dp[si][ei];
+        int minCost = (int) 1e8;
+
+        int sum = 0;
+
+        for (int i = si; i <= ei; i++)
+            sum += freq[i];
+
+        for (int cut = si; cut <= ei; cut++) {
+            int lres = cut == si ? 0 : obst(val, freq, si, cut - 1, dp);
+            int rres = cut == ei ? 0 : obst(val, freq, cut + 1, ei, dp);
+            // sum += freq[cut];
+            minCost = Math.min(minCost, lres + sum + rres); // sum : sumOfRange(freq, si,ei);
+        }
+
+        return dp[si][ei] = minCost; // minCost + sum
     }
 
     public static void main(String... args) {
