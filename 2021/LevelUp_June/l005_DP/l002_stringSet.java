903cbc2a1f0a96da4aceff89505394c9ce65e6bd
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-26 12:42:48 +0530

    update

--- 2021/LevelUp_June/l005_DP/l002_stringSet.java
+++ 2021/LevelUp_June/l005_DP/l002_stringSet.java
@@ -643,20 +643,20 @@ public class l002_stringSet {
     }
 
     // 213
-    public int rob(int[] arr, int si, int ei, int[] dp) {
+    public int rob_II(int[] arr, int si, int ei, int[] dp) {
         if (si > ei)
             return 0;
 
         if (dp[si] != -1)
             return dp[si];
 
-        int robCurrHouse = arr[si] + rob(arr, si + 2, ei, dp);
-        int notRobCurrHouse = rob(arr, si + 1, ei, dp);
+        int robCurrHouse = arr[si] + rob_II(arr, si + 2, ei, dp);
+        int notRobCurrHouse = rob_II(arr, si + 1, ei, dp);
 
         return dp[si] = Math.max(robCurrHouse, notRobCurrHouse);
     }
 
-    public int rob(int[] nums) {
+    public int rob_II(int[] nums) {
         int n = nums.length;
         if (n == 0 || n == 1) {
             return n == 1 ? nums[0] : 0;
@@ -667,7 +667,32 @@ public class l002_stringSet {
         int[] dp2 = new int[n];
         Arrays.fill(dp2, -1);
 
-        return Math.max(rob(nums, 0, n - 2, dp1), rob(nums, 1, n - 1, dp2));
+        return Math.max(rob_II(nums, 0, n - 2, dp1), rob_II(nums, 1, n - 1, dp2));
 
+    }
+
+    // 198
+    public int rob_I(int[] arr, int n, int[] dp) {
+        if (n <= 0)
+            return 0;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        int robCurrHouse = arr[n - 1] + rob_I(arr, n - 2, dp);
+        int notRobCurrHouse = rob_I(arr, n - 1, dp);
+
+        return dp[n] = Math.max(robCurrHouse, notRobCurrHouse);
+    }
+
+    public int rob_I(int[] nums) {
+        int n = nums.length;
+        if (n == 0 || n == 1) {
+            return n == 1 ? nums[0] : 0;
+        }
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return rob_I(nums, n, dp);
     }
 }
