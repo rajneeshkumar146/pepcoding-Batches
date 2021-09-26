82f9a62fd2d44b7174c47cfd37a75f642765487e
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-09-26 11:45:46 +0530

    update

--- 2021/LevelUp_June/l005_DP/l002_stringSet.java
+++ 2021/LevelUp_June/l005_DP/l002_stringSet.java
@@ -591,4 +591,83 @@ public class l002_stringSet {
 
         return isMatch(s, p, n, m, dp) == 1;
     }
+
+    // 1278
+    public int[][] minChanges(String str) {
+        int n = str.length();
+        int[][] dp = new int[n][n];
+        for (int gap = 1; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (gap == 1)
+                    dp[i][j] = str.charAt(i) == str.charAt(j) ? 0 : 1;
+                else
+                    dp[i][j] = str.charAt(i) == str.charAt(j) ? dp[i + 1][j - 1] : dp[i + 1][j - 1] + 1;
+            }
+        }
+
+        return dp;
+    }
+
+    public int palindromePartition(String s, int k, int si, int[][] dp, int[][] minChanges) {
+        if (s.length() - si <= k) {
+            return dp[si][k] = s.length() - si == k ? 0 : (int) 1e9;
+        }
+
+        if (k == 1)
+            dp[si][k] = minChanges[si][s.length() - 1];
+
+        if (dp[si][k] != -1) {
+            return dp[si][k];
+        }
+
+        int minAns = (int) 1e9;
+        for (int i = si; i < s.length(); i++) {
+            int minChangesInMyString = minChanges[si][i];
+            int minChangesInRecString = palindromePartition(s, k - 1, i + 1, dp, minChanges);
+
+            if (minChangesInRecString != (int) 1e9)
+                minAns = Math.min(minAns, minChangesInRecString + minChangesInMyString);
+        }
+
+        return dp[si][k] = minAns;
+    }
+
+    public int palindromePartition(String s, int k) {
+        int n = s.length();
+        int[][] minChanges = minChanges(s);
+        int[][] dp = new int[n + 1][k + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        return palindromePartition(s, k, 0, dp, minChanges);
+    }
+
+    // 213
+    public int rob(int[] arr, int si, int ei, int[] dp) {
+        if (si > ei)
+            return 0;
+
+        if (dp[si] != -1)
+            return dp[si];
+
+        int robCurrHouse = arr[si] + rob(arr, si + 2, ei, dp);
+        int notRobCurrHouse = rob(arr, si + 1, ei, dp);
+
+        return dp[si] = Math.max(robCurrHouse, notRobCurrHouse);
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0 || n == 1) {
+            return n == 1 ? nums[0] : 0;
+        }
+        int[] dp1 = new int[n];
+        Arrays.fill(dp1, -1);
+
+        int[] dp2 = new int[n];
+        Arrays.fill(dp2, -1);
+
+        return Math.max(rob(nums, 0, n - 2, dp1), rob(nums, 1, n - 1, dp2));
+
+    }
 }
