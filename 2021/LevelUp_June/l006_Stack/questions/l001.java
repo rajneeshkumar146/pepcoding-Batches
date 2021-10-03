fcfa854123f803e2ca5c86a45cfb512bb8c54891
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-10-03 21:59:46 +0530

    update

--- 2021/LevelUp_June/l006_Stack/questions/l001.java
+++ 2021/LevelUp_June/l006_Stack/questions/l001.java
@@ -61,13 +61,95 @@ public class l001 {
         st.addFirst(-1);
 
         for (int i = n - 1; i >= 0; i--) {
-            while (st.getFirst() != -1 && arr[st.getFirst()] > arr[i]) 
+            while (st.getFirst() != -1 && arr[st.getFirst()] > arr[i])
                 ans[st.removeFirst()] = i;
 
             st.addFirst(i);
         }
 
         return ans;
+    }
+
+    // 503
+    public int[] nextGreaterElements(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+
+        for (int i = 0; i < 2 * n; i++) {
+            while (st.getFirst() != -1 && arr[st.getFirst()] < arr[i % n])
+                ans[st.removeFirst()] = i % n;
+
+            if (i < n)
+                st.addFirst(i);
+        }
+
+        return ans;
+    }
+
+    // https://practice.geeksforgeeks.org/problems/stock-span-problem-1587115621/1
+    public static int[] calculateSpan(int[] arr, int n) {
+        int[] ans = new int[n];
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+
+        for (int i = 0; i < n; i++) {
+            while (st.getFirst() != -1 && arr[st.getFirst()] <= arr[i])
+                st.removeFirst();
+
+            ans[i] = i - st.getFirst();
+            st.addFirst(i);
+        }
+
+        return ans;
+    }
+
+    // 901
+    class StockSpanner {
+        int day = 0;
+        LinkedList<int[]> st = new LinkedList<>();
+
+        public StockSpanner() {
+            // {idx, val}
+            st.addFirst(new int[] { -1, -1 });
+        }
+
+        public int next(int price) {
+            while (st.getFirst()[0] != -1 && st.getFirst()[1] <= price)
+                st.removeFirst();
+
+            int span = day - st.getFirst()[0];
+            st.addFirst(new int[] { day++, price });
+            return span;
+        }
+    }
+
+    // HM -> 739
+
+    // 20
+    public boolean isValid(String str) {
+        LinkedList<Character> st = new LinkedList<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{')
+                st.addFirst(ch);
+            else {
+                if (st.size() == 0)
+                    return false;
+                else if (st.getFirst() == '(' && ch != ')')
+                    return false;
+                else if (st.getFirst() == '[' && ch != ']')
+                    return false;
+                else if (st.getFirst() == '{' && ch != '}')
+                    return false;
+                else
+                    st.removeFirst();
+            }
+        }
+
+        return st.size() == 0;
     }
 
 }
