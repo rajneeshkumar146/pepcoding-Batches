b46f3f7e92030989a0004bfc3e49f81333cea57d
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-10-03 20:19:11 +0530

    update

---
+++ 2021/LevelUp_June/l006_Stack/questions/l001.java
@@ -0,0 +1,73 @@
+import java.util.LinkedList;
+import java.util.Arrays;
+
+public class l001 {
+    public static int[] NGOR(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, n);
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+
+        for (int i = 0; i < n; i++) {
+            while (st.getFirst() != -1 && arr[st.getFirst()] < arr[i])
+                ans[st.removeFirst()] = i;
+
+            st.addFirst(i);
+        }
+
+        return ans;
+    }
+
+    public static int[] NSOR(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, n);
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+
+        for (int i = 0; i < n; i++) {
+            while (st.getFirst() != -1 && arr[st.getFirst()] > arr[i])
+                ans[st.removeFirst()] = i;
+
+            st.addFirst(i);
+        }
+
+        return ans;
+    }
+
+    public static int[] NGOL(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.getFirst() != -1 && arr[st.getFirst()] < arr[i])
+                ans[st.removeFirst()] = i;
+
+            st.addFirst(i);
+        }
+
+        return ans;
+    }
+
+    public static int[] NSOL(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.getFirst() != -1 && arr[st.getFirst()] > arr[i]) 
+                ans[st.removeFirst()] = i;
+
+            st.addFirst(i);
+        }
+
+        return ans;
+    }
+
+}
