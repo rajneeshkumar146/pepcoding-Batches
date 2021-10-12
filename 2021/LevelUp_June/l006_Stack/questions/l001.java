69b97ccec713b323aa12e7353933ce701b720c91
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-10-12 22:01:14 +0530

    update

--- 2021/LevelUp_June/l006_Stack/questions/l001.java
+++ 2021/LevelUp_June/l006_Stack/questions/l001.java
@@ -472,7 +472,7 @@ public class l001 {
         }
     }
 
-    //155
+    // 155
     class MinStack {
         LinkedList<Long> st = new LinkedList<>();
         long minSf = 0;
@@ -507,15 +507,78 @@ public class l001 {
 
         public int top() {
             if (st.getFirst() < minSf) {
-                return (int)minSf;
+                return (int) minSf;
             }
 
-            return (int)(long)st.getFirst();
+            return (int) (long) st.getFirst();
         }
 
         public int getMin() {
-            return (int)minSf;
+            return (int) minSf;
         }
+    }
+
+    public int[] exclusiveTime(int n, List<String> logs) {
+        class logPair {
+            int id, timeStamp, sleepTime;
+            boolean isStart = false;
+
+            logPair(String str) {
+                String[] ar = str.split(":");
+                this.id = Integer.parseInt(ar[0]);
+                this.timeStamp = Integer.parseInt(ar[2]);
+                this.isStart = ar[1].equals("start");
+                this.sleepTime = 0;
+            }
+        }
+
+        LinkedList<logPair> st = new LinkedList<>();
+        int[] ans = new int[n];
+
+        for (String s : logs) {
+            logPair log = new logPair(s);
+
+            if (log.isStart)
+                st.addFirst(log);
+            else {
+                logPair rp = st.removeFirst();
+                ans[rp.id] += log.timeStamp - rp.timeStamp + 1 - rp.sleepTime;
+
+                if (st.size() != 0)
+                    st.getFirst().sleepTime += log.timeStamp - rp.timeStamp + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // 853
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = speed.length;
+
+        // {position, time}
+        double[][] disTimeData = new double[n][2];
+        for (int i = 0; i < n; i++) {
+            disTimeData[i][0] = position[i] * 1.0;
+            disTimeData[i][1] = ((target - position[i]) * 1.0) / speed[i];
+        }
+
+        Arrays.sort(disTimeData, (a, b) -> {
+            return (int) (a[0] - b[0]);
+        });
+
+        int carFleet = 1;
+        double prevTime = disTimeData[n - 1][1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (disTimeData[i][1] > prevTime) {
+                prevTime = disTimeData[i][1];
+                carFleet++;
+            } else {
+                // fleet will be same;
+            }
+        }
+
+        return carFleet;
     }
 
 }
