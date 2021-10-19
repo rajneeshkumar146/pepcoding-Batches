4828e3760bbb9b3141e7602f02bbf73926155bf8
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-10-19 22:12:50 +0530

    update

--- 2021/LevelUp_June/l007_HashMapHeap/HashMapQuestions.java
+++ 2021/LevelUp_June/l007_HashMapHeap/HashMapQuestions.java
@@ -1,3 +1,30 @@
+import java.util.HashSet;
+
 public class HashMapQuestions {
+
+    // 128
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> map = new HashSet<>();
+        for (int ele : nums)
+            map.add(ele);
+
+        int len = 0;
+        for (int ele : nums) {
+            if (!map.contains(ele))
+                continue;
+
+            int prev = ele - 1, next = ele + 1;
+            map.remove(ele);
+            while (map.contains(prev))
+                map.remove(prev--);
+            while (map.contains(next))
+                map.remove(next++);
+
+            len = Math.max(len, next - prev - 1);
+        }
+
+        return len;
+
+    }
 
 }
--- 2021/LevelUp_June/l007_HashMapHeap/heapQuestions.java
+++ 2021/LevelUp_June/l007_HashMapHeap/heapQuestions.java
@@ -136,4 +136,72 @@ public class heapQuestions {
 
         return minHeight;
     }
+
+    //1642
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        int n = heights.length;
+        for(int i = 1; i < n;i++){
+            int currDiff = heights[i] - heights[i - 1];
+            if(currDiff > 0){
+                pq.add(currDiff);
+                
+                if(pq.size() > ladders){
+                    bricks -= pq.remove();
+                }
+                
+                if(bricks < 0) return i - 1; 
+            }
+        }
+        
+        return  n - 1;
+    }
+
+    //632
+    public int[] smallestRange(List<List<Integer>> nums) {
+        
+        int n = nums.size();
+        
+        //{r,c}
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            int r1 = a[0], c1 = a[1];
+            int r2 = b[0], c2 = b[1];
+            
+            return nums.get(r1).get(c1) - nums.get(r2).get(c2);
+            
+        });
+        
+        int maxValue = -(int)1e9;
+        
+        for(int i = 0; i < n;i++){
+            pq.add(new int[]{i,0});
+            maxValue = Math.max(maxValue, nums.get(i).get(0));
+        }
+        
+        int range = (int)1e9, sp = -1, ep = -1;
+        while(pq.size() == n){
+            
+            int[] re = pq.remove();
+            int r = re[0], c = re[1], val = nums.get(r).get(c);
+            
+            if(maxValue - val < range){
+                range = maxValue - val;
+                sp = val;
+                ep =  maxValue;
+            }
+            
+            c++;
+            if(c < nums.get(r).size()){
+                pq.add(new int[]{r,c});
+                maxValue = Math.max(maxValue, nums.get(r).get(c));
+            }
+        }
+        
+        return new int[]{sp,ep};
+    }
+
+
+
+
 }
