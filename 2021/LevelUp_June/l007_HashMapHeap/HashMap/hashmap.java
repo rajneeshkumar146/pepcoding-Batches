c83a0274eaf8822f3f9efffefee15c470bb87cff
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-10-25 21:40:30 +0530

    update

---
+++ 2021/LevelUp_June/l007_HashMapHeap/HashMap/hashmap.java
@@ -0,0 +1,158 @@
+import java.util.ArrayList;
+
+public class hashmap {
+
+    private class Node {
+        Integer key = 0;
+        Integer value = 0;
+        Node next = null;
+
+        Node(Integer key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private class linkedlist {
+        public Node head = null;
+        public Node tail = null;
+        public int NoOfElements = 0;
+
+        public linkedlist() {
+            this.head = this.tail = null;
+            this.NoOfElements = 0;
+        }
+
+        public int size() {
+            return NoOfElements;
+        }
+
+        public void addLast(Node node) {
+            if (head == null)
+                head = tail = node;
+            else {
+                this.tail.next = node;
+                this.tail = node;
+            }
+
+            NoOfElements++;
+        }
+
+        public int getFirst() {
+            return this.head.key;
+        }
+
+        public Node removeFirst() {
+            Node node = this.head;
+            if (this.NoOfElements == 0)
+                head = tail = null;
+            else {
+                this.head = node.next;
+                node.next = null;
+            }
+
+            NoOfElements--;
+            return node;
+        }
+    }
+
+    private linkedlist[] containers;
+    private int sizeOfHM = 0;
+
+    public void assignValues(int size) {
+        containers = new linkedlist[size];
+        for (int i = 0; i < size; i++) {
+            containers[i] = new linkedlist();
+        }
+    }
+
+    private void rehash(){
+        
+    }
+
+    public void put(Integer key, Integer value) {
+        boolean isKey = containsKey(key);
+        linkedlist group = group(key);
+        if(isKey){
+            group.head.value = value;
+        }else{
+            group.addLast(new Node(key,value));
+            this.sizeOfHM++;
+
+            double lambda = (group.size() / this.containers.length * 1.0);
+            if(lambda > 0.745){
+                rehash();
+            }
+        }
+
+    }
+
+    public void putIfAbsent(Integer key, Integer defualtValue) {
+        boolean isKey = containsKey(key);
+        if (!isKey)
+            put(key, defualtValue);
+    }
+
+    public ArrayList<Integer> keySet() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < this.containers.length; i++) {
+            linkedlist group = this.containers[i];
+            int size = group.size();
+
+            while (size-- > 0) {
+                Node node = group.removeFirst();
+                list.add(node.key);
+                group.addLast(node);
+            }
+        }
+
+        return list;
+    }
+
+    public Integer remove(Integer key) {
+        boolean isKey = containsKey(key);
+        linkedlist group = group(key);
+        if (!isKey)
+            return null;
+
+        Node node = group.removeFirst();
+        this.sizeOfHM--;
+        return node.key;
+    }
+
+    public Integer getOrDefault(Integer key, Integer defaultValue) {
+        Integer value = get(key);
+        return value != null ? value : defaultValue;
+    }
+
+    public Integer get(Integer key) {
+        boolean isKey = containsKey(key);
+        linkedlist group = group(key);
+
+        return isKey ? group.head.value : null;
+    }
+
+    public boolean containsKey(Integer key) {
+        linkedlist group = group(key);
+        int size = group.size();
+
+        while (size-- > 0) {
+            if (group.getFirst() == key)
+                return true;
+
+            group.addLast(group.removeFirst());
+        }
+
+        return false;
+    }
+
+    private linkedlist group(Integer key) {
+        int code = hashCode(key);
+        return this.containers[code];
+    }
+
+    private int hashCode(Integer key) {
+        int value = key.hashCode();
+        return value % containers.length;
+    }
+}
