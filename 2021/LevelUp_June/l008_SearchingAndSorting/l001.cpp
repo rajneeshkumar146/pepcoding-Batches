1759955224a8dda503dd6c0d9c741b186d40d379
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-10-26 21:59:09 +0530

    update

--- 2021/LevelUp_June/l008_SearchingAndSorting/l001.cpp
+++ 2021/LevelUp_June/l008_SearchingAndSorting/l001.cpp
@@ -113,3 +113,22 @@ int perfectLocation(vector<int> &arr, int data)
 
     return ei; // si
 }
+
+bool searchMatrix(vector<vector<int>> &matrix, int target)
+{
+    int n = matrix.size(), m = matrix[0].size();
+    int si = 0, ei = n * m - 1;
+
+    while (si <= ei)
+    {
+        int mid = (si + ei) / 2, r = mid / m, c = mid % m;
+        if (matrix[r][c] == target)
+            return true;
+        else if (matrix[r][c] < target)
+            si = mid + 1;
+        else
+            ei = mid - 1;
+    }
+
+    return false;
+}
