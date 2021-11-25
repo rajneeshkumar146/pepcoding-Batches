3598384d4079d9dc8d95ffcffccc3794a9c471ef
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-11-25 22:26:28 +0530

    update

--- 2021/FJP3/lecture_002/l001.java
+++ 2021/FJP3/lecture_002/l001.java
@@ -295,6 +295,28 @@ public class l001 {
     }
 
     public static void pattern16(int n) {
+        int nst = 1, nsp = 2 * n - 3;
+
+        for (int r = 1; r <= n; r++) {
+            int count = 1;
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(count + "\t");
+            }
+
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("\t");
+            }
+
+            if (r == n)
+                nst--;
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(count + "\t");
+            }
+
+            nst++;
+            nsp -= 2;
+            System.out.println();
+        }
 
     }
 
@@ -364,6 +386,6 @@ public class l001 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        pattern14(n);
+        pattern16(n);
     }
 }
