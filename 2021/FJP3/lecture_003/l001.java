a67d6cbf99ce750fd1c77e32c0ae99d7b08be3f1
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-11-26 22:07:17 +0530

    update

---
+++ 2021/FJP3/lecture_003/l001.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class l001 {
+    public static int getDigitFrequency(int n, int d) {
+        int occu = 0;
+        while (n != 0) {
+            int ld = n % 10;
+            n /= 10;
+            if (ld == d)
+                occu++;
+        }
+
+        return occu;
+    }
+
+    public static int decimalToBinary(int n) {
+        int ans = 0, pow = 1;
+        while (n != 0) {
+            int rem = n % 2;
+            n /= 2;
+
+            ans += rem * pow;
+            pow = pow * 10;
+        }
+
+        return ans;
+    }
+
+    // 1 < b <= 10
+    public static int decimalToanyBase(int n, int b) {
+
+    }
+
+    // 1 < b <= 10
+    public static int anyBaseToDecimal(int n, int b) {
+
+    }
+
+    public static int binaryToDecimal(int n) {
+        int ans = 0, pow = 1;
+        while (n != 0) {
+            int rem = n % 10;
+            n /= 10;
+
+            ans += rem * pow;
+            pow = pow * 2;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int binary = decimalToBinary(n);
+        System.out.println(binary);
+        
+        int decimal = binaryToDecimal(binary);
+        System.out.println(decimal);
+    }
+
+}
