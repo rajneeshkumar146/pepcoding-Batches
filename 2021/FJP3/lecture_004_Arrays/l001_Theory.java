90f7eae41481a4219400418dd352aac264fbde47
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-11-29 23:49:31 +0530

    update

--- 2021/FJP3/lecture_004_Arrays/l001_Theory.java
+++ 2021/FJP3/lecture_004_Arrays/l001_Theory.java
@@ -5,30 +5,54 @@ public class l001_Theory {
 
     public static void takeInput(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = scn.nextInt();   // setter
+            arr[i] = scn.nextInt(); // setter
         }
     }
 
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");  // getter
+            System.out.print(arr[i] + " "); // getter
         }
         System.out.println();
     }
 
-    public static boolean findElement(int[] arr,int data){
+    public static boolean findElement(int[] arr, int data) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == data)
+                return true;
+        }
+        return false;
+    }
+
+    public static int maximum(int[] arr) {
+        int n = arr.length, maxEle = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            maxEle = Math.max(maxEle, arr[i]);
+        }
+
+        return maxEle;
+    }
+
+    public static int minimum(int[] arr) {
+        int n = arr.length, minEle = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            minEle = Math.min(minEle, arr[i]);
+        }
+
+        return minEle;
+    }
+
+    // {2,3,4,4,4,4,4,4,4,2}, firstIndex of 4 is 2
+    public static int firstIndex(int[] arr, int data) {
 
     }
 
-    public static int maximum(int[] arr){
-
+    // {2,3,4,4,4,4,4,4,4,2}, lastIndex of 4 is 8
+    public static int lastIndex(int[] arr, int data) {
     }
-
-    public static int minimum(int[] arr){
-
-    }
-
-
 
     public static void main(String[] args) {
         int n = scn.nextInt();
