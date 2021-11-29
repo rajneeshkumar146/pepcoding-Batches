a65c8d39b061573959948fec57cf5c66b234ed26
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-11-29 22:17:30 +0530

    update

--- 2021/FJP3/lecture_003/l001.java
+++ 2021/FJP3/lecture_003/l001.java
@@ -115,6 +115,70 @@ public class l001 {
     // m - n = ??
     public static int decimalSubtraction(int n, int m) {
         int ans = 0, pow = 1, borrow = 0;
+        while (m != 0) {
+            int sub = borrow + m % 10 - n % 10;
+            n /= 10;
+            m /= 10;
+
+            if (sub < 0) {
+                sub += 10;
+                borrow = -1;
+            } else
+                borrow = 0;
+
+            ans += sub * pow;
+            pow *= 10;
+        }
+
+        return ans;
+    }
+
+    public static int anyBaseSubtraction(int n, int m, int base) {
+        int ans = 0, pow = 1, borrow = 0;
+        while (m != 0) {
+            int sub = borrow + m % 10 - n % 10;
+            n /= 10;
+            m /= 10;
+
+            if (sub < 0) {
+                sub += base;
+                borrow = -1;
+            } else
+                borrow = 0;
+
+            ans += sub * pow;
+            pow *= 10;
+        }
+
+        return ans;
+    }
+
+    public static int multiplyDigitWithNumber(int n, int digit, int base) {
+        int ans = 0, carry = 0, pow = 1;
+        while (n != 0 || carry != 0) {
+            int prod = carry + (n % 10) * digit;
+            n /= 10;
+
+            carry = prod / base;
+            prod %= base;
+
+            ans += prod * pow;
+            pow *= 10;
+        }
+
+        return ans;
+    }
+
+    public static int getProduct(int b, int n, int m) {
+        int ans = 0, pow = 1;
+        while (m != 0) {
+            int digitMultiply = multiplyDigitWithNumber(n, m % 10, b);
+            m /= 10;
+
+            ans = anyBaseAddition(ans, digitMultiply * pow, b);
+            pow *= 10;
+        }
+
         return ans;
     }
 
