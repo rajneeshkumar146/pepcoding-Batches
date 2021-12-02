f966c5292e89065a4c0607d9b715202610d0a64b
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-12-02 22:50:38 +0530

    add

---
+++ 2021/FJP3/lecture_004_Arrays/inverseAnArray/Main.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + "\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static int[] inverse(int[] a){
+    int [] ans = new int[a.length];
+    for(int i = 0; i<a.length; i++){
+        int val = a[i];
+        ans[val]= i;
+    }
+    return ans;
+    
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] inv = inverse(a);
+    display(inv);
+ }
+
+}
