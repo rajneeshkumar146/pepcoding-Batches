44b229643f667d5672f6d5a2c3801271e7841ee2
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-12-03 21:49:49 +0530

    binarySearch

---
+++ 2021/FJP3/lecture_004_Arrays/binarySearch/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main{
+    public  static int binarySearch(int [] arr, int x){
+        // return the index of element else -1
+        
+    }
+        
+
+    public static void main(String [] args){
+        int [] arr = {4,6,8,10,12,14,16,18,20,22,24};
+        int x = 20;
+        // int x = 24;
+        // int x = 11;
+        int ans = binarySearch(arr,x);
+        System.out.println(ans);
+
+    }
+}
