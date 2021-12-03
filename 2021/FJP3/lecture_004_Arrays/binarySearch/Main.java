f92ec48796996d1279574156bcc01fae72686022
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-12-03 22:16:30 +0530

    code

--- 2021/FJP3/lecture_004_Arrays/binarySearch/Main.java
+++ 2021/FJP3/lecture_004_Arrays/binarySearch/Main.java
@@ -2,12 +2,31 @@ import java.util.*;
 
 public class Main{
     public  static int binarySearch(int [] arr, int x){
-        // return the index of element else -1
+        int li = 0;
+        int ri = arr.length-1;
+
+        while(li<=ri){
+            int mid = (li+ri)/2;
+
+            if(arr[mid] == x){
+                return mid;
+            }
+            else if(arr[mid] > x){
+                //left side me dhundho
+                ri = mid-1;
+            }
+            else if(arr[mid] < x){
+                //right me dhundho
+                li = mid + 1;
+            }
+        }
+        return -1;
         
     }
         
 
     public static void main(String [] args){
+        //incre
         int [] arr = {4,6,8,10,12,14,16,18,20,22,24};
         int x = 20;
         // int x = 24;
