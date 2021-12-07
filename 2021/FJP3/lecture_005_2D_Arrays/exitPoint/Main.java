9f785d0d804f87deb6bfb1dde98555520a05ca5d
rajneeshkumar146 <rajneeshkumar@gmail.com>
2021-12-07 21:50:20 +0530

    added

---
+++ 2021/FJP3/lecture_005_2D_Arrays/exitPoint/Main.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static Scanner scn = new Scanner(System.in);
+    
+    
+    public static void exitPoint(int [][] arr){
+        //east 0. south 1, west = 2, north = 3
+        
+        int dir = 0;
+        
+        //initially top left 0,0
+        int i = 0;
+        int j = 0;
+        int n = arr.length;
+        int m = arr[0].length;
+        
+        while(true){
+            dir += arr[i][j];
+            dir %= 4;
+            
+            if(dir == 0){
+                
+            }
+            else if(dir == 1){
+                
+            }
+            else if(dir == 2){
+                
+            }
+            else{
+                //dir == 3
+                
+            }
+            
+            if( i < 0 || j < 0 || i >= n || j >= m){
+                break;
+            }
+            
+        }
+        
+        //from which cell I have gone out of grid
+        
+        if(i < 0){
+            i++;
+        }
+        else if(i>=n){
+            i--;
+        }
+        else if(j<0){
+            j++;
+        }
+        else if(j >= m){
+            j--;
+        }
+        
+        System.out.println(i);
+        System.out.println(j);
+        
+    }
+    public static void main(String[] args) throws Exception {
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        
+        int [][] arr = new int[n][m];
+        
+        input(arr);
+        
+        exitPoint(arr);
+    }
+
+}
