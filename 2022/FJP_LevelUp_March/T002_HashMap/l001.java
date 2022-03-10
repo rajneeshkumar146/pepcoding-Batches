3204d67b06380c8ca48cf6617f8779f4bc79a80b
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-03-10 23:43:47 +0530

    update

--- 2022/FJP_LevelUp_March/T002_HashMap/l001.java
+++ 2022/FJP_LevelUp_March/T002_HashMap/l001.java
@@ -48,12 +48,55 @@ public class l001 {
         return dp[IDX];
     }
 
+    public int numDecodings_opti(String s) {
+        int a = 1, b = 0;
+        for (int idx = s.length() - 1; idx >= 0; idx--) {
+            char ch1 = s.charAt(idx);
+            int count = 0;
+            if (ch1 != '0') {
+                count += a;
+
+                if (idx < s.length() - 1) {
+                    int num = (ch1 - '0') * 10 + (s.charAt(idx + 1) - '0');
+                    if (num <= 26)
+                        count += b;
+                }
+            }
+            b = a;
+            a = count;
+        }
+
+        return a;
+    }
+
     public int numDecodings(String s) {
         int n = s.length();
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
 
         return numDecodings_memo(s, 0, dp);
+    }
+
+    public int rob_memo(int[] nums, int idx, int[] dp) {
+        if (idx == nums.length) {
+            return dp[idx] = 0;
+        }
+        if (dp[idx] != -1)
+            return dp[idx];
+
+        int robThatHouse = nums[idx] + (idx + 2 > nums.length ? 0 : rob_memo(nums, idx + 2, dp));
+        int notRobThatHouse = rob_memo(nums, idx + 1, dp);
+
+        return dp[idx] = Math.max(robThatHouse, notRobThatHouse);
+
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return rob_memo(nums, 0, dp);
     }
 
 }
