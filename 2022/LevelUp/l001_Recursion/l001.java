506ce2856eb4af92dc11174d3fa96b5a94afb616
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-03-23 23:39:29 +0530

    update

--- 2022/LevelUp/l001_Recursion/l001.java
+++ 2022/LevelUp/l001_Recursion/l001.java
@@ -94,7 +94,6 @@ public class l001 {
                 int r = sr + rad * dir[d][0];
                 int c = sc + rad * dir[d][1];
 
-                // Is it a right code?
                 if (r >= 0 && c >= 0 && r <= er && c <= ec) {
                     if (!vis[r][c])
                         count += floodFillJump(r, c, er, ec, vis, dir, dirS, psf + dirS[d] + rad);
@@ -105,6 +104,36 @@ public class l001 {
 
         vis[sr][sc] = false;
         return count;
+    }
+
+    // 63
+    public int uniquePathsWithObstacles(int sr, int sc, int er, int ec, int[][] obstacleGrid, int[][] dir) {
+        if (sr == er && sc == ec) {
+            return 1;
+        }
+
+        obstacleGrid[sr][sc] = 1;
+        int count = 0;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (r >= 0 && c >= 0 && r <= er && c <= ec && obstacleGrid[r][c] == 0) {
+                count += uniquePathsWithObstacles(r, c, er, ec, obstacleGrid, dir);
+            }
+
+        }
+        obstacleGrid[sr][sc] = 0;
+        return count;
+    }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[n - 1][m - 1] == 1)
+            return 0;
+        int[][] dir = { { 0, 1 }, { 1, 0 } };
+
+        return uniquePathsWithObstacles(0, 0, n - 1, m - 1, obstacleGrid, dir);
     }
 
     public static void main(String[] args) {
