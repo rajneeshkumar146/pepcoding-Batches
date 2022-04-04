7a5fec6a5ef5480ad268fd9848f1f3c5404897c6
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-04-04 23:41:25 +0530

    update

--- 2022/LevelUp/l001_Recursion/l004_Queen.java
+++ 2022/LevelUp/l001_Recursion/l004_Queen.java
@@ -205,6 +205,7 @@ public class l004_Queen {
     // crypto
     static String str1 = "send", str2 = "more", str3 = "money";
     static boolean[] isNumUsed = new boolean[10];
+    static int[] mapping = new int[26];
 
     public static void crypto() {
         String str = str1 + str2 + str3;
@@ -221,11 +222,40 @@ public class l004_Queen {
 
         if (str.length() > 10)
             return;
+
+        System.out.println(crypto(str, 0));
+    }
+
+    private static int stringToNumber(String str) {
+        int res = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            res = res * 10 + mapping[ch - 'a'];
+        }
+
+        return res;
+    }
+
+    private static boolean isValidMapping() {
+        if (mapping[str1.charAt(0) - 'a'] == 0 || mapping[str2.charAt(0) - 'a'] == 0
+                || mapping[str3.charAt(0) - 'a'] == 0)
+            return false;
+
+        int num1 = stringToNumber(str1);
+        int num2 = stringToNumber(str2);
+        int num3 = stringToNumber(str3);
+
+        return num1 + num2 == num3;
     }
 
     public static int crypto(String str, int idx) {
         if (idx == str.length()) {
-            if (valid) {
+            if (isValidMapping()) {
+                for (int i = 0; i < str.length(); i++) {
+                    char ch = str.charAt(i);
+                    System.out.print(ch + "=" + mapping[ch - 'a'] + ", ");
+                }
+                System.out.println();
                 return 1;
             }
 
@@ -233,12 +263,15 @@ public class l004_Queen {
         }
 
         int count = 0;
+        char ch = str.charAt(idx);
         for (int num = 0; num <= 9; num++) {
             if (!isNumUsed[num]) {
                 isNumUsed[num] = true;
+                mapping[ch - 'a'] = num;
 
                 count += crypto(str, idx + 1);
 
+                mapping[ch - 'a'] = 0;
                 isNumUsed[num] = false;
             }
 
@@ -255,7 +288,8 @@ public class l004_Queen {
         // System.out.println(queenPermutation2D(boxes, 4, ""));
 
         // System.out.println(nqueen_01(boxes, 4, 0, ""));
-        System.out.println(nqueen_01_Perm(boxes, 4, ""));
+        // System.out.println(nqueen_01_Perm(boxes, 4, ""));
+        crypto();
     }
 
 }
---
+++ 2022/LevelUp/l001_Recursion/l005_bits.java
@@ -0,0 +1,66 @@
+public class l005_bits {
+
+    public static int leftShift(int x) {
+        return x << 2;
+    }
+
+    public static int rightShift(int x) {
+        return x >> 2;
+    }
+
+    public static int setTrue(int x, int idx) {
+        int mask = 1 << idx;
+        return x | mask;
+    }
+
+    public static int setFalse(int x, int idx) {
+        int mask = ~(1 << idx);
+        return x & mask;
+    }
+
+    public static int multiplyBy2(int num, int pow) {
+        return (num << pow);
+    }
+
+    public static int divideBy2(int num, int pow) {
+        return (num >> pow);
+    }
+
+    public static boolean isEven(int x) {
+        return (x & 1) == 0;
+    }
+
+    // 231
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    // 342
+    public boolean isPowerOfFour(int n) {
+
+    }
+
+    // 136
+    public int singleNumber(int[] nums) {
+
+    }
+
+    // 268
+    public int missingNumber(int[] nums) {
+
+    }
+
+    // 191
+    public int hammingWeight(int n) {
+
+    }
+
+    // 338
+    public int[] countBits(int n) {
+
+    }
+
+    public static void main(String... args) {
+        System.out.println(rightShift(40));
+    }
+}
