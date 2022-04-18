a32b9721c194257ff7370015d0ca8dfaf323596e
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-04-18 23:36:17 +0530

    update

--- 2022/LevelUp/l002_LinkedList/l001.java
+++ 2022/LevelUp/l002_LinkedList/l001.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class l001 {
     public class ListNode {
         int val;
@@ -166,19 +168,339 @@ public class l001 {
         c1.next = h2;
     }
 
-    // 19
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null)
+            return head;
 
+        ListNode slow = head, fast = head;
+        while (n-- > 0)
+            fast = fast.next;
+
+        if (fast == null) {
+            ListNode rn = head;
+            head = head.next;
+            rn.next = null;
+            return head;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        ListNode rn = slow.next;
+        slow.next = rn.next;
+        rn.next = null;
+        return head;
+    }
+
+    public void removeNthFromEnd_followUp(ListNode head, int n) {
+        if (head == null)
+            return;
+
+        ListNode slow = head, fast = head;
+        while (n-- > 0)
+            fast = fast.next;
+
+        if (fast == null) {
+            head.val = head.next.val;
+            ListNode rn = slow.next;
+            slow.next = rn.next;
+            rn.next = null;
+            return;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        ListNode rn = slow.next;
+        slow.next = rn.next;
+        rn.next = null;
     }
 
     // 2
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode dummy = new ListNode(-1), prev = dummy, c1 = l1, c2 = l2;
 
+        int carry = 0;
+        while (c1 != null || c2 != null || carry != 0) {
+            int sum = carry + (c1 != null ? c1.val : 0) + (c2 != null ? c2.val : 0);
+            carry = sum / 10;
+            prev.next = new ListNode(sum % 10);
+
+            prev = prev.next;
+            if (c1 != null)
+                c1 = c1.next;
+            if (c2 != null)
+                c2 = c2.next;
+        }
+
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode head = dummy.next;
+        dummy.next = null;
+
+        head = reverseList(head);
+        return head;
     }
 
     // pepcoding
-    public ListNode subtractTwoNumbers(ListNode l1, ListNode l2) {
+
+    public static int isBiggerList(ListNode l1, ListNode l2) {
+        int len1 = len(l1), len2 = len(l2);
+        if (len1 == len2) {
+            ListNode c1 = l1, c2 = l2;
+            while (c1 != null) {
+                if (c1.val != c2.val)
+                    return c1.val - c2.val;
+                c1 = c1.next;
+                c2 = c2.next;
+            }
+        }
+
+        return len1 - len2;
+    }
+
+    public static ListNode subtractTwoNumbers(ListNode l1, ListNode l2) {
+        if (isBiggerList(l1, l2) < 0) {
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode dummy = new ListNode(-1), prev = dummy, c1 = l1, c2 = l2;
+
+        int borrow = 0;
+        while (c1 != null || c2 != null) {
+            int val = borrow + (c1 != null ? c1.val : 0) - (c2 != null ? c2.val : 0);
+            if (val < 0) {
+                val += 10;
+                borrow = -1;
+            } else {
+                borrow = 0;
+            }
+
+            prev.next = new ListNode(val);
+
+            prev = prev.next;
+            if (c1 != null)
+                c1 = c1.next;
+            if (c2 != null)
+                c2 = c2.next;
+        }
+
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode head = dummy.next;
+        dummy.next = null;
+
+        head = reverseList(head);
+        c1 = head;
+
+        while (c1.next != null) {
+            if (c1.val != 0) {
+                break;
+            }
+            c1 = c1.next;
+        }
+
+        return c1;
+    }
+
+    public static ListNode removeDuplicates(ListNode head) {
+        if (head == null && head.next == null)
+            return head;
+        ListNode prev = head, curr = head.next;
+
+        while (curr != null) {
+            if (prev.val != curr.val) {
+                prev.next = curr;
+                prev = prev.next;
+            }
+
+            curr = curr.next;
+        }
+        prev.next = curr;
+
+        return head;
+    }
+
+    public static ListNode removeDuplicates_02(ListNode head) {
+        if (head == null && head.next == null)
+            return head;
+        ListNode dummy = new ListNode(-1), prev = dummy, curr = head.next;
+        prev.next = head;
+
+        while (curr != null) {
+            boolean isSequence = false;
+            while (curr != null && prev.next.val == curr.val) {
+                isSequence = true;
+                curr = curr.next;
+            }
+
+            if (isSequence) {
+                prev.next = curr;
+            } else {
+                prev = prev.next;
+            }
+
+            if (curr != null)
+                curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+
+    // pepcoding
+    public static ListNode segregateEvenOdd(ListNode head) {
         return null;
+    }
+
+    // 328
+    public ListNode oddEvenList(ListNode head) {
+        return null;
+    }
+
+    // 148
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode mid = midNode(head), nHead = mid.next;
+        mid.next = null;
+        return mergeTwoLists(sortList(head), sortList(nHead));
+    }
+
+    // 23
+    public ListNode mergeKLists_bruteForce(ListNode[] lists) {
+        ListNode ans = null;
+        for (ListNode list : lists)
+            ans = mergeTwoLists(ans, list);
+
+        return ans;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists, int si, int ei) {
+        if (si >= ei)
+            return si > ei ? null : lists[si];
+        int mid = (si + ei) / 2;
+
+        return mergeTwoLists(mergeKLists(lists, si, mid), mergeKLists(lists, mid + 1, ei));
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+        return mergeKLists(lists, 0, n - 1);
+    }
+
+    // 25
+    ListNode th = null, tt = null;
+
+    private void addFirst(ListNode node) {
+        if (th == null)
+            th = tt = node;
+        else {
+            node.next = th;
+            th = node;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null || k == 1)
+            return head;
+
+        int len = length(head);
+        ListNode ah = null, at = null, curr = head;
+        while (len >= k) {
+            int tempK = k;
+            while (tempK-- > 0) {
+                ListNode forw = curr.next;
+                curr.next = null;
+                addFirst(curr);
+                curr = forw;
+            }
+
+            if (ah == null) {
+                ah = th;
+                at = tt;
+            } else {
+                at.next = th;
+                at = tt;
+            }
+
+            th = tt = null;
+            len -= k;
+        }
+        at.next = curr;
+        return ah;
+    }
+
+    // 92
+    public ListNode reverseBetween(ListNode head, int n, int m) {
+        if (head == null || head.next == null || n == m)
+            return head;
+        int idx = 1;
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            while (curr != null && idx >= n && idx <= m) {
+                ListNode forw = curr.next;
+                curr.next = null;
+                addFirst(curr);
+                curr = forw;
+                idx++;
+            }
+
+            if (idx > m) {
+                if (prev != null) {
+                    prev.next = th;
+                    tt.next = curr;
+                    return head;
+                } else {
+                    tt.next = curr;
+                    return th;
+                }
+            }
+
+            idx++;
+            prev = curr;
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    // 817
+    public int numComponents(ListNode head, int[] nums) {
+        if (head == null || nums.length == 0)
+            return 0;
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int ele : nums)
+            set.add(ele);
+
+        ListNode curr = head;
+        int components = 0;
+        while (curr != null) {
+            if (set.contains(curr.val) && (curr.next == null || !set.contains(curr.next.val)))
+                components++;
+            curr = curr.next;
+        }
+        return components;
+    }
+
+    // 1171
+    public ListNode removeZeroSumSublists(ListNode head) {
+
+    }
+
+    // 138
+    public Node copyRandomList(Node head) {
+
     }
 
 }
