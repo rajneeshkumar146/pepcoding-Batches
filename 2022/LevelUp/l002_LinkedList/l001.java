ac5ea0b57a339b75f999a98c4b8c7b0dc676b3cf
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-04-20 23:05:23 +0530

    update

--- 2022/LevelUp/l002_LinkedList/l001.java
+++ 2022/LevelUp/l002_LinkedList/l001.java
@@ -512,7 +512,72 @@ public class l001 {
 
     // 138
     public Node copyRandomList(Node head) {
+        return null;
 
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                break;
+        }
+
+        return slow == fast;
+
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                break;
+        }
+
+        if (slow != fast)
+            return null;
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode getTail(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+
+        return curr;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode tail = getTail(headA);
+        tail.next = headB;
+        ListNode intersectionNode = detectCycle(headA);
+        tail.next = null;  
+
+        return intersectionNode;
     }
 
 }
