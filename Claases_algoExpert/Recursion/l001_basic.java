2856ccb19aa2b46c0c24a59fe701d3f0a148c68f
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-04-23 11:28:12 +0530

    update

--- Claases_algoExpert/Recursion/l001_basic.java
+++ Claases_algoExpert/Recursion/l001_basic.java
@@ -12,24 +12,59 @@ public class l001_basic {
         if (a > b)
             return;
 
-            printDecreasing(a + 1, b);
+        printDecreasing(a + 1, b);
+        System.out.println(a);
+    }
+
+    public static void printEvenOdd(int a, int b) {
+        if (a > b)
+            return;
+
+        if (a % 2 == 0)
             System.out.println(a);
-    }
-
-    public static void printEvenOdd(int a,int b){
-
-    }
-
-    public static int fact(int n){
+        printEvenOdd(a + 1, b);
+        if (a % 2 != 0)
+            System.out.println(a);
 
     }
 
-    public static int pow(int a,int b){
+    public static int fact(int n) {
+        return n == 0 ? 1 : n * fact(n - 1);
+    }
+
+    public static int pow(int a, int b) {
+        return b == 0 ? 1 : a * pow(a, b - 1);
+    }
+
+    public static int getNthFib(int n) {
+        if (n <= 1)
+            return n;
+        return getNthFib(n - 1) + getNthFib(n - 2);
+    }
+
+    public static int check(int n) {
+        if (n <= 1) {
+            System.out.println("base: " + n);
+            return n + 3;
+        }
+
+        int count = 0;
+
+        System.out.println("pre: " + n);
+        count += check(n - 1);
+
+        System.out.println("in: " + n);
+
+        count += check(n - 2);
+        System.out.println("post: " + n);
+
+        return count + 3;
 
     }
 
     public static void main(String... args) {
         printDecreasing(5, 15);
+        System.out.println(check(5));
     }
 
 }
