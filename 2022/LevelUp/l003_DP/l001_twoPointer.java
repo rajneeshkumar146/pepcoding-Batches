11d62c901c4b79e113131cd50d94a29527a796e9
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-04-27 23:38:02 +0530

    update

---
+++ 2022/LevelUp/l003_DP/l001_twoPointer.java
@@ -0,0 +1,173 @@
+public class l001_twoPointer {
+    /*
+     * 1. Faith
+     * 2. Tree Diagram
+     * 3. Recursion
+     * 4. Recursion -> Memoization
+     * 5. Obervation
+     * 6. Memoization -> Tabulation after observation
+     * 7. Optimization
+     */
+
+    public static void display(int[] dp) {
+        for (int ele : dp)
+            System.out.print(ele + " ");
+        System.out.println();
+    }
+
+    public static void display2D(int[][] dp) {
+        for (int[] d : dp)
+            display(d);
+    }
+
+    public static int fibo_memo(int n, int[] dp) {
+        if (n <= 1)
+            return dp[n] = n;
+        if (dp[n] != 0)
+            return dp[n];
+
+        return dp[n] = fibo_memo(n - 1, dp) + fibo_memo(n - 2, dp);
+    }
+
+    public static int fibo_tabu(int N, int[] dp) {
+        for (int n = 0; n <= N; n++) {
+            if (n <= 1) {
+                dp[n] = n;
+                continue;
+            }
+
+            dp[n] = dp[n - 1] + dp[n - 2];// fibo_memo(n - 1, dp) + fibo_memo(n - 2, dp);
+        }
+        return dp[N];
+    }
+
+    public static int fibo_opti(int N) {
+        int a = 0, b = 1;
+        for (int n = 0; n < N; n++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+    public static void fibo() {
+        int n = 12;
+        int[] dp = new int[n + 1];
+        // System.out.println(fibo_memo(n, dp));
+        System.out.println(fibo_tabu(n, dp));
+        System.out.println(fibo_opti(n));
+        display(dp);
+    }
+
+    // 1137
+    public int tribonacci_memo(int n, int[] dp) {
+        if (n <= 2) {
+            return dp[n] = (n == 0 ? 0 : 1);
+        }
+
+        if (dp[n] != 0)
+            return dp[n];
+        return dp[n] = tribonacci_memo(n - 1, dp) + tribonacci_memo(n - 2, dp) + tribonacci_memo(n - 3, dp);
+    }
+
+    public int tribonacci_tabu(int N, int[] dp) {
+        for (int n = 0; n <= N; n++) {
+            if (n <= 2) {
+                dp[n] = (n == 0 ? 0 : 1);
+                continue;
+            }
+
+            dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3];
+        }
+
+        return dp[N];
+    }
+
+    public int tribonacci_opti(int N) {
+        int a = 0, b = 1, c = 1;
+        for (int n = 0; n < N; n++) {
+            int sum = a + b + c;
+            a = b;
+            b = c;
+            c = sum;
+        }
+
+        return a;
+    }
+
+    public int tribonacci(int n) {
+        int[] dp = new int[n + 1];
+        int ans = tribonacci_memo(n, dp);
+        display(dp);
+        return ans;
+    }
+
+    // 70
+    public int climbStairs_memo(int n, int[] dp) {
+        if (n <= 1)
+            return dp[n] = 1;
+        if (dp[n] != 0)
+            return dp[n];
+
+        return dp[n] = climbStairs_memo(n - 1, dp) + climbStairs_memo(n - 2, dp);
+    }
+
+    public int climbStairs_tabu(int N, int[] dp) {
+        for (int n = 0; n <= N; n++) {
+            if (n <= 1) {
+                dp[n] = 1;
+                continue;
+            }
+
+            dp[n] = dp[n - 1] + dp[n - 2];
+        }
+        return dp[N];
+    }
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        return climbStairs_memo(n, dp);
+    }
+
+    // 746
+    public int minCostClimbingStairs_memo(int[] cost, int n, int[] dp) {
+        if (n <= 1) {
+            return dp[n] = cost[n];
+        }
+
+        if (dp[n] != 0)
+            return dp[n];
+
+        int fcall = minCostClimbingStairs_memo(cost, n - 1, dp);
+        int scall = minCostClimbingStairs_memo(cost, n - 2, dp);
+        int ans = Math.min(fcall, scall) + (n == cost.length ? 0 : cost[n]);
+
+        return dp[n] = ans;
+    }
+
+    public int minCostClimbingStairs_tabu(int[] cost, int N, int[] dp) {
+        for (int n = 0; n <= N; n++) {
+            if (n <= 1) {
+                dp[n] = cost[n];
+                continue;
+            }
+
+            dp[n] = Math.min(dp[n - 1], dp[n - 2]) + (n == cost.length ? 0 : cost[n]);
+        }
+
+        return dp[N];
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        return minCostClimbingStairs_memo(cost, n, dp);
+    }
+
+    // HW : https://practice.geeksforgeeks.org/problems/friends-pairing-problem5425/1
+
+    public static void main(String[] args) {
+        tribonacci(10);
+    }
+}
