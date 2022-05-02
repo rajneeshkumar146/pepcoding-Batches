faa03e47ed39b1d14e05bfd9fb1d47954c3dbb9f
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-05-02 23:47:33 +0530

    update

--- 2022/LevelUp/l003_DP/l001_leetcode.java
+++ 2022/LevelUp/l003_DP/l001_leetcode.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class l001_leetcode {
     // 62
     public int uniquePaths(int n, int m) {
@@ -100,7 +102,39 @@ public class l001_leetcode {
     }
 
     // https://practice.geeksforgeeks.org/problems/gold-mine-problem2608/1/
-    
+
+    public static int goldmine_memo(int sr, int sc, int er, int ec, int[][] arr, int[][] dp, int[][] dir) {
+        if (sc == ec) {
+            return dp[sr][sc] = arr[sr][sc];
+        }
+
+        if (dp[sr][sc] != -1)
+            return dp[sr][sc];
+
+        int maxCoin = 0;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0], c = sc + dir[d][1];
+            if (r >= 0 && c >= 0 && r < dp.length && c < dp[0].length) {
+                maxCoin = Math.max(maxCoin, goldmine_memo(r, c, er, ec, arr, dp, dir) + arr[sr][sc]);
+            }
+        }
+
+        return dp[sr][sc] = maxCoin;
+    }
+
+    public static int maxGold(int n, int m, int[][] arr) {
+        int[][] dir = { { 0, 1 }, { 1, 1 }, { -1, 1 } };
+        int[][] dp = new int[n][m];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        int maxCoin = 0;
+        for (int r = 0; r < arr.length; r++) {
+            maxCoin = Math.max(maxCoin, goldmine_memo(r, 0, n - 1, m - 1, arr, dp, dir));
+        }
+
+        return maxCoin;
+    }
+
     // https://www.geeksforgeeks.org/count-the-number-of-ways-to-divide-n-in-k-groups-incrementally/
 
 }
