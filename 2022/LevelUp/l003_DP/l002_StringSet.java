00ec9265ecc7d83b30aacee0f72f921716944e75
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-05-04 23:50:31 +0530

    upadte

--- 2022/LevelUp/l003_DP/l002_StringSet.java
+++ 2022/LevelUp/l003_DP/l002_StringSet.java
@@ -79,6 +79,23 @@ public class l002_StringSet {
             return dp[n][m] = Math.max(b, c);
     }
 
+    public static int longestCommonSubsequence_tabu(String s1, String s2, int N, int M, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = 0;
+                    continue;
+                }
+                if (s1.charAt(n - 1) == s2.charAt(m - 1))
+                    dp[n][m] = dp[n - 1][m - 1] + 1;
+                else
+                    dp[n][m] = Math.max(dp[n][m - 1], dp[n - 1][m]);
+            }
+        }
+
+        return dp[N][M];
+    }
+
     public static int longestCommonSubsequence(String s1, String s2) {
         int n = s1.length(), m = s2.length();
         int[][] dp = new int[n + 1][m + 1];
@@ -88,6 +105,66 @@ public class l002_StringSet {
         display2D(dp);
         return ans;
     }
+
+    // =======================================
+
+    public static int minDistance_memo(String s1, String s2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = n != 0 ? n : m;
+        }
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        int a = minDistance_memo(s1, s2, n - 1, m - 1, dp);
+        int b = minDistance_memo(s1, s2, n - 1, m, dp);
+        int c = minDistance_memo(s1, s2, n, m - 1, dp);
+
+        if (s1.charAt(n - 1) == s2.charAt(m - 1))
+            return dp[n][m] = a;
+        else
+            return dp[n][m] = Math.min(Math.min(a, b), c) + 1;
+    }
+
+    public static int minDistance_tabu(String s1, String s2, int N, int M, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int m = 0; m <= M; m++) {
+                if (n == 0 || m == 0) {
+                    dp[n][m] = n != 0 ? n : m;
+                    continue;
+                }
+                if (s1.charAt(n - 1) == s2.charAt(m - 1))
+                    dp[n][m] = dp[n - 1][m - 1];
+                else
+                    dp[n][m] = Math.min(Math.min(dp[n][m - 1], dp[n - 1][m]), dp[n - 1][m - 1]) + 1;
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public int minDistance(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        int ans = minDistance_memo(s1, s2, n, m, dp);
+        return ans;
+    }
+
+    public int minDistance_followup(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+
+        int[] cost = { 2, 5, 3 }; // insert, delete, replace
+
+        int ans = 0;
+        return ans;
+    }
+
+    // https://www.geeksforgeeks.org/minimum-number-deletions-insertions-transform-one-string-another/
 
     public static void main(String[] args) {
         longestPlaindromicSubsequence();
