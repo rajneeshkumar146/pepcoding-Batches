615575559ce4a9f76f243279712ae8877fe76dc0
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-05-04 22:33:57 +0530

    upadte

--- 2022/LevelUp/l003_DP/l002_StringSet.java
+++ 2022/LevelUp/l003_DP/l002_StringSet.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class l002_StringSet {
 
     public static void display(int[] dp) {
@@ -55,6 +57,36 @@ public class l002_StringSet {
 
         display2D(dp);
         System.out.println(ans);
+    }
+
+    // =======================================================================
+
+    public static int longestCommonSubsequence_memo(String s1, String s2, int n, int m, int[][] dp) {
+        if (n == 0 || m == 0) {
+            return dp[n][m] = 0;
+        }
+
+        if (dp[n][m] != 0)
+            return dp[n][m];
+
+        int a = longestCommonSubsequence_memo(s1, s2, n - 1, m - 1, dp);
+        int b = longestCommonSubsequence_memo(s1, s2, n - 1, m, dp);
+        int c = longestCommonSubsequence_memo(s1, s2, n, m - 1, dp);
+
+        if (s1.charAt(n - 1) == s2.charAt(m - 1))
+            return dp[n][m] = a + 1;
+        else
+            return dp[n][m] = Math.max(b, c);
+    }
+
+    public static int longestCommonSubsequence(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        int ans = longestCommonSubsequence_memo(s1, s2, n, m, dp);
+        display2D(dp);
+        return ans;
     }
 
     public static void main(String[] args) {
