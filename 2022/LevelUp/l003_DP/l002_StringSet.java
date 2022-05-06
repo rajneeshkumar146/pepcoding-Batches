b3410e721b104d72a0d6ad0575022543428a8c86
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-05-06 21:53:58 +0530

    update

--- 2022/LevelUp/l003_DP/l002_StringSet.java
+++ 2022/LevelUp/l003_DP/l002_StringSet.java
@@ -166,6 +166,33 @@ public class l002_StringSet {
 
     // https://www.geeksforgeeks.org/minimum-number-deletions-insertions-transform-one-string-another/
 
+    // 115
+    public int numDistinct_memo(String s, String t, int n, int m, int[][] dp) {
+        if (m == 0)
+            return dp[n][m] = 1;
+        else if (n == 0)
+            return dp[n][m] = 0;
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        int a = numDistinct_memo(s, t, n - 1, m - 1, dp);
+        int b = numDistinct_memo(s, t, n - 1, m, dp);
+
+        if (s.charAt(n - 1) == t.charAt(m - 1))
+            return dp[n][m] = a + b;
+        else
+            return dp[n][m] = b;
+    }
+
+    public int numDistinct(String s, String t) {
+        int n = s.length(), m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int[] d : dp)
+            Arrays.fill(d, -1);
+        return numDistinct_memo(s, t, n, m, dp);
+    }
+
     public static void main(String[] args) {
         longestPlaindromicSubsequence();
     }
