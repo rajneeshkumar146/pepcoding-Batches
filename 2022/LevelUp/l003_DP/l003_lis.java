b1c385b50a029c594f249566d2bb35798e4fde9c
rajneeshkumar146 <rajneeshkumar@gmail.com>
2022-05-09 23:33:43 +0530

    update

--- 2022/LevelUp/l003_DP/l003_lis.java
+++ 2022/LevelUp/l003_DP/l003_lis.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class l003_lis {
 
     private static int LIS_rec(int[] arr, int idx, int[] dp) {
@@ -25,7 +27,8 @@ public class l003_lis {
         }
     }
 
-    public static int LIS_DP(int[] arr, int[] dp) {
+    // left to right
+    public static int LIS_DP_LR(int[] arr, int[] dp) {
         int maxLen = 0, n = arr.length;
         for (int i = 0; i < n; i++) {
             dp[i] = 1;
@@ -41,12 +44,46 @@ public class l003_lis {
         return maxLen;
     }
 
-    public static int LDS_DP(int[] arr, int[] dp) {
+    public static int LIS_DP_RL(int[] arr, int[] dp) {
         int maxLen = 0, n = arr.length;
         for (int i = n - 1; i >= 0; i--) {
             dp[i] = 1;
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+
+    // left to right
+    public static int reverse_LDS_DP_LR(int[] arr, int[] dp) {
+        int maxLen = 0, n = arr.length;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+
+    // Left to right
+    public static int reverse_LDS_DP_RL(int[] arr, int[] dp) {
+        int maxLen = 0, n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > arr[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
@@ -96,8 +133,8 @@ public class l003_lis {
         int[] LIS = new int[n];
         int[] LDS = new int[n];
 
-        LIS_DP(nums, LIS);
-        LDS_DP(nums, LDS);
+        LIS_DP_LR(nums, LIS);
+        LIS_DP_RL(nums, LDS);
 
         int maxLen = 0;
         for (int i = 0; i < n; i++) {
@@ -124,7 +161,47 @@ public class l003_lis {
     }
 
     // reverse_longestBitonicSequnece
+    public int reverse_LongestBitonicSequence(int[] nums) {
+        int n = nums.length;
+
+        int[] LIS = new int[n];
+        int[] LDS = new int[n];
+
+        reverse_LDS_DP_LR(nums, LIS);
+        reverse_LDS_DP_RL(nums, LDS);
+
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            maxLen = Math.max(maxLen, LIS[i] + LDS[i] - 1);
+        }
+        return maxLen;
+    }
 
     // 354: https://leetcode.com/problems/russian-doll-envelopes/
+    // O(n2)
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> {
+            return a[0] - b[0];
+        });
+
+        int n = envelopes.length, maxLen = 0;
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (envelopes[j][1] < envelopes[i][1] && envelopes[j][0] < envelopes[i][0])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+
+    // O(NlogN)
+    public int maxEnvelopes_optimized(int[][] envelopes) {
+
+    }
 
 }
